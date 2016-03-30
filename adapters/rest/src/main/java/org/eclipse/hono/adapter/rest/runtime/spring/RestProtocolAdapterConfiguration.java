00f93bbbd36d564881196f3f22fb46ab53732161
Henryk Konsek <hekonsek@gmail.com>
2016-03-30 09:52:46 +0200

    AMQP connection of REST protocol adapter main runner should be configurable.

--- adapters/rest/src/main/java/org/eclipse/hono/adapter/rest/runtime/spring/RestProtocolAdapterConfiguration.java
+++ adapters/rest/src/main/java/org/eclipse/hono/adapter/rest/runtime/spring/RestProtocolAdapterConfiguration.java
@@ -9,7 +9,6 @@
 package org.eclipse.hono.adapter.rest.runtime.spring;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.component.amqp.AMQPComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.eclipse.hono.adapter.rest.DefaultHttpRequestMapping;
 import org.eclipse.hono.adapter.rest.HttpRequestMapping;
@@ -18,15 +17,18 @@ import org.eclipse.hono.adapter.rest.RestProtocolAdapter;
 
 import java.util.Optional;
 
+import static java.lang.Integer.parseInt;
+import static org.apache.camel.component.amqp.AMQPComponent.amqp10Component;
+
 public class RestProtocolAdapterConfiguration {
 
     public static void main(String... args) throws Exception {
         CamelContext camelContext = new DefaultCamelContext();
 
         // IoT Connector configuration
-        String amqpBrokerUrl = "localhost";
-        int amqpBrokerPort = 5672;
-        camelContext.addComponent("amqp", AMQPComponent.amqp10Component("amqp://guest:guest@" + amqpBrokerUrl + ":" + amqpBrokerPort));
+        String amqpBrokerUrl = property("AMQP_SERVICE_HOST", "localhost");
+        int amqpBrokerPort = property("AMQP_SERVICE_PORT", 5672);
+        camelContext.addComponent("amqp", amqp10Component("amqp://guest:guest@" + amqpBrokerUrl + ":" + amqpBrokerPort));
 
         // REST endpoint configuration
         String host = "0.0.0.0";
@@ -36,6 +38,23 @@ public class RestProtocolAdapterConfiguration {
         camelContext.addRoutes(protocolAdapter);
 
         camelContext.start();
+    }
+
+    private static String property(String key, String defaultValue) {
+        String property = System.getProperty(key);
+        if(property != null) {
+            return property;
+        }
+        property = System.getenv(key);
+        if(property != null) {
+            return property;
+        }
+        return defaultValue;
+    }
+
+    private static int property(String key, int defaultValue) {
+        String property = property(key, null);
+        return property == null ? defaultValue : parseInt(property);
     }
 
 }
