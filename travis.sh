638b71d98daf51f19ba7cb66b2fd36639aadc632
Dominik Guggemos <dominik.guggemos@bosch-si.com>
2016-08-25 13:09:42 +0200

    [#45] next try, use DOCKER_HOST instead of docker.host

--- travis.sh
+++ travis.sh
@@ -5,7 +5,6 @@ set -e
 export PING_SLEEP=30s
 export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export BUILD_OUTPUT=$WORKDIR/build.out
-export docker.host=$DOCKER_HOST
 
 touch $BUILD_OUTPUT
 
