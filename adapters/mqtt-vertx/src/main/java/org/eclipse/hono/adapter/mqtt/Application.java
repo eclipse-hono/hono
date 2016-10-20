0f99b19416f32f284ba0bb8f4bc74362c04af284
Paolo Patierno <ppatierno@live.com>
2016-10-20 16:30:56 +0200

    Added first step implementation for the MQTT adapter  (#62)
    * Added first step implementation for the MQTT adapter. Run as verticle and start the Vert.x MQTT server.
    
    Signed-off-by: ppatierno <ppatierno@live.com>
    
    * Added "final" for parameters on bindXXXserver on HTTP REST and MQTT adapter
    Removed mention about Registration API on MQTT adapter
    
    Signed-off-by: ppatierno <ppatierno@live.com>
    
    * Addressed the "final" modifiers for methods params in HTTP REST and MQTT adapters
    
    Signed-off-by: ppatierno <ppatierno@live.com>

--- adapters/mqtt-vertx/src/main/java/org/eclipse/hono/adapter/mqtt/Application.java
+++ adapters/mqtt-vertx/src/main/java/org/eclipse/hono/adapter/mqtt/Application.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016 Bosch Software Innovations GmbH.
+ * Copyright (c) 2016 Red Hat
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,19 +7,15 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Bosch Software Innovations GmbH - initial creation
+ *    Red Hat - initial creation
  */
-package org.eclipse.hono.adapter.rest;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
+package org.eclipse.hono.adapter.mqtt;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,19 +25,16 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import io.vertx.core.CompositeFuture;
-import io.vertx.core.Future;
-import io.vertx.core.Handler;
-import io.vertx.core.Vertx;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * The Hono REST adapter main application class.
- * <p>
- * This class uses Spring Boot for configuring the REST adapter's properties.
- * By default there will be as many instances of the REST adapter verticle created as there are CPU cores
- * available. The {@code hono.maxinstances} config property can be used to set the maximum number
- * of instances to create. This may be useful for executing tests etc.
- * </p>
+ * The Hono MQTT adapter main application class.
  */
 @ComponentScan
 @Configuration
@@ -52,12 +45,12 @@ public class Application {
 
     @Autowired
     private Vertx vertx;
-    @Value(value = "${hono.maxinstances:0}")
+    @Value(value = "${hono.maxinstances:1}")
     private int maxInstances;
     @Value(value = "${hono.startuptimeout:20}")
     private int startupTimeout;
     @Autowired
-    private RestAdapterFactory factory;
+    private MqttAdapterFactory factory;
     private AtomicBoolean running = new AtomicBoolean();
 
     @PostConstruct
@@ -65,49 +58,50 @@ public class Application {
 
         if (running.compareAndSet(false, true)) {
             final int instanceCount;
-            if (maxInstances > 0 && maxInstances < Runtime.getRuntime().availableProcessors()) {
-                instanceCount = maxInstances;
+            if (this.maxInstances > 0 && this.maxInstances < Runtime.getRuntime().availableProcessors()) {
+                instanceCount = this.maxInstances;
             } else {
                 instanceCount = Runtime.getRuntime().availableProcessors();
             }
 
             try {
+
                 final CountDownLatch latch = new CountDownLatch(1);
                 final Future<Void> startFuture = Future.future();
                 startFuture.setHandler(done -> {
                     if (done.succeeded()) {
                         latch.countDown();
                     } else {
-                      LOG.error("could not start REST adapter", done.cause());
+                        LOG.error("could not start MQTT adapter", done.cause());
                     }
                 });
 
-                deployVerticle(instanceCount, startFuture);
+                this.deployVerticle(instanceCount, startFuture);
 
-                if (latch.await(startupTimeout, TimeUnit.SECONDS)) {
-                    LOG.info("REST adapter startup completed successfully");
+                if (latch.await(this.startupTimeout, TimeUnit.SECONDS)) {
+                    LOG.info("MQTT adapter startup completed successfully");
                 } else {
-                    LOG.error("startup timed out after {} seconds, shutting down ...", startupTimeout);
-                    shutdown();
+                    LOG.error("startup timed out after {} seconds, shutting down ...", this.startupTimeout);
+                    this.shutdown();
                 }
+
             } catch (InterruptedException e) {
                 LOG.error("startup process has been interrupted, shutting down ...");
                 Thread.currentThread().interrupt();
-                shutdown();
+                this.shutdown();
             }
         }
     }
 
     private void deployVerticle(final int instanceCount, final Future<Void> resultHandler) {
 
-        LOG.debug("starting up {} instances of REST adapter verticle", instanceCount);
-        @SuppressWarnings("rawtypes")
+        LOG.debug("starting up {} instances of MQTT adapter verticle", instanceCount);
         List<Future> results = new ArrayList<>();
         for (int i = 1; i <= instanceCount; i++) {
             final int instanceId = i;
             final Future<String> result = Future.future();
             results.add(result);
-            vertx.deployVerticle(factory.getRestAdapter(), d -> {
+            this.vertx.deployVerticle(this.factory.getMqttAdapter(), d -> {
                 if (d.succeeded()) {
                     LOG.debug("verticle instance {} deployed", instanceId);
                     result.complete();
@@ -118,50 +112,51 @@ public class Application {
             });
         }
         CompositeFuture.all(results).setHandler(done -> {
-            if (done.succeeded()) {
-                resultHandler.complete();
-            } else {
-                resultHandler.fail(done.cause());
-            }
+           if (done.succeeded()) {
+               resultHandler.complete();
+           } else {
+               resultHandler.fail(done.cause());
+           }
         });
+
     }
 
     @PreDestroy
     public void shutdown() {
-        if (running.compareAndSet(true, false)) {
+        if (this.running.compareAndSet(true, false)) {
             this.shutdown(startupTimeout, succeeded -> {
                 // do nothing
             });
         }
     }
 
-    public void shutdown(final long maxWaitTime, final Handler<Boolean> shutdownHandler) {
+    private void shutdown(final long maxWaitTime, final Handler<Boolean> shutdownHandler) {
 
         try {
+
             final CountDownLatch latch = new CountDownLatch(1);
-            if (vertx != null) {
-                vertx.close(r -> {
+            if (this.vertx != null) {
+                this.vertx.close(r -> {
                     if (r.failed()) {
-                        LOG.error("could not shut down REST adapter cleanly", r.cause());
+                        LOG.error("could not shut down MQTT adapter cleanly", r.cause());
                     }
                     latch.countDown();
                 });
             }
             if (latch.await(maxWaitTime, TimeUnit.SECONDS)) {
-                LOG.info("REST adapter shut down completed");
+                LOG.info("MQTT adapter shut down completed");
                 shutdownHandler.handle(Boolean.TRUE);
             } else {
-                LOG.error("shut down of REST adapter timed out, aborting...");
+                LOG.error("shut down of MQTT adapter timed out, aborting...");
                 shutdownHandler.handle(Boolean.FALSE);
             }
+
         } catch (InterruptedException e) {
-            LOG.error("shut down of REST adapter has been interrupted, aborting...");
+            LOG.error("shut down of MQTT adapter has been interrupted, aborting...");
             Thread.currentThread().interrupt();
             shutdownHandler.handle(Boolean.FALSE);
         }
     }
 
-    public static void main(final String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+    public static void main(final String[] args) { SpringApplication.run(Application.class, args); }
 }
