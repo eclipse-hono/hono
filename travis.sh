79a54b4898bc0d3b45c70a8c6bc3600b06731d93
ppatierno <ppatierno@live.com>
2017-02-27 15:27:09 +0100

    Added a way for compiling Qpid Proton and Qpid Dispatch Router using a Docker image Added before script steps for executing above image

--- travis.sh
+++ travis.sh
@@ -25,7 +25,7 @@ bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
 PING_LOOP_PID=$!
 
 # Build Qpid Proton and Qpid Dispatch Router bits and related Docker image
-make
+#make
 
 mvn install -Pbuild-docker-image,run-tests >> $BUILD_OUTPUT 2>&1
 
