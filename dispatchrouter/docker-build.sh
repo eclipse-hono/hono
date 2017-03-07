08f8fc63c80014bed75d0e6e87d5a1d86323c8d3
ppatierno <ppatierno@live.com>
2017-03-07 15:51:10 +0100

    Avoiding to push router image when a PR or not master branch
    Signed-off-by: ppatierno <ppatierno@live.com>

--- dispatchrouter/docker-build.sh
+++ dispatchrouter/docker-build.sh
@@ -12,7 +12,13 @@ fi
 
 docker build -t $REPO:$COMMIT $DIR || exit 1
 
-docker login -u $DOCKER_USER -p $DOCKER_PASS || exit 1
-docker tag $REPO:$COMMIT $REPO:$TAG || exit 1
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER || exit 1
-docker push $REPO || exit 1
+if [ "$TRAVIS_BRANCH" == "master" ] || [ -n "$TRAVIS_TAG" ]
+then
+    if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+    then
+        docker login -u $DOCKER_USER -p $DOCKER_PASS || exit 1
+        docker tag $REPO:$COMMIT $REPO:$TAG || exit 1
+        docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER || exit 1
+        docker push $REPO || exit 1
+    fi
+fi
