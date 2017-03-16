5a0e1e3ca225b1f36835ec207aac5c37cbe83813
ppatierno <ppatierno@live.com>
2017-03-16 09:49:29 +0100

    Added scripts for deploying/undeploying Eclipse Hono on OpenShift with a one "script" deployment Added usage of the above scripts in the web site documentation
    Signed-off-by: ppatierno <ppatierno@live.com>

---
+++ example/openshift/openshift_deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+echo DEPLOYING ECLIPSE HONO ON OPENSHIFT
+
+# creating new project
+oc new-project hono --description="Open source IoT connectivity" --display-name="Eclipse Hono"
+
+# creating the directory for Hono Server persistent volume
+if [ ! -d /tmp/hono ]; then
+    mkdir /tmp/hono
+    chmod 777 /tmp/hono
+else
+    echo /tmp/hono already exists !
+fi
+
+# creating Hono Server persistent volume (admin needed)
+oc login -u system:admin
+oc create -f ../../application/target/fabric8/hono-app-pv.yml
+
+# starting to deploy Eclipse Hono (developer user)
+oc login -u developer
+oc create -f ../../application/target/fabric8/hono-app-pvc.yml
+
+echo Deploying Qpid Dispatch Router ...
+oc create -f ../../dispatchrouter/target/fabric8/dispatch-router-svc.yml
+oc create -f ../../dispatchrouter/target/fabric8/dispatch-router-dc.yml
+echo ... done
+
+echo Deploying Hono Server ...
+oc create -f ../../application/target/fabric8/hono-app-svc.yml
+oc create -f ../../application/target/fabric8/hono-app-dc.yml
+echo ... done
+
+echo Deploying HTTP REST adapter ...
+oc create -f ../../adapters/rest-vertx/target/fabric8/hono-adapter-rest-vertx-svc.yml
+oc create -f ../../adapters/rest-vertx/target/fabric8/hono-adapter-rest-vertx-dc.yml
+echo ... done
+
+echo Deploying MQTT adapter ...
+oc create -f ../../adapters/mqtt-vertx/target/fabric8/hono-adapter-mqtt-vertx-svc.yml
+oc create -f ../../adapters/mqtt-vertx/target/fabric8/hono-adapter-mqtt-vertx-dc.yml
+echo ... done
+
+echo ECLIPSE HONO DEPLOYED ON OPENSHIFT
