ebb73e0bbf5d93bd9e58f84edcb3749e1842508a
ppatierno <ppatierno@live.com>
2017-05-12 10:55:56 +0200

    Added OpenShift route files for REST and MQTT adapters, Hono server and router
    Signed-off-by: ppatierno <ppatierno@live.com>

--- example/openshift/openshift_deploy.sh
+++ example/openshift/openshift_deploy.sh
@@ -24,21 +24,26 @@ oc create -f ../../application/target/fabric8/hono-app-pvc.yml
 echo Deploying Qpid Dispatch Router ...
 oc create -f ../../dispatchrouter/target/fabric8/dispatch-router-svc.yml
 oc create -f ../../dispatchrouter/target/fabric8/dispatch-router-dc.yml
+oc create -f ../../dispatchrouter/target/fabric8/dispatch-router-route.yml
 echo ... done
 
 echo Deploying Hono Server ...
 oc create -f ../../application/target/fabric8/hono-app-svc.yml
 oc create -f ../../application/target/fabric8/hono-app-dc.yml
+oc create -f ../../application/target/fabric8/hono-app-route.yml
 echo ... done
 
 echo Deploying HTTP REST adapter ...
 oc create -f ../../adapters/rest-vertx/target/fabric8/hono-adapter-rest-vertx-svc.yml
 oc create -f ../../adapters/rest-vertx/target/fabric8/hono-adapter-rest-vertx-dc.yml
+oc create -f ../../adapters/rest-vertx/target/fabric8/hono-adapter-rest-vertx-route.yml
+oc create -f ../../adapters/rest-vertx/target/fabric8/hono-adapter-rest-vertx-insecure-route.yml
 echo ... done
 
 echo Deploying MQTT adapter ...
 oc create -f ../../adapters/mqtt-vertx/target/fabric8/hono-adapter-mqtt-vertx-svc.yml
 oc create -f ../../adapters/mqtt-vertx/target/fabric8/hono-adapter-mqtt-vertx-dc.yml
+oc create -f ../../adapters/mqtt-vertx/target/fabric8/hono-adapter-mqtt-vertx-route.yml
 echo ... done
 
 echo ECLIPSE HONO DEPLOYED ON OPENSHIFT
