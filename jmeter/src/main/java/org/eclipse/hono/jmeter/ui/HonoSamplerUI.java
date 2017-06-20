32fd7e25fa97d6b2d2e0f8f7dcff88834d1e3e87
Marc Pellmann <marc.pellmann@bosch-si.com>
2017-06-20 15:06:53 +0200

    Support for device registry as own/new service
    Signed-off-by: Marc Pellmann <marc.pellmann@bosch-si.com>

--- jmeter/src/main/java/org/eclipse/hono/jmeter/ui/HonoSamplerUI.java
+++ jmeter/src/main/java/org/eclipse/hono/jmeter/ui/HonoSamplerUI.java
@@ -13,12 +13,10 @@
 package org.eclipse.hono.jmeter.ui;
 
 import java.awt.*;
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 import javax.swing.*;
 
-import org.apache.jmeter.gui.util.JLabeledRadioI18N;
 import org.apache.jmeter.gui.util.VerticalPanel;
 import org.apache.jmeter.samplers.gui.AbstractSamplerGui;
 import org.apache.jmeter.testelement.TestElement;
@@ -31,45 +29,29 @@ import org.eclipse.hono.jmeter.HonoSampler;
  */
 abstract public class HonoSamplerUI extends AbstractSamplerGui {
 
-    private final JLabeledTextField host           = new JLabeledTextField("Host");
-    private final JLabeledTextField port           = new JLabeledTextField("Port");
-    private final JLabeledTextField user           = new JLabeledTextField("User");
-    private final JLabeledTextField pwd            = new JLabeledTextField("Password");
-    private final JLabeledTextField container      = new JLabeledTextField("Name");
     private final JLabeledTextField tenant         = new JLabeledTextField("Tenant");
-    private final JLabeledTextField trustStorePath = new JLabeledTextField("Truststore path");
+    private final JLabeledTextField container      = new JLabeledTextField("Name");
     private final JLabeledChoice    endpoint       = new JLabeledChoice("Endpoint",
             Stream.of(HonoSampler.Endpoint.values()).map(HonoSampler.Endpoint::name).toArray(String[]::new));
+    private final HonoServerOptionsPanel honoServerOptions;
+    private final JPanel optsPanel = new VerticalPanel();
 
-    private JPanel optsPanel;
-
-    public HonoSamplerUI() {
+    public HonoSamplerUI(final String serverTitle) {
+        honoServerOptions = new HonoServerOptionsPanel(serverTitle);
         setLayout(new BorderLayout());
         setBorder(makeBorder());
-
         add(makeTitlePanel(), BorderLayout.NORTH);
-        JPanel mainPanel = new VerticalPanel();
-        add(mainPanel, BorderLayout.CENTER);
-        mainPanel.add(createHonoOptions());
+        add(optsPanel, BorderLayout.CENTER);
     }
 
-    private JPanel createHonoOptions() {
-        JPanel optsPanelCon = new VerticalPanel();
-        optsPanelCon.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Hono options"));
-        optsPanel = new VerticalPanel();
-        optsPanel.add(host);
-        optsPanel.add(port);
-        optsPanel.add(user);
-        optsPanel.add(pwd);
-        optsPanel.add(container);
+    protected void addDefaultOptions() {
+        optsPanel.add(honoServerOptions);
         optsPanel.add(endpoint);
         optsPanel.add(tenant);
-        optsPanel.add(trustStorePath);
-        optsPanelCon.add(optsPanel);
-        return optsPanelCon;
+        optsPanel.add(container);
     }
 
-    void addOption(final JComponent component) {
+    protected void addOption(final JComponent component) {
         optsPanel.add(component);
     }
 
@@ -82,47 +64,43 @@ abstract public class HonoSamplerUI extends AbstractSamplerGui {
     public void modifyTestElement(final TestElement testElement) {
         this.configureTestElement(testElement);
         HonoSampler sampler = (HonoSampler) testElement;
-        sampler.setHost(host.getText());
+        sampler.setHost(honoServerOptions.getHost().getText());
         int portI = 0;
         try {
-            portI = Integer.parseInt(port.getText());
+            portI = Integer.parseInt(honoServerOptions.getPort().getText());
         } catch (NumberFormatException e) {
-            port.setText("0");
+            honoServerOptions.getPort().setText("0");
         }
         sampler.setPort(portI);
-        sampler.setUser(user.getText());
-        sampler.setPwd(pwd.getText());
+        sampler.setUser(honoServerOptions.getUser().getText());
+        sampler.setPwd(honoServerOptions.getPwd().getText());
         sampler.setEndpoint(HonoSampler.Endpoint.valueOf(endpoint.getText()));
         sampler.setTenant(tenant.getText());
         sampler.setContainer(container.getText());
-        sampler.setTrustStorePath(trustStorePath.getText());
+        sampler.setTrustStorePath(honoServerOptions.getTrustStorePath().getText());
     }
 
     @Override
     public void configure(final TestElement element) {
         super.configure(element);
         HonoSampler sampler = (HonoSampler) element;
-        host.setText(sampler.getHost());
-        port.setText(sampler.getPort() + "");
-        user.setText(sampler.getUser());
-        pwd.setText(sampler.getPwd());
+        honoServerOptions.getHost().setText(sampler.getHost());
+        honoServerOptions.getPort().setText(sampler.getPort() + "");
+        honoServerOptions.getUser().setText(sampler.getUser());
+        honoServerOptions.getPwd().setText(sampler.getPwd());
         endpoint.setText(sampler.getEndpoint());
         tenant.setText(sampler.getTenant());
         container.setText(sampler.getContainer());
-        trustStorePath.setText(sampler.getTrustStorePath());
+        honoServerOptions.getTrustStorePath().setText(sampler.getTrustStorePath());
     }
 
     @Override
     public void clearGui() {
         super.clearGui();
-        host.setText("");
-        port.setText("");
-        user.setText("");
-        pwd.setText("");
+        honoServerOptions.clearGui();
         endpoint.setSelectedIndex(0);
         tenant.setText("");
         container.setText("");
-        trustStorePath.setText("");
     }
 
 }
---
+++ jmeter/src/main/java/org/eclipse/hono/jmeter/ui/HonoServerOptionsPanel.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2016,2017 Bosch Software Innovations GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Bosch Software Innovations GmbH - initial creation
+ */
+package org.eclipse.hono.jmeter.ui;
+
+import javax.swing.*;
+
+import org.apache.jmeter.gui.util.VerticalPanel;
+import org.apache.jorphan.gui.JLabeledTextField;
+
+/**
+ * Server connection options
+ */
+public class HonoServerOptionsPanel extends VerticalPanel {
+
+    private final JLabeledTextField host           = new JLabeledTextField("Host");
+    private final JLabeledTextField port           = new JLabeledTextField("Port");
+    private final JLabeledTextField user           = new JLabeledTextField("User");
+    private final JLabeledTextField pwd            = new JLabeledTextField("Password");
+    private final JLabeledTextField trustStorePath = new JLabeledTextField("Truststore path");
+
+    public HonoServerOptionsPanel(final String title) {
+        setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), title));
+        add(host);
+        add(port);
+        add(user);
+        add(pwd);
+        add(trustStorePath);
+    }
+
+    public JLabeledTextField getHost() {
+        return host;
+    }
+
+    public JLabeledTextField getPort() {
+        return port;
+    }
+
+    public JLabeledTextField getUser() {
+        return user;
+    }
+
+    public JLabeledTextField getPwd() {
+        return pwd;
+    }
+
+    public JLabeledTextField getTrustStorePath() {
+        return trustStorePath;
+    }
+
+    public void clearGui() {
+        host.setText("");
+        port.setText("");
+        user.setText("");
+        pwd.setText("");
+        trustStorePath.setText("");
+    }
+}
