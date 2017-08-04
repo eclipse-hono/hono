cabf211015f20d99b69e934389e6004da65a1632
Kai Hudalla <kai.hudalla@bosch-si.com>
2017-08-04 09:08:33 +0200

    Define a variable for common options used for service creation.
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

--- example/src/main/deploy/docker/swarm_deploy.sh
+++ example/src/main/deploy/docker/swarm_deploy.sh
@@ -16,37 +16,38 @@ HONO_HOME=$SCRIPTPATH/../../../..
 CONFIG=$SCRIPTPATH/../../config
 CERTS=$CONFIG/hono-demo-certs-jar
 NS=hono
+CREATE_OPTIONS="-l project=$NS --network $NS --detach"
 
 echo DEPLOYING ECLIPSE HONO TO DOCKER SWARM
 
 # creating Hono network
-docker network create --driver overlay $NS
+docker network create --label project=$NS --driver overlay $NS
 
-docker secret create -l $NS trusted-certs.pem $CERTS/trusted-certs.pem
+docker secret create -l project=$NS trusted-certs.pem $CERTS/trusted-certs.pem
 
 echo
 echo Deploying Influx DB and Grafana ...
-docker service create -l $NS --detach --name influxdb --network $NS \
+docker service create $CREATE_OPTIONS --name influxdb \
   --env INFLUXDB_ADMIN_ENABLED=true \
   --env INFLUXDB_GRAPHITE_0_ENABLED=true \
   --env INFLUXDB_GRAPHITE_0_TEMPLATES='*.counter.hono.messaging.receivers.upstream.links.* host.measurement.measurement.measurement.measurement.measurement.measurement.type.tenant.measurement*, *.counter.hono.messaging.senders.downstream.* host.measurement.measurement.measurement.measurement.measurement.type.tenant.measurement*, *.gauge.hono.messaging.link.downstream.credits.* host.measurement.measurement.measurement.measurement.measurement.measurement.type.tenant, *.counter.hono.messaging.messages.* host.measurement.measurement.measurement.measurement.type.tenant.measurement*, *.meter.hono.messaging.messages.* host.measurement.measurement.measurement.measurement.type.tenant.measurement*, host.measurement*' \
   influxdb:${influxdb.version}
-docker service create -l $NS --detach --name grafana --network $NS -p 3000:3000 eclipsehono/grafana:${project.version}
+docker service create $CREATE_OPTIONS --name grafana -p 3000:3000 eclipsehono/grafana:${project.version}
 echo ... done
 
 echo
 echo Deploying Artemis broker ...
-docker service create -l $NS --detach --name hono-artemis --network $NS eclipsehono/hono-artemis:${project.version}
+docker service create $CREATE_OPTIONS --name hono-artemis eclipsehono/hono-artemis:${project.version}
 echo ... done
 
 echo
 echo Deploying Qpid Dispatch Router ...
-docker secret create -l $NS qdrouter-key.pem $CERTS/qdrouter-key.pem
-docker secret create -l $NS qdrouter-cert.pem $CERTS/qdrouter-cert.pem
-docker secret create -l $NS qdrouterd.json $CONFIG/hono-dispatch-router-jar/qpid/qdrouterd-with-broker.json
-docker secret create -l $NS qdrouter-sasl.conf $CONFIG/hono-dispatch-router-jar/sasl/qdrouter-sasl.conf
-docker secret create -l $NS qdrouterd.sasldb $CONFIG/hono-dispatch-router-jar/sasl/qdrouterd.sasldb
-docker service create -l $NS --detach --name hono-dispatch-router --network $NS -p 15671:5671 -p 15672:5672 \
+docker secret create -l project=$NS qdrouter-key.pem $CERTS/qdrouter-key.pem
+docker secret create -l project=$NS qdrouter-cert.pem $CERTS/qdrouter-cert.pem
+docker secret create -l project=$NS qdrouterd.json $CONFIG/hono-dispatch-router-jar/qpid/qdrouterd-with-broker.json
+docker secret create -l project=$NS qdrouter-sasl.conf $CONFIG/hono-dispatch-router-jar/sasl/qdrouter-sasl.conf
+docker secret create -l project=$NS qdrouterd.sasldb $CONFIG/hono-dispatch-router-jar/sasl/qdrouterd.sasldb
+docker service create $CREATE_OPTIONS --name hono-dispatch-router -p 15671:5671 -p 15672:5672 \
   --secret qdrouter-key.pem \
   --secret qdrouter-cert.pem \
   --secret trusted-certs.pem \
@@ -58,10 +59,10 @@ echo ... done
 
 echo
 echo Deploying Authentication Server ...
-docker secret create -l $NS auth-server-key.pem $CERTS/auth-server-key.pem
-docker secret create -l $NS auth-server-cert.pem $CERTS/auth-server-cert.pem
-docker secret create -l $NS hono-service-auth-config.yml $CONFIG/hono-service-auth-config.yml
-docker service create -l $NS --detach --name hono-service-auth --network $NS \
+docker secret create -l project=$NS auth-server-key.pem $CERTS/auth-server-key.pem
+docker secret create -l project=$NS auth-server-cert.pem $CERTS/auth-server-cert.pem
+docker secret create -l project=$NS hono-service-auth-config.yml $CONFIG/hono-service-auth-config.yml
+docker service create $CREATE_OPTIONS --name hono-service-auth \
   --secret auth-server-key.pem \
   --secret auth-server-cert.pem \
   --secret trusted-certs.pem \
@@ -74,10 +75,10 @@ echo ... done
 
 echo
 echo Deploying Device Registry ...
-docker secret create -l $NS device-registry-key.pem $CERTS/device-registry-key.pem
-docker secret create -l $NS device-registry-cert.pem $CERTS/device-registry-cert.pem
-docker secret create -l $NS hono-service-device-registry-config.yml $CONFIG/hono-service-device-registry-config.yml
-docker service create -l $NS --detach --name hono-service-device-registry --network $NS \
+docker secret create -l project=$NS device-registry-key.pem $CERTS/device-registry-key.pem
+docker secret create -l project=$NS device-registry-cert.pem $CERTS/device-registry-cert.pem
+docker secret create -l project=$NS hono-service-device-registry-config.yml $CONFIG/hono-service-device-registry-config.yml
+docker service create $CREATE_OPTIONS --name hono-service-device-registry -p 25671:5671 \
   --secret device-registry-key.pem \
   --secret device-registry-cert.pem \
   --secret auth-server-cert.pem \
@@ -91,10 +92,10 @@ echo ... done
 
 echo
 echo Deploying Hono Messaging ...
-docker secret create -l $NS hono-messaging-key.pem $CERTS/hono-messaging-key.pem
-docker secret create -l $NS hono-messaging-cert.pem $CERTS/hono-messaging-cert.pem
-docker secret create -l $NS hono-service-messaging-config.yml $CONFIG/hono-service-messaging-config.yml
-docker service create -l $NS --detach --name hono-service-messaging --network $NS \
+docker secret create -l project=$NS hono-messaging-key.pem $CERTS/hono-messaging-key.pem
+docker secret create -l project=$NS hono-messaging-cert.pem $CERTS/hono-messaging-cert.pem
+docker secret create -l project=$NS hono-service-messaging-config.yml $CONFIG/hono-service-messaging-config.yml
+docker service create $CREATE_OPTIONS --name hono-service-messaging -p 5671:5671 \
   --secret hono-messaging-key.pem \
   --secret hono-messaging-cert.pem \
   --secret auth-server-cert.pem \
@@ -108,10 +109,10 @@ echo ... done
 
 echo
 echo Deploying HTTP REST adapter ...
-docker secret create -l $NS rest-adapter-key.pem $CERTS/rest-adapter-key.pem
-docker secret create -l $NS rest-adapter-cert.pem $CERTS/rest-adapter-cert.pem
-docker secret create -l $NS hono-adapter-rest-vertx-config.yml $CONFIG/hono-adapter-rest-vertx-config.yml
-docker service create -l $NS --detach --name hono-adapter-rest-vertx --network $NS -p 8080:8080 -p 8443:8443 \
+docker secret create -l project=$NS rest-adapter-key.pem $CERTS/rest-adapter-key.pem
+docker secret create -l project=$NS rest-adapter-cert.pem $CERTS/rest-adapter-cert.pem
+docker secret create -l project=$NS hono-adapter-rest-vertx-config.yml $CONFIG/hono-adapter-rest-vertx-config.yml
+docker service create $CREATE_OPTIONS --name hono-adapter-rest-vertx -p 8080:8080 -p 8443:8443 \
   --secret rest-adapter-key.pem \
   --secret rest-adapter-cert.pem \
   --secret trusted-certs.pem \
@@ -124,10 +125,10 @@ echo ... done
 
 echo
 echo Deploying MQTT adapter ...
-docker secret create -l $NS mqtt-adapter-key.pem $CERTS/mqtt-adapter-key.pem
-docker secret create -l $NS mqtt-adapter-cert.pem $CERTS/mqtt-adapter-cert.pem
-docker secret create -l $NS hono-adapter-mqtt-vertx-config.yml $CONFIG/hono-adapter-mqtt-vertx-config.yml
-docker service create -l $NS --detach --name hono-adapter-mqtt-vertx --network $NS -p 1883:1883 -p 8883:8883 \
+docker secret create -l project=$NS mqtt-adapter-key.pem $CERTS/mqtt-adapter-key.pem
+docker secret create -l project=$NS mqtt-adapter-cert.pem $CERTS/mqtt-adapter-cert.pem
+docker secret create -l project=$NS hono-adapter-mqtt-vertx-config.yml $CONFIG/hono-adapter-mqtt-vertx-config.yml
+docker service create $CREATE_OPTIONS --name hono-adapter-mqtt-vertx -p 1883:1883 -p 8883:8883 \
   --secret mqtt-adapter-key.pem \
   --secret mqtt-adapter-cert.pem \
   --secret trusted-certs.pem \
