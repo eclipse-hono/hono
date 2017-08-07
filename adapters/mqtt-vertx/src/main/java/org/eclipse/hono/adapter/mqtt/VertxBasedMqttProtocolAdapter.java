14f467a14aa587c891988b8866550ed4d2e31ff3
Karsten Frank <Karsten.Frank@bosch-si.com>
2017-08-07 11:04:27 +0200

    [#261] Authentication of devices in MQTT protocol adapter configurable.
    For test setups, the authentication of devices can be switched off by
    a new property.
    
    Signed-off-by: Karsten Frank <Karsten.Frank@bosch-si.com>

--- adapters/mqtt-vertx/src/main/java/org/eclipse/hono/adapter/mqtt/VertxBasedMqttProtocolAdapter.java
+++ adapters/mqtt-vertx/src/main/java/org/eclipse/hono/adapter/mqtt/VertxBasedMqttProtocolAdapter.java
@@ -41,7 +41,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 /**
  * A Vert.x based Hono protocol adapter for accessing Hono's Telemetry API using MQTT.
  */
-public class VertxBasedMqttProtocolAdapter extends AbstractProtocolAdapterBase<ServiceConfigProperties> {
+public class VertxBasedMqttProtocolAdapter extends AbstractProtocolAdapterBase<MqttProtocolAdapterProperties> {
 
     private static final Logger LOG = LoggerFactory.getLogger(VertxBasedMqttProtocolAdapter.class);
 
@@ -205,28 +205,33 @@ public class VertxBasedMqttProtocolAdapter extends AbstractProtocolAdapterBase<S
                     LOG.trace("removed registration assertion for client [{}]", endpoint.clientIdentifier());
             });
 
-            // check credentials for valid authentication
-            // so far, only hashed-password supported, more to follow
-            try {
+            if (getConfig().isAuthenticateDevices()) {
 
-                MqttUsernamePassword authObject = MqttUsernamePassword.create(endpoint,
-                        getConfig().isSingleTenant());
+                // check credentials for valid authentication
+                // so far, only hashed-password supported, more to follow
+                try {
 
-                Future<Void> validationTracker = Future.future();
-                validationTracker.setHandler(result -> {
-                    if (result.failed()) {
-                        endpoint.reject(MqttConnectReturnCode.CONNECTION_REFUSED_NOT_AUTHORIZED);
-                    }
-                });
-                validateCredentialsForDevice(authObject.getTenantId(), authObject.getType(), authObject.getAuthId(),
-                        authObject.getPassword()).compose(
-                                deviceId -> {
-                                    LOG.trace("successfully authenticated device id <{}>", deviceId);
-                                    endpoint.accept(false);
-                                }, validationTracker);
-            } catch (IllegalArgumentException e) {
-                LOG.warn(e.getMessage());
-                endpoint.reject(MqttConnectReturnCode.CONNECTION_REFUSED_BAD_USER_NAME_OR_PASSWORD);
+                    MqttUsernamePassword authObject = MqttUsernamePassword.create(endpoint,
+                            getConfig().isSingleTenant());
+
+                    Future<Void> validationTracker = Future.future();
+                    validationTracker.setHandler(result -> {
+                        if (result.failed()) {
+                            endpoint.reject(MqttConnectReturnCode.CONNECTION_REFUSED_NOT_AUTHORIZED);
+                        }
+                    });
+                    validateCredentialsForDevice(authObject.getTenantId(), authObject.getType(), authObject.getAuthId(),
+                            authObject.getPassword()).compose(
+                            deviceId -> {
+                                LOG.trace("successfully authenticated device id <{}>", deviceId);
+                                endpoint.accept(false);
+                            }, validationTracker);
+                } catch (IllegalArgumentException e) {
+                    LOG.warn(e.getMessage());
+                    endpoint.reject(MqttConnectReturnCode.CONNECTION_REFUSED_BAD_USER_NAME_OR_PASSWORD);
+                }
+            } else {
+                endpoint.accept(false);
             }
         }
     }
