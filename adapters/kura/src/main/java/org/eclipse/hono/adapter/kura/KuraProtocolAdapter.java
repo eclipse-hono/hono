1c91f1af741e2e2523fce7f3b63b0ea6005f6b81
Kai Hudalla <kai.hudalla@bosch-si.com>
2017-10-31 18:28:10 +0100

    Add Eclipse Kura protocol adapter.
    The adapter can be used to publish messages from Eclipse Kura gateways
    to Hono's Telemetry and Event APIs.
    
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

---
+++ adapters/kura/src/main/java/org/eclipse/hono/adapter/kura/KuraProtocolAdapter.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2017 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 1.0 which is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * SPDX-License-Identifier: EPL-1.0
+ */
+
+package org.eclipse.hono.adapter.kura;
+
+import java.util.Arrays;
+
+import org.apache.qpid.proton.message.Message;
+import org.eclipse.hono.adapter.mqtt.AbstractVertxBasedMqttProtocolAdapter;
+import org.eclipse.hono.service.auth.device.Device;
+import org.eclipse.hono.util.ResourceIdentifier;
+
+import io.netty.handler.codec.mqtt.MqttQoS;
+import io.vertx.core.Future;
+import io.vertx.mqtt.messages.MqttPublishMessage;
+
+/**
+ * A Vert.x based Hono protocol adapter for publishing messages to Hono's Telemetry and Event APIs from
+ * <a href="https://www.eclipse.org/kura">Eclipse Kura</a> gateways.
+ */
+public final class KuraProtocolAdapter extends AbstractVertxBasedMqttProtocolAdapter<KuraAdapterProperties> {
+
+    @Override
+    protected Future<Message> getDownstreamMessage(final MqttPublishMessage messageFromDevice) {
+
+        return getDownstreamMessage(messageFromDevice, null);
+    }
+
+    @Override
+    protected Future<Message> getDownstreamMessage(final MqttPublishMessage messageFromDevice, final Device deviceIdentity) {
+
+        final Future<Message> result = Future.future();
+        try {
+            final ResourceIdentifier topic = ResourceIdentifier.fromString(messageFromDevice.topicName());
+            ResourceIdentifier mappedTopic = null;
+            String contentType = null;
+
+            if (getConfig().getControlPrefix().equals(topic.getEndpoint())) {
+
+                // this is a "control" message
+                contentType = getConfig().getCtrlMsgContentType();
+                final String[] mappedPath = Arrays.copyOf(topic.getResourcePath(), topic.getResourcePath().length);
+                mappedPath[0] = getEndpoint(messageFromDevice.qosLevel());
+                mappedTopic = ResourceIdentifier.fromPath(mappedPath);
+
+            } else {
+
+                // map "data" messages based on QoS
+                contentType = getConfig().getDataMsgContentType();
+                final String[] mappedPath = new String[topic.getResourcePath().length + 1];
+                System.arraycopy(topic.getResourcePath(), 0, mappedPath, 1, topic.getResourcePath().length);
+                mappedPath[0] = getEndpoint(messageFromDevice.qosLevel());
+                mappedTopic = ResourceIdentifier.fromPath(mappedPath);
+            }
+
+            if (mappedTopic.getResourcePath().length < 3) {
+                // topic does not contain account_name and client_id
+                result.fail(new IllegalArgumentException("topic does not comply with Kura format"));
+            } else {
+                LOG.debug("mapped Kura message [topic: {}, QoS: {}] to Hono message [to: {}, device_id: {}, content-type: {}]",
+                        topic, messageFromDevice.qosLevel(), mappedTopic.getBasePath(), mappedTopic.getResourceId(), contentType);
+                result.complete(newMessage(mappedTopic.getBasePath(), mappedTopic.getResourceId(), contentType));
+            }
+        } catch (IllegalArgumentException e) {
+            result.fail(e);
+        }
+        return result;
+    }
+
+    private static String getEndpoint(final MqttQoS level) {
+
+        switch(level) {
+        case AT_MOST_ONCE:
+            return AbstractVertxBasedMqttProtocolAdapter.TELEMETRY_ENDPOINT;
+        default:
+            return AbstractVertxBasedMqttProtocolAdapter.EVENT_ENDPOINT;
+        }
+    }
+}
---
+++ adapters/kura/src/test/java/org/eclipse/hono/adapter/kura/KuraProtocolAdapterTest.java
@@ -0,0 +1,198 @@
+/**
+ * Copyright (c) 2017 Bosch Software Innovations GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Bosch Software Innovations GmbH - initial creation
+ */
+
+package org.eclipse.hono.adapter.kura;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.apache.qpid.proton.message.Message;
+import org.eclipse.hono.util.MessageHelper;
+import org.eclipse.hono.util.ResourceIdentifier;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.netty.handler.codec.mqtt.MqttQoS;
+import io.vertx.core.Future;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.mqtt.messages.MqttPublishMessage;
+
+/**
+ * Verifies behavior of {@link KuraProtocolAdapter}.
+ * 
+ */
+@RunWith(VertxUnitRunner.class)
+public class KuraProtocolAdapterTest {
+
+    private KuraAdapterProperties config;
+    private KuraProtocolAdapter adapter;
+
+    /**
+     * Sets up the fixture.
+     */
+    @Before
+    public void setUp() {
+
+        config = new KuraAdapterProperties();
+        adapter = new KuraProtocolAdapter();
+        adapter.setConfig(config);
+    }
+
+    /**
+     * Verifies that the adapter maps control messages with QoS 1 published from a Kura gateway to
+     * the Event endpoint.
+     * 
+     * @param ctx The helper to use for running tests on vert.x.
+     */
+    @Test
+    public void testGetDownstreamMessageMapsKuraControlMessagesToEventApi(final TestContext ctx) {
+
+        // GIVEN an adapter configured to use the standard topic.control-prefix $EDC
+
+        // WHEN a message is published to a topic with the Kura $EDC prefix as endpoint
+        final MqttPublishMessage message = newMessage(MqttQoS.AT_LEAST_ONCE, "$EDC/my-scope/4711");
+        final Async determineAddressSuccess = ctx.async();
+        Future<Message> msgTracker = adapter.getDownstreamMessage(message).map(msg -> {
+            determineAddressSuccess.complete();
+            return msg;
+        });
+
+        // THEN the message is forwarded to the event API
+        determineAddressSuccess.await(2000);
+        assertMessageProperties(msgTracker.result(), config.getCtrlMsgContentType(),
+                KuraProtocolAdapter.EVENT_ENDPOINT, "my-scope", "4711");
+    }
+
+    /**
+     * Verifies that the adapter maps control messages with QoS 0 published from a Kura gateway to
+     * the Telemetry endpoint.
+     * 
+     * @param ctx The helper to use for running tests on vert.x.
+     */
+    @Test
+    public void testGetDownstreamMessageMapsKuraControlMessagesToTelemetryApi(final TestContext ctx) {
+
+        // GIVEN an adapter configured to use the standard topic.control-prefix $EDC
+        // and a custom control message content type
+        config.setCtrlMsgContentType("control-msg");
+
+        // WHEN a message is published to a topic with the Kura $EDC prefix as endpoint
+        final MqttPublishMessage message = newMessage(MqttQoS.AT_MOST_ONCE, "$EDC/my-scope/4711");
+        final Async determineAddressSuccess = ctx.async();
+        Future<Message> msgTracker = adapter.getDownstreamMessage(message).map(msg -> {
+            determineAddressSuccess.complete();
+            return msg;
+        });
+
+        // THEN the message is forwarded to the telemetry API
+        // and has the custom control message content type
+        determineAddressSuccess.await(2000);
+        assertMessageProperties(msgTracker.result(), config.getCtrlMsgContentType(),
+                KuraProtocolAdapter.TELEMETRY_ENDPOINT, "my-scope", "4711");
+    }
+
+    /**
+     * Verifies that the adapter recognizes control messages published to a topic with a custom control prefix.
+     * 
+     * @param ctx The helper to use for running tests on vert.x.
+     */
+    @Test
+    public void testGetDownstreamMessageRecognizesControlMessagesWithCustomControlPrefix(final TestContext ctx) {
+
+        // GIVEN an adapter configured to use a custom topic.control-prefix
+        config.setControlPrefix("bumlux");
+
+        // WHEN a message is published to a topic with the custom prefix as endpoint
+        final MqttPublishMessage message = newMessage(MqttQoS.AT_MOST_ONCE, "bumlux/my-scope/4711");
+        final Async determineAddressSuccess = ctx.async();
+        Future<Message> msgTracker = adapter.getDownstreamMessage(message).map(msg -> {
+            determineAddressSuccess.complete();
+            return msg;
+        });
+
+        // THEN the message is recognized as a control message and forwarded to the event API
+        determineAddressSuccess.await(2000);
+        assertMessageProperties(msgTracker.result(), config.getCtrlMsgContentType(),
+                KuraProtocolAdapter.TELEMETRY_ENDPOINT, "my-scope", "4711");
+    }
+
+    /**
+     * Verifies that the adapter forwards data messages with QoS 0 published from a Kura gateway to
+     * the Telemetry endpoint.
+     * 
+     * @param ctx The helper to use for running tests on vert.x.
+     */
+    @Test
+    public void testGetDownstreamMessageMapsKuraDataMessagesToTelemetryApi(final TestContext ctx) {
+
+        // GIVEN an adapter configured with a custom data message content type
+        config.setDataMsgContentType("data-msg");
+
+        // WHEN a message is published to an application topic with QoS 0
+        final MqttPublishMessage message = newMessage(MqttQoS.AT_MOST_ONCE, "my-scope/4711");
+        final Async determineAddressSuccess = ctx.async();
+        Future<Message> msgTracker = adapter.getDownstreamMessage(message).map(msg -> {
+            determineAddressSuccess.complete();
+            return msg;
+        });
+
+        // THEN the message is forwarded to the telemetry API
+        // and has the configured data message content type
+        determineAddressSuccess.await(2000);
+        assertMessageProperties(msgTracker.result(), config.getDataMsgContentType(),
+                KuraProtocolAdapter.TELEMETRY_ENDPOINT, "my-scope", "4711");
+    }
+
+    /**
+     * Verifies that the adapter forwards application messages with QoS 1 published from a Kura gateway to
+     * the Event endpoint.
+     * 
+     * @param ctx The helper to use for running tests on vert.x.
+     */
+    @Test
+    public void testGetDownstreamMessageMapsKuraDataMessagesToEventApi(final TestContext ctx) {
+
+        // GIVEN an adapter
+
+        // WHEN a message is published to an application topic with QoS 1
+        final MqttPublishMessage message = newMessage(MqttQoS.AT_LEAST_ONCE, "my-scope/4711");
+        final Async determineAddressSuccess = ctx.async();
+        Future<Message> msgTracker = adapter.getDownstreamMessage(message).map(msg -> {
+            determineAddressSuccess.complete();
+            return msg;
+        });
+
+        // THEN the message is forwarded to the event API
+        determineAddressSuccess.await(2000);
+        assertMessageProperties(msgTracker.result(), config.getDataMsgContentType(),
+                KuraProtocolAdapter.EVENT_ENDPOINT, "my-scope", "4711");
+    }
+
+    private void assertMessageProperties(final Message msg, final String contentType, final String endpoint, String tenantId, final String deviceId) {
+        assertThat(msg.getContentType(), is(contentType));
+        ResourceIdentifier address = ResourceIdentifier.fromString(msg.getAddress());
+        assertThat(address.getEndpoint(), is(endpoint));
+        assertThat(MessageHelper.getDeviceId(msg), is(deviceId));
+    }
+
+    private static MqttPublishMessage newMessage(final MqttQoS qosLevel, final String topic) {
+        final MqttPublishMessage message = mock(MqttPublishMessage.class);
+        when(message.qosLevel()).thenReturn(qosLevel);
+        when(message.topicName()).thenReturn(topic);
+        return message;
+    }
+}
