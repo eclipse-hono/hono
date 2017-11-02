eclipse-hono/hono

commit aa80ce70fbea9070ff6013fe48acde30b81dc915

Refactor MQTT adapter hierarchy to better support custom adapters.
MQTT adapters now map MQTT messages to AMQP messages directly, thus
allowing them to not only map the original topic to a Hono address but
also to specify the content type and set additional properties.

Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

adapters/mqtt-vertx/src/main/java/org/eclipse/hono/adapter/mqtt/VertxBasedMqttProtocolAdapter.java
@@ -8,93 +8,102 @@
  *
  * Contributors:
  *    Red Hat - initial creation
+ *    Bosch Software Innovations GmbH - add Eclipse Kura support
  */
 
 package org.eclipse.hono.adapter.mqtt;
 
-import org.eclipse.hono.client.MessageSender;
+import org.apache.qpid.proton.message.Message;
 import org.eclipse.hono.config.ProtocolAdapterProperties;
 import org.eclipse.hono.service.auth.device.Device;
 import org.eclipse.hono.util.ResourceIdentifier;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import io.netty.handler.codec.mqtt.MqttQoS;
 import io.vertx.core.Future;
 import io.vertx.mqtt.messages.MqttPublishMessage;
 
 /**
- * A Vert.x based Hono protocol adapter for accessing Hono's Telemetry API using MQTT.
+ * A Vert.x based Hono protocol adapter for publishing messages to Hono's Telemetry and Event APIs using MQTT.
  */
 public final class VertxBasedMqttProtocolAdapter extends AbstractVertxBasedMqttProtocolAdapter<ProtocolAdapterProperties> {
 
-    /**
-     * Gets a sender for forwarding a message that has been published by an unauthenticated device.
-     * <p>
-     * Verifies that topic that the message has been published to has
-     * contains a <em>tenantId</em> and a <em>deviceId</em>.
-     * If it does, a sender is returned that corresponds to the <em>endpoint</em> of the topic.
-     * Otherwise the returned future is failed.
-     * 
-     * @param message The message to get the sender for.
-     * @param topic The topic that the message has been published to.
-     * @return A future containing the sender.
-     */
-    protected Future<MessageSender> getSender(final MqttPublishMessage message, final ResourceIdentifier topic) {
-
-        if (topic.getTenantId() == null || topic.getResourceId() == null) {
-            LOG.debug("discarding message published to unsupported topic [{}]", message.topicName());
-            return Future.failedFuture("unsupported topic name");
-        } else {
-            return getSenderForTopic(message.qosLevel(), topic.getEndpoint(), topic.getTenantId());
-        }
-    }
+    private MqttAdapterMetrics metrics;
 
     /**
-     * Gets a sender for forwarding a message that has been published by an authenticated device.
-     * <p>
-     * Verifies that the <em>tenantId</em> and <em>deviceId</em> contained
-     * in the topic match the authenticated device by means of the
-     * {@link #validateCredentialsWithTopicStructure(ResourceIdentifier, String, String)} method.
-     * If they do, a sender is returned that corresponds to the <em>endpoint</em> of the topic.
-     * Otherwise the returned future is failed.
-     * 
-     * @param message The message to get the sender for.
-     * @param topic The topic that the message has been published to.
-     * @param authenticatedDevice The authenticated device that has published the message.
-     * @return A future containing the sender.
+     * Sets the metrics for this service
+     *
+     * @param metrics The metrics
      */
-    protected Future<MessageSender> getSender(final MqttPublishMessage message, final ResourceIdentifier topic,
-            final Device authenticatedDevice) {
-
-        if (validateCredentialsWithTopicStructure(topic, authenticatedDevice.getTenantId(), authenticatedDevice.getDeviceId())) {
-            return getSenderForTopic(message.qosLevel(), topic.getEndpoint(), authenticatedDevice.getTenantId());
-        } else {
-            LOG.debug("discarding message published by device [tenant-id: {}, device-id: {}] to unauthorized topic [{}]",
-                    authenticatedDevice.getTenantId(), authenticatedDevice.getDeviceId(), message.topicName());
-            return Future.failedFuture("unauthorized ");
-        }
+    @Autowired
+    public final void setMetrics(final MqttAdapterMetrics metrics) {
+        this.metrics = metrics;
     }
 
-    private Future<MessageSender> getSenderForTopic(final MqttQoS qosLevel, final String endpoint, final String tenantId) {
-
-        if (endpoint.equals(TELEMETRY_ENDPOINT)) {
-            if (!MqttQoS.AT_MOST_ONCE.equals(qosLevel)) {
-                // client tries to send telemetry message using QoS 1 or 2
-                return Future.failedFuture("Only QoS 0 supported for telemetry messages");
-            } else {
-                return getTelemetrySender(tenantId);
-            }
-        } else if (endpoint.equals(EVENT_ENDPOINT)) {
-            if (!MqttQoS.AT_LEAST_ONCE.equals(qosLevel)) {
-                // client tries to send event message using QoS 0 or 2
-                return Future.failedFuture("Only QoS 1 supported for event messages");
-            } else {
-                return getEventSender(tenantId);
-            }
-        } else {
-            // MQTT client is trying to publish on a not supported endpoint
-            LOG.debug("no such endpoint [{}]", endpoint);
-            return Future.failedFuture("no such endpoint");
-        }
+    @Override
+    protected void onMessageSent(final ResourceIdentifier downstreamAddress) {
+        metrics.incrementProcessedMqttMessages(downstreamAddress.getEndpoint(), downstreamAddress.getTenantId());
     }
 
+    @Override
+    protected void onMessageUndeliverable(final ResourceIdentifier downstreamAddress) {
+        metrics.incrementUndeliverableMqttMessages(downstreamAddress.getEndpoint(), downstreamAddress.getTenantId());
+    }
+
+    @Override
+    protected Future<Message> getDownstreamMessage(final MqttPublishMessage messageFromDevice) {
+
+        return mapTopic(messageFromDevice).map(address -> {
+            if (address.getTenantId() == null || address.getResourceId() == null) {
+                throw new IllegalArgumentException("topic of unauthenticated message must contain tenant and device IDs");
+            } else {
+                return newMessage(address.getBasePath(), CONTENT_TYPE_OCTET_STREAM);
+            }
+        });
+    }
+
+    @Override
+    protected Future<Message> getDownstreamMessage(final MqttPublishMessage messageFromDevice, final Device deviceIdentity) {
+
+        return mapTopic(messageFromDevice).map(address -> {
+            if (address.getTenantId() != null && address.getResourceId() == null) {
+                throw new IllegalArgumentException("topic of authenticated message must not contain tenant ID only");
+            } else if (address.getTenantId() == null && address.getResourceId() == null) {
+                // use authenticated device's tenant to fill in missing information
+                final ResourceIdentifier downstreamAddress = ResourceIdentifier.from(address.getEndpoint(),
+                        deviceIdentity.getTenantId(), deviceIdentity.getDeviceId());
+                return newMessage(downstreamAddress.getBasePath(), CONTENT_TYPE_OCTET_STREAM);
+            } else {
+                return newMessage(address.getBasePath(), CONTENT_TYPE_OCTET_STREAM);
+            }
+        });
+    }
+
+    private Future<ResourceIdentifier> mapTopic(final MqttPublishMessage message) {
+
+        try {
+            final ResourceIdentifier topic = ResourceIdentifier.fromString(message.topicName());
+            if (TELEMETRY_ENDPOINT.equals(topic.getEndpoint())) {
+                if (!MqttQoS.AT_MOST_ONCE.equals(message.qosLevel())) {
+                    // client tries to send telemetry message using QoS 1 or 2
+                    return Future.failedFuture("Only QoS 0 supported for telemetry messages");
+                } else {
+                    return Future.succeededFuture(topic);
+                }
+            } else if (EVENT_ENDPOINT.equals(topic.getEndpoint())) {
+                if (!MqttQoS.AT_LEAST_ONCE.equals(message.qosLevel())) {
+                    // client tries to send event message using QoS 0 or 2
+                    return Future.failedFuture("Only QoS 1 supported for event messages");
+                } else {
+                    return Future.succeededFuture(topic);
+                }
+            } else {
+                // MQTT client is trying to publish on a not supported endpoint
+                LOG.debug("no such endpoint [{}]", topic.getEndpoint());
+                return Future.failedFuture("no such endpoint");
+            }
+        } catch (IllegalArgumentException e) {
+            return Future.failedFuture(e);
+        }
+    }
 }

adapters/mqtt-vertx/src/test/java/org/eclipse/hono/adapter/mqtt/VertxBasedMqttProtocolAdapterTest.java
@@ -12,10 +12,16 @@
 
 package org.eclipse.hono.adapter.mqtt;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.apache.qpid.proton.message.Message;
+import org.eclipse.hono.config.ProtocolAdapterProperties;
+import org.eclipse.hono.service.auth.device.Device;
 import org.eclipse.hono.util.ResourceIdentifier;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -33,6 +39,20 @@ import io.vertx.mqtt.messages.MqttPublishMessage;
 @RunWith(VertxUnitRunner.class)
 public class VertxBasedMqttProtocolAdapterTest {
 
+    private ProtocolAdapterProperties config;
+    private VertxBasedMqttProtocolAdapter adapter;
+
+    /**
+     * Sets up the fixture.
+     */
+    @Before
+    public void setUp() {
+
+        config = new ProtocolAdapterProperties();
+        adapter = new VertxBasedMqttProtocolAdapter();
+        adapter.setConfig(config);
+    }
+
     /**
      * Verifies that the adapter rejects messages published to topics containing an endpoint
      * other than <em>telemetry</em> or <em>event</em>.
@@ -40,21 +60,19 @@ public class VertxBasedMqttProtocolAdapterTest {
      * @param ctx The helper to use for running tests on vert.x.
      */
     @Test
-    public void testGetSenderFailsForUnsupportedEndpoint(final TestContext ctx) {
+    public void testGetDownstreamMessageFailsForUnknownEndpoint(final TestContext ctx) {
 
         // GIVEN an adapter
-        VertxBasedMqttProtocolAdapter adapter = new VertxBasedMqttProtocolAdapter();
 
-        // WHEN a message is published to topic "unknown"
-        final MqttPublishMessage message = mock(MqttPublishMessage.class);
-        final ResourceIdentifier topic = ResourceIdentifier.from("unknown", "tenant", "4711");
-        final Async determineSenderFailure = ctx.async();
-        adapter.getSender(message, topic).recover(t -> {
-            determineSenderFailure.complete();
+        // WHEN a device publishes a message to a topic with an unknown endpoint
+        final MqttPublishMessage message = newMessage(MqttQoS.AT_MOST_ONCE, "unknown");
+        final Async determineAddressFailure = ctx.async();
+        adapter.getDownstreamMessage(message).recover(t -> {
+            determineAddressFailure.complete();
             return Future.failedFuture(t);
         });
         // THEN no downstream sender can be created for the message
-        determineSenderFailure.await(2000);
+        determineAddressFailure.await(2000);
     }
 
     /**
@@ -63,22 +81,19 @@ public class VertxBasedMqttProtocolAdapterTest {
      * @param ctx The helper to use for running tests on vert.x.
      */
     @Test
-    public void testGetSenderFailsForQoS1TelemetryMessage(final TestContext ctx) {
+    public void testGetDownstreamMessageFailsForQoS1TelemetryMessage(final TestContext ctx) {
 
         // GIVEN an adapter
-        VertxBasedMqttProtocolAdapter adapter = new VertxBasedMqttProtocolAdapter();
 
-        // WHEN a message is published to endpoint "telemetry" with QoS 1
-        final MqttPublishMessage message = mock(MqttPublishMessage.class);
-        when(message.qosLevel()).thenReturn(MqttQoS.AT_LEAST_ONCE);
-        final ResourceIdentifier topic = ResourceIdentifier.from(VertxBasedMqttProtocolAdapter.TELEMETRY_ENDPOINT, "tenant", "4711");
-        final Async determineSenderFailure = ctx.async();
-        adapter.getSender(message, topic).recover(t -> {
-            determineSenderFailure.complete();
+        // WHEN a device publishes a message with QoS 1 to a "telemetry" topic
+        final MqttPublishMessage message = newMessage(MqttQoS.AT_LEAST_ONCE, AbstractVertxBasedMqttProtocolAdapter.TELEMETRY_ENDPOINT);
+        final Async determineAddressFailure = ctx.async();
+        adapter.getDownstreamMessage(message).recover(t -> {
+            determineAddressFailure.complete();
             return Future.failedFuture(t);
         });
         // THEN no downstream sender can be created for the message
-        determineSenderFailure.await(2000);
+        determineAddressFailure.await(2000);
     }
 
     /**
@@ -87,21 +102,94 @@ public class VertxBasedMqttProtocolAdapterTest {
      * @param ctx The helper to use for running tests on vert.x.
      */
     @Test
-    public void testGetSenderFailsForQoS0EventMessage(final TestContext ctx) {
+    public void testGetDownstreamMessageFailsForQoS0EventMessage(final TestContext ctx) {
 
         // GIVEN an adapter
-        VertxBasedMqttProtocolAdapter adapter = new VertxBasedMqttProtocolAdapter();
 
-        // WHEN a message is published to endpoint "event" with QoS 0
-        final MqttPublishMessage message = mock(MqttPublishMessage.class);
-        when(message.qosLevel()).thenReturn(MqttQoS.AT_MOST_ONCE);
-        final ResourceIdentifier topic = ResourceIdentifier.from(VertxBasedMqttProtocolAdapter.EVENT_ENDPOINT, "tenant", "4711");
-        final Async determineSenderFailure = ctx.async();
-        adapter.getSender(message, topic).recover(t -> {
-            determineSenderFailure.complete();
+        // WHEN a device publishes a message with QoS 0 to an "event" topic
+        final MqttPublishMessage message = newMessage(MqttQoS.AT_MOST_ONCE, AbstractVertxBasedMqttProtocolAdapter.EVENT_ENDPOINT);
+        final Async messageFailure = ctx.async();
+        adapter.getDownstreamMessage(message).recover(t -> {
+            messageFailure.complete();
             return Future.failedFuture(t);
         });
         // THEN no downstream sender can be created for the message
-        determineSenderFailure.await(2000);
+        messageFailure.await(2000);
+    }
+
+    /**
+     * Verifies that the adapter fails to map a topic without a tenant ID received from an anonymous device.
+     * 
+     * @param ctx The helper to use for running tests on vert.x.
+     */
+    @Test
+    public void testGetDownstreamMessageFailsForMissingTenant(final TestContext ctx) {
+
+        // GIVEN an adapter
+
+        // WHEN an anonymous device publishes a message to a topic that does not contain a tenant ID
+        final MqttPublishMessage message = newMessage(MqttQoS.AT_MOST_ONCE, VertxBasedMqttProtocolAdapter.TELEMETRY_ENDPOINT);
+        final Async determineAddressFailure = ctx.async();
+        adapter.getDownstreamMessage(message).recover(t -> {
+            determineAddressFailure.complete();
+            return Future.failedFuture(t);
+        });
+
+        // THEN the message cannot be mapped to an address
+        determineAddressFailure.await(2000);
+    }
+
+    /**
+     * Verifies that the adapter fails to map a topic without a device ID received from an anonymous device.
+     * 
+     * @param ctx The helper to use for running tests on vert.x.
+     */
+    @Test
+    public void testGetDownstreamMessageFailsForMissingDeviceId(final TestContext ctx) {
+
+        // GIVEN an adapter
+
+        // WHEN an anonymous device publishes a message to a topic that does not contain a device ID
+        final MqttPublishMessage message = newMessage(MqttQoS.AT_MOST_ONCE, VertxBasedMqttProtocolAdapter.TELEMETRY_ENDPOINT + "/my-tenant");
+        final Async determineAddressFailure = ctx.async();
+        adapter.getDownstreamMessage(message).recover(t -> {
+            determineAddressFailure.complete();
+            return Future.failedFuture(t);
+        });
+
+        // THEN the message cannot be mapped to an address
+        determineAddressFailure.await(2000);
+    }
+
+    /**
+     * Verifies that the adapter uses an authenticated device's identity when mapping a topic without tenant ID.
+     * 
+     * @param ctx The helper to use for running tests on vert.x.
+     */
+    @Test
+    public void testGetDownstreamMessageUsesDeviceIdentityForTopicWithoutTenant(final TestContext ctx) {
+
+        // GIVEN an adapter
+
+        // WHEN an authenticated device publishes a message to a topic that does not contain a tenant ID
+        final MqttPublishMessage message = newMessage(MqttQoS.AT_MOST_ONCE, VertxBasedMqttProtocolAdapter.TELEMETRY_ENDPOINT);
+        final Async determineAddressSuccess = ctx.async();
+        Future<Message> downstreamMessage = adapter.getDownstreamMessage(message, new Device("my-tenant", "4711")).map(msg -> {
+            determineAddressSuccess.complete();
+            return msg;
+        });
+
+        // THEN the mapped address contains the authenticated device's tenant and device ID
+        determineAddressSuccess.await(2000);
+        final ResourceIdentifier downstreamAddress = ResourceIdentifier.fromString(downstreamMessage.result().getAddress());
+        assertThat(downstreamAddress.getEndpoint(), is(VertxBasedMqttProtocolAdapter.TELEMETRY_ENDPOINT));
+        assertThat(downstreamAddress.getTenantId(), is("my-tenant"));
+    }
+
+    private static MqttPublishMessage newMessage(final MqttQoS qosLevel, final String topic) {
+        final MqttPublishMessage message = mock(MqttPublishMessage.class);
+        when(message.qosLevel()).thenReturn(qosLevel);
+        when(message.topicName()).thenReturn(topic);
+        return message;
     }
 }

commit 2a7d8ee5ff223078134b3babe7b582457e89a94c

Fix authorization of messages published by an authenticated device.
Fixes a regression introduced by
aa80ce70fbea9070ff6013fe48acde30b81dc915

Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

adapters/mqtt-vertx-base/src/main/java/org/eclipse/hono/adapter/mqtt/AbstractVertxBasedMqttProtocolAdapter.java
@@ -377,33 +377,39 @@ public abstract class AbstractVertxBasedMqttProtocolAdapter<T extends ProtocolAd
 
         LOG.trace("received message [topic: {}, QoS: {}] from authenticated device [tenant-id: {}, device-id: {}]",
                 messageFromDevice.topicName(), messageFromDevice.qosLevel(), authenticatedDevice.getTenantId(), authenticatedDevice.getDeviceId());
+
         final Future<Message> mappedMessage = getDownstreamMessage(messageFromDevice, authenticatedDevice);
-        mappedMessage.compose(message -> authorize(message, authenticatedDevice)).compose(authorizedAddress -> {
-            return publishMessage(endpoint, messageFromDevice, mappedMessage.result(), authorizedAddress).map(s -> {
+
+        mappedMessage.compose(message -> authorize(message, authenticatedDevice)).compose(authorizedResource -> {
+
+            return publishMessage(endpoint, messageFromDevice, mappedMessage.result(), authorizedResource).map(s -> {
+
                 LOG.trace("successfully processed message [topic: {}, QoS: {}] from authenticated device [tenantId: {}, deviceId: {}]",
                         messageFromDevice.topicName(), messageFromDevice.qosLevel(), authenticatedDevice.getTenantId(), authenticatedDevice.getDeviceId());
-                onMessageSent(authorizedAddress);
+                onMessageSent(authorizedResource);
                 return (Void) null;
+
             }).recover(f -> {
+
                 LOG.debug("error processing message [topic: {}, QoS: {}] from authenticated device [tenantId: {}, deviceId: {}]: {}",
                         messageFromDevice.topicName(), messageFromDevice.qosLevel(), authenticatedDevice.getTenantId(), authenticatedDevice.getDeviceId(), f.getMessage());
                 if (!ClientErrorException.class.isInstance(f)) {
-                    onMessageUndeliverable(authorizedAddress);
+                    onMessageUndeliverable(authorizedResource);
                 }
                 return Future.failedFuture(f);
             });
         });
     }
 
-    private Future<Void> publishMessage(final MqttEndpoint endpoint, final MqttPublishMessage messageFromDevice, final Message message, final ResourceIdentifier authorizedAddress) {
+    private Future<Void> publishMessage(final MqttEndpoint endpoint, final MqttPublishMessage messageFromDevice, final Message message, final ResourceIdentifier authorizedResource) {
 
-        final Future<String> assertionTracker = getRegistrationAssertion(endpoint, authorizedAddress.getTenantId(), authorizedAddress.getResourceId());
-        final Future<MessageSender> senderTracker = getSenderForEndpoint(authorizedAddress.getEndpoint(), authorizedAddress.getTenantId());
+        final Future<String> assertionTracker = getRegistrationAssertion(endpoint, authorizedResource.getTenantId(), authorizedResource.getResourceId());
+        final Future<MessageSender> senderTracker = getSenderForEndpoint(authorizedResource.getEndpoint(), authorizedResource.getTenantId());
 
         return CompositeFuture.all(assertionTracker, senderTracker).recover(t -> {
             return Future.failedFuture(t);
         }).compose(ok -> {
-            addProperties(messageFromDevice, message, assertionTracker.result(), authorizedAddress.getResourceId());
+            addProperties(messageFromDevice, message, assertionTracker.result());
             return doUploadMessage(message, endpoint, messageFromDevice, senderTracker.result());
         });
     }
@@ -413,24 +419,34 @@ public abstract class AbstractVertxBasedMqttProtocolAdapter<T extends ProtocolAd
      * 
      * @param message The message to authorize.
      * @param authenticatedDevice The authenticated device or {@code null} if the device has not been authenticated.
-     * @return A succeeded future containing the authorized message's address.
+     * @return A succeeded future containing a resource identifier for the authorized message. The future will
+     *         be failed if the message does not contain a <em>device_id</em> application property or has a
+     *         malformed address.
      */
     private Future<ResourceIdentifier> authorize(final Message message, final Device authenticatedDevice) {
 
-        try {
-            final ResourceIdentifier address = ResourceIdentifier.fromString(message.getAddress());
-            if (authenticatedDevice == null) {
-                return Future.succeededFuture(address);
-            } else if (validateCredentialsWithTopicStructure(address, authenticatedDevice.getTenantId(), authenticatedDevice.getDeviceId())) {
-                return Future.succeededFuture(address);
-            } else {
-                LOG.debug("discarding message published by authenticated device [tenant-id: {}, device-id: {}] to unauthorized topic [{}]",
-                        authenticatedDevice.getTenantId(), authenticatedDevice.getDeviceId(), address);
-                return Future.failedFuture("unauthorized ");
+        final String deviceId = MessageHelper.getDeviceId(message);
+        final String to = message.getAddress();
+        if (deviceId == null) {
+            return Future.failedFuture(new IllegalArgumentException("message contains no device_id property"));
+        } else if (to == null) {
+                return Future.failedFuture(new IllegalArgumentException("message has no address"));
+        } else {
+            try {
+                final ResourceIdentifier address = ResourceIdentifier.fromString(to + "/" + deviceId);
+                if (authenticatedDevice == null) {
+                    return Future.succeededFuture(address);
+                } else if (validateCredentialsWithTopicStructure(address, authenticatedDevice.getTenantId(), authenticatedDevice.getDeviceId())) {
+                    return Future.succeededFuture(address);
+                } else {
+                    LOG.debug("discarding message published by authenticated device [tenant-id: {}, device-id: {}] to unauthorized topic [{}]",
+                            authenticatedDevice.getTenantId(), authenticatedDevice.getDeviceId(), address);
+                    return Future.failedFuture(new SecurityException("device is not authorized to publish to topic"));
+                }
+            } catch (IllegalArgumentException e) {
+                LOG.debug("discarding message mapped to malformed address [to: {}]", message.getAddress());
+                return Future.failedFuture(e);
             }
-        } catch (IllegalArgumentException e) {
-            LOG.debug("discarding message mapped to malformed address [to: {}]", message.getAddress());
-            return Future.failedFuture(e);
         }
     }
 
@@ -457,13 +473,12 @@ public abstract class AbstractVertxBasedMqttProtocolAdapter<T extends ProtocolAd
         }
     }
 
-    private void addProperties(final MqttPublishMessage messageFromDevice, final Message message, final String registrationAssertion, final String deviceId) {
+    private void addProperties(final MqttPublishMessage messageFromDevice, final Message message, final String registrationAssertion) {
 
         if (message.getContentType() == null) {
             message.setContentType(CONTENT_TYPE_OCTET_STREAM);
         }
         message.setBody(new Data(new Binary(messageFromDevice.payload().getBytes())));
-        MessageHelper.addDeviceId(message, deviceId);
         MessageHelper.addRegistrationAssertion(message, registrationAssertion);
         MessageHelper.addProperty(message, PROPERTY_HONO_ORIG_ADDRESS, messageFromDevice.topicName());
     }
@@ -530,20 +545,23 @@ public abstract class AbstractVertxBasedMqttProtocolAdapter<T extends ProtocolAd
     }
 
     /**
-     * Creates a new AMQP 1.0 message for an address and content type.
+     * Creates a new AMQP 1.0 message for an address, device ID and content type.
      * 
      * @param address The receiver of the message.
+     * @param deviceId The identifier of the device that the message originates from.
      * @param contentType The content type describing the message's payload.
      * @return The message.
-     * @throws NullPointerException if address or content type are {@code null}.
+     * @throws NullPointerException if any of the parameters is {@code null}.
      */
-    protected static Message newMessage(final String address, final String contentType) {
+    protected static Message newMessage(final String address, final String deviceId, final String contentType) {
 
         Objects.requireNonNull(address);
+        Objects.requireNonNull(deviceId);
         Objects.requireNonNull(contentType);
 
         final Message msg = ProtonHelper.message();
         msg.setAddress(address.toString());
+        MessageHelper.addDeviceId(msg, deviceId);
         msg.setContentType(contentType);
         return msg;
     }

adapters/mqtt-vertx/src/main/java/org/eclipse/hono/adapter/mqtt/VertxBasedMqttProtocolAdapter.java
@@ -57,7 +57,7 @@ public final class VertxBasedMqttProtocolAdapter extends AbstractVertxBasedMqttP
             if (address.getTenantId() == null || address.getResourceId() == null) {
                 throw new IllegalArgumentException("topic of unauthenticated message must contain tenant and device IDs");
             } else {
-                return newMessage(address.getBasePath(), CONTENT_TYPE_OCTET_STREAM);
+                return newMessage(address.getBasePath(), address.getResourceId(), CONTENT_TYPE_OCTET_STREAM);
             }
         });
     }
@@ -72,9 +72,9 @@ public final class VertxBasedMqttProtocolAdapter extends AbstractVertxBasedMqttP
                 // use authenticated device's tenant to fill in missing information
                 final ResourceIdentifier downstreamAddress = ResourceIdentifier.from(address.getEndpoint(),
                         deviceIdentity.getTenantId(), deviceIdentity.getDeviceId());
-                return newMessage(downstreamAddress.getBasePath(), CONTENT_TYPE_OCTET_STREAM);
+                return newMessage(downstreamAddress.getBasePath(), downstreamAddress.getResourceId(), CONTENT_TYPE_OCTET_STREAM);
             } else {
-                return newMessage(address.getBasePath(), CONTENT_TYPE_OCTET_STREAM);
+                return newMessage(address.getBasePath(), address.getResourceId(), CONTENT_TYPE_OCTET_STREAM);
             }
         });
     }

adapters/mqtt-vertx/src/test/java/org/eclipse/hono/adapter/mqtt/VertxBasedMqttProtocolAdapterTest.java
@@ -20,6 +20,7 @@ import static org.mockito.Mockito.when;
 import org.apache.qpid.proton.message.Message;
 import org.eclipse.hono.config.ProtocolAdapterProperties;
 import org.eclipse.hono.service.auth.device.Device;
+import org.eclipse.hono.util.MessageHelper;
 import org.eclipse.hono.util.ResourceIdentifier;
 import org.junit.Before;
 import org.junit.Test;
@@ -184,6 +185,7 @@ public class VertxBasedMqttProtocolAdapterTest {
         final ResourceIdentifier downstreamAddress = ResourceIdentifier.fromString(downstreamMessage.result().getAddress());
         assertThat(downstreamAddress.getEndpoint(), is(VertxBasedMqttProtocolAdapter.TELEMETRY_ENDPOINT));
         assertThat(downstreamAddress.getTenantId(), is("my-tenant"));
+        assertThat(MessageHelper.getDeviceId(downstreamMessage.result()), is("4711"));
     }
 
     private static MqttPublishMessage newMessage(final MqttQoS qosLevel, final String topic) {