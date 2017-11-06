58c99a0ca921388f9b8b023e13b122e64d075dda
Dejan Bosanac <dejan@nighttale.net>
2017-11-06 13:59:26 +0100

    EnMasse integration
    EnMasse integration - fixes and documentation
    
    EnMasse integration - final touches

---
+++ example/src/main/deploy/openshift/common.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# Copyright (c) 2017 Red Hat and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#    Red Hat - initial creation
+#    Bosch Software Innovations GmbH
+
+function waiting_containers_ready {
+    ADDR_SPACE=$1
+    pods_id=$(oc get pods -n ${ADDR_SPACE} | awk 'NR >1 {print $1}')
+    for pod_id in ${pods_id}
+    do
+        ready=$(oc get -o json pod -n ${ADDR_SPACE}  $pod_id -o jsonpath={.status.containerStatuses[0].ready})
+        if [ ${ready} == "false" ]
+        then
+            return 1
+        fi
+    done
+    return 0
+}
+
+function wait_for_enmasse {
+    EXPECTED_PODS=$1
+    ADDRESS_SPACE=$2
+    TIMEOUT=600
+    NOW=$(date +%s)
+    END=$(($NOW + $TIMEOUT))
+    while true
+    do
+        NOW=$(date +%s)
+        if [ $NOW -gt $END ]; then
+            echo -e "\nTimed out waiting for nodes to come up!"
+            pods=`oc get pods -n ${ADDRESS_SPACE}`
+            echo "PODS: $pods"
+            exit 1
+        fi
+        num_running=`oc get pods -n ${ADDRESS_SPACE} -l app=enmasse | grep -v deploy | grep -c Running`
+        if [ "$num_running" -eq "$EXPECTED_PODS" ]; then
+            waiting_containers_ready ${ADDRESS_SPACE}
+            if [ $? -gt 0 ]
+            then
+                echo "All pods are up but all containers are not ready yet"
+                tput cuu1 && tput civis
+            else
+                tput el && tput cnorm
+                echo "EnMasse is ready!"
+                break
+            fi
+        else
+            echo "$num_running/$EXPECTED_PODS up"
+            tput cuu1 && tput civis
+        fi
+        sleep 5
+    done
+}
---
+++ example/src/main/deploy/openshift/enmasse_deploy.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+
+# Copyright (c) 2017 Red Hat and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#    Red Hat - initial creation
+#    Bosch Software Innovations GmbH
+
+# Absolute path this script is in
+SCRIPTPATH="$(cd "$(dirname "$0")" && pwd -P)"
+CONFIG=$SCRIPTPATH/../../config
+CERTS=$CONFIG/hono-demo-certs-jar
+HONO_HOME=$SCRIPTPATH/../../../..
+
+source $SCRIPTPATH/common.sh
+
+echo DEPLOYING ECLIPSE HONO ON OPENSHIFT
+
+# creating the directory for Hono Server persistent volume
+if [ ! -d /tmp/hono ]; then
+    mkdir /tmp/hono
+    chmod 777 /tmp/hono
+else
+    echo /tmp/hono already exists!
+fi
+
+# creating Hono persistent volume (admin needed)
+oc login -u system:admin
+oc create -f $SCRIPTPATH/hono-pv.yml
+
+oc login -u developer
+
+# use hono project
+oc project hono
+
+echo "Waiting for EnMasse..."
+wait_for_enmasse 7 hono
+
+# create addresses
+curl -X PUT -T addresses.json -H "content-type: application/json" http://$(oc get route restapi -o jsonpath='{.spec.host}')/v1/addresses/default
+
+# starting to deploy Eclipse Hono (developer user)
+echo
+echo "Deploying influxDB & Grafana ..."
+oc create secret generic influxdb-conf --from-file=$CONFIG/influxdb.conf
+oc create -f $CONFIG/hono-metrics-jar/META-INF/fabric8/openshift.yml
+echo ... done
+
+echo Deploying Authentication Server ...
+oc create secret generic hono-service-auth-conf \
+  --from-file=$CERTS/auth-server-key.pem \
+  --from-file=$CERTS/auth-server-cert.pem \
+  --from-file=$CERTS/trusted-certs.pem \
+  --from-file=application.yml=$CONFIG/hono-service-auth-config.yml
+oc create -f $CONFIG/hono-service-auth-jar/META-INF/fabric8/openshift.yml
+echo ... done
+
+echo Deploying Device Registry ...
+oc create secret generic hono-service-device-registry-conf \
+  --from-file=$CERTS/device-registry-key.pem \
+  --from-file=$CERTS/device-registry-cert.pem \
+  --from-file=$CERTS/auth-server-cert.pem \
+  --from-file=$CERTS/trusted-certs.pem \
+  --from-file=$CONFIG/example-credentials.json \
+  --from-file=application.yml=$CONFIG/hono-service-device-registry-config.yml
+oc create -f $CONFIG/hono-service-device-registry-jar/META-INF/fabric8/openshift.yml
+echo ... done
+
+echo Deploying Hono Messaging ...
+oc create secret generic hono-service-messaging-conf \
+  --from-file=$CERTS/hono-messaging-key.pem \
+  --from-file=$CERTS/hono-messaging-cert.pem \
+  --from-file=$CERTS/auth-server-cert.pem \
+  --from-file=$CERTS/trusted-certs.pem \
+  --from-file=application.yml=$CONFIG/hono-service-messaging-config-enmasse.yml
+oc create -f $CONFIG/hono-service-messaging-jar/META-INF/fabric8/openshift.yml
+echo ... done
+
+echo Deploying HTTP REST adapter ...
+oc create secret generic hono-adapter-rest-vertx-conf \
+  --from-file=$CERTS/rest-adapter-key.pem \
+  --from-file=$CERTS/rest-adapter-cert.pem \
+  --from-file=$CERTS/trusted-certs.pem \
+  --from-file=application.yml=$CONFIG/hono-adapter-rest-vertx-config.yml
+oc create -f $CONFIG/hono-adapter-rest-vertx-jar/META-INF/fabric8/openshift.yml
+echo ... done
+
+echo Deploying MQTT adapter ...
+oc create secret generic hono-adapter-mqtt-vertx-conf \
+  --from-file=$CERTS/mqtt-adapter-key.pem \
+  --from-file=$CERTS/mqtt-adapter-cert.pem \
+  --from-file=$CERTS/trusted-certs.pem \
+  --from-file=application.yml=$CONFIG/hono-adapter-mqtt-vertx-config.yml
+oc create -f $CONFIG/hono-adapter-mqtt-vertx-jar/META-INF/fabric8/openshift.yml
+echo ... done
+
+echo ECLIPSE HONO DEPLOYED ON OPENSHIFT
