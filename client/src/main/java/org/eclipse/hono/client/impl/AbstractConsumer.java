2a89041e07ef599d41190111ae02d05cc45102be
Jens Reimann <jreimann@redhat.com>
2017-11-16 17:41:57 +0100

    Fix missing license header
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

--- client/src/main/java/org/eclipse/hono/client/impl/AbstractConsumer.java
+++ client/src/main/java/org/eclipse/hono/client/impl/AbstractConsumer.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2016 Bosch Software Innovations GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Bosch Software Innovations GmbH - initial creation
+ */
+
 package org.eclipse.hono.client.impl;
 
 import io.vertx.core.AsyncResult;
