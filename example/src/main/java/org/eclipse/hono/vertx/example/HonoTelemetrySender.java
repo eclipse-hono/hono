84aec2a61cba9bb99aec744b522bd5c4878d34b9
Karsten Frank <Karsten.Frank@bosch-si.com>
2017-11-27 11:24:40 +0100

    [#409] Moved all code explanations to javadoc.
    In this commit, only the documentation of the Java sender is updated
    and points to the provided classes in the example module.
    
    Signed-off-by: Karsten Frank <Karsten.Frank@bosch-si.com>

--- example/src/main/java/org/eclipse/hono/vertx/example/HonoTelemetrySender.java
+++ example/src/main/java/org/eclipse/hono/vertx/example/HonoTelemetrySender.java
@@ -1,14 +1,14 @@
 package org.eclipse.hono.vertx.example;
 
-import org.eclipse.hono.vertx.example.base.AbstractHonoSender;
+import org.eclipse.hono.vertx.example.base.HonoSenderBase;
 
 /**
  * Example class with minimal dependencies for sending telemetry data to Hono.
- *
+ * <p>
  * Please refer to {@link org.eclipse.hono.vertx.example.base.HonoExampleConstants} to configure where Hono's
  * microservices are reachable.
  */
-public class HonoTelemetrySender extends AbstractHonoSender {
+public class HonoTelemetrySender extends HonoSenderBase {
     public static void main(final String[] args) throws Exception {
         System.out.println("Starting downstream telemetry sender...");
         HonoTelemetrySender honoDownstreamSender = new HonoTelemetrySender();
