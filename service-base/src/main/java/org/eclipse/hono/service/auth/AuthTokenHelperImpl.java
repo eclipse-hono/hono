041fbbe3086fdc33dd17feb2b6d5f8ae30a1ff11
Jens Reimann <jreimann@redhat.com>
2017-12-08 14:04:01 +0100

    De-duplicate code in JwtHelper derived classes
    This change does pull up common code from JwtHelper derived classes
    back into JwtHelper.
    
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

--- service-base/src/main/java/org/eclipse/hono/service/auth/AuthTokenHelperImpl.java
+++ service-base/src/main/java/org/eclipse/hono/service/auth/AuthTokenHelperImpl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017 Bosch Software Innovations GmbH.
+ * Copyright (c) 2017 Bosch Software Innovations GmbH and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,11 +8,11 @@
  *
  * Contributors:
  *    Bosch Software Innovations GmbH - initial creation
+ *    Red Hat Inc
  */
 
 package org.eclipse.hono.service.auth;
 
-import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
 import java.util.Objects;
@@ -20,8 +20,6 @@ import java.util.Objects;
 import org.eclipse.hono.auth.Authorities;
 import org.eclipse.hono.config.SignatureSupportingConfigProperties;
 import org.eclipse.hono.util.JwtHelper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
@@ -35,8 +33,6 @@ import io.vertx.core.Vertx;
  *
  */
 public class AuthTokenHelperImpl extends JwtHelper implements AuthTokenHelper {
-
-    private static final Logger LOG = LoggerFactory.getLogger(AuthTokenHelperImpl.class);
 
     private AuthTokenHelperImpl() {
         this(null);
@@ -56,22 +52,9 @@ public class AuthTokenHelperImpl extends JwtHelper implements AuthTokenHelper {
      * @throws IllegalArgumentException if the key material cannot be determined from config.
      */
     public static AuthTokenHelper forSigning(final Vertx vertx, final SignatureSupportingConfigProperties config) {
-        Objects.requireNonNull(config);
-        if (!config.isAppropriateForCreating()) {
-            throw new IllegalArgumentException("configuration does not specify any signing key material");
-        } else {
-            AuthTokenHelperImpl result = new AuthTokenHelperImpl(vertx);
-            result.tokenLifetime = Duration.ofSeconds(config.getTokenExpiration());
-            if (config.getSharedSecret() != null) {
-                byte[] secret = getBytes(config.getSharedSecret());
-                result.setSharedSecret(secret);
-                LOG.info("using shared secret [{} bytes] for signing JWTs", secret.length);
-            } else if (config.getKeyPath() != null) {
-                result.setPrivateKey(config.getKeyPath());
-                LOG.info("using private key [{}] for signing JWTs", config.getKeyPath());
-            }
-            return result;
-        }
+
+        return JwtHelper.forSigning(config, () -> new AuthTokenHelperImpl(vertx));
+
     }
 
     /**
@@ -84,21 +67,9 @@ public class AuthTokenHelperImpl extends JwtHelper implements AuthTokenHelper {
      * @throws IllegalArgumentException if the key material cannot be determined from config.
      */
     public static AuthTokenHelper forValidating(final Vertx vertx, final SignatureSupportingConfigProperties config) {
-        Objects.requireNonNull(config);
-        if (!config.isAppropriateForValidating()) {
-            throw new IllegalArgumentException("configuration does not specify any key material for validating signatures");
-        } else {
-            AuthTokenHelperImpl result = new AuthTokenHelperImpl(vertx);
-            if (config.getSharedSecret() != null) {
-                byte[] secret = getBytes(config.getSharedSecret());
-                result.setSharedSecret(secret);
-                LOG.info("using shared secret [{} bytes] for validating JWT signatures", secret.length);
-            } else if (config.getCertPath() != null) {
-                result.setPublicKey(config.getCertPath());
-                LOG.info("using public key from certificate [{}] for validating JWT signatures", config.getCertPath());
-            }
-            return result;
-        }
+
+        return JwtHelper.forValidating(config, () -> new AuthTokenHelperImpl(vertx));
+
     }
 
     /**
@@ -110,11 +81,9 @@ public class AuthTokenHelperImpl extends JwtHelper implements AuthTokenHelper {
      * @throws NullPointerException if sharedSecret is {@code null}.
      */
     public static AuthTokenHelper forSharedSecret(final String sharedSecret, final long tokenExpirationSeconds) {
-        Objects.requireNonNull(sharedSecret);
-        AuthTokenHelperImpl result = new AuthTokenHelperImpl();
-        result.setSharedSecret(getBytes(sharedSecret));
-        result.tokenLifetime = Duration.ofSeconds(tokenExpirationSeconds);
-        return result;
+
+        return JwtHelper.forSharedSecret(sharedSecret, tokenExpirationSeconds, AuthTokenHelperImpl::new);
+
     }
 
     @Override
