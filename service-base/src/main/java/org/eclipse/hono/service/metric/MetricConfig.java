c740c5329d6726f8227a69eb26c3366112726343
Kai Hudalla <kai.hudalla@bosch-si.com>
2017-12-20 18:38:23 +0100

    Remove obsolete import.
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

--- service-base/src/main/java/org/eclipse/hono/service/metric/MetricConfig.java
+++ service-base/src/main/java/org/eclipse/hono/service/metric/MetricConfig.java
@@ -19,7 +19,6 @@ import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
