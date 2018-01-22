1f2d56ed5e33dceec00235a9b0bdc45702004e09
Jens Reimann <jreimann@redhat.com>
2018-01-22 16:58:28 +0100

    [#454] Fix memory leak in message sender using handlers
    This change fixes a memory leak by locally settling messages in:
    * EventSenderImpl
    * ForwardingEventDownstreamAdapter
    * TelemetrySenderImp
    
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

--- services/messaging/src/main/java/org/eclipse/hono/event/impl/ForwardingEventDownstreamAdapter.java
+++ services/messaging/src/main/java/org/eclipse/hono/event/impl/ForwardingEventDownstreamAdapter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016 Bosch Software Innovations GmbH.
+ * Copyright (c) 2016, 2018 Bosch Software Innovations GmbH and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,6 +8,7 @@
  *
  * Contributors:
  *    Bosch Software Innovations GmbH - initial creation
+ *    Red Hat Inc
  *
  */
 package org.eclipse.hono.event.impl;
@@ -47,7 +48,16 @@ public final class ForwardingEventDownstreamAdapter extends ForwardingDownstream
     }
 
     protected void forwardMessage(final ProtonSender sender, final Message msg, final ProtonDelivery delivery) {
-        sender.send(msg, updatedDelivery -> delivery.disposition(updatedDelivery.getRemoteState(), updatedDelivery.remotelySettled()));
+        sender.send(msg, updatedDelivery -> {
+
+            // settle downstream message locally
+
+            updatedDelivery.settle();
+
+            // forward downstream state to upstream message
+
+            delivery.disposition(updatedDelivery.getRemoteState(), updatedDelivery.remotelySettled());
+        });
     }
 
     @Override
