a76c5d326047fa6c432a8981d5c0364af0591db7
Jens Reimann <jreimann@redhat.com>
2018-01-24 13:49:37 +0100

    Use Set and EnumSet instead of iterating over array
    This change uses optimized EnumSets instead of manually iterating
    over the array.
    
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

--- services/messaging/src/main/java/org/eclipse/hono/event/impl/EventEndpoint.java
+++ services/messaging/src/main/java/org/eclipse/hono/event/impl/EventEndpoint.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016, 2017 Bosch Software Innovations GmbH.
+ * Copyright (c) 2016, 2017 Bosch Software Innovations GmbH and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,12 +8,16 @@
  *
  * Contributors:
  *    Bosch Software Innovations GmbH - initial creation
+ *    Red Hat Inc
  *
  */
 
 package org.eclipse.hono.event.impl;
 
+import java.util.Collections;
+import java.util.EnumSet;
 import java.util.Objects;
+import java.util.Set;
 
 import org.apache.qpid.proton.message.Message;
 import org.eclipse.hono.util.EventConstants;
@@ -39,7 +43,8 @@ import io.vertx.proton.ProtonQoS;
 @Qualifier("event")
 public final class EventEndpoint extends MessageForwardingEndpoint<HonoMessagingConfigProperties> {
 
-    private static final ProtonQoS[] SUPPORTED_DELIVERY_MODES = new ProtonQoS[]{ ProtonQoS.AT_LEAST_ONCE };
+    private static final Set<ProtonQoS> SUPPORTED_DELIVERY_MODES = Collections
+            .unmodifiableSet(EnumSet.of(ProtonQoS.AT_LEAST_ONCE));
 
     /**
      * Creates a new endpoint.
@@ -79,7 +84,7 @@ public final class EventEndpoint extends MessageForwardingEndpoint<HonoMessaging
      * @return <em>AT_LEAST_ONCE</em> delivery mode.
      */
     @Override
-    protected ProtonQoS[] getEndpointQos() {
+    protected Set<ProtonQoS> getEndpointQos() {
         return SUPPORTED_DELIVERY_MODES;
     }
 }
