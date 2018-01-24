e511e4b3db172c86498c7a3ca755e77536d26ccf
Kai Hudalla <kai.hudalla@bosch-si.com>
2018-01-24 15:09:26 +0100

    Add JavaDoc.
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

--- core/src/main/java/org/eclipse/hono/util/RegistrationResult.java
+++ core/src/main/java/org/eclipse/hono/util/RegistrationResult.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016,2017 Bosch Software Innovations GmbH.
+ * Copyright (c) 2016, 2018 Bosch Software Innovations GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,6 +12,7 @@
 
 package org.eclipse.hono.util;
 
+import io.vertx.core.json.DecodeException;
 import io.vertx.core.json.JsonObject;
 
 /**
@@ -19,21 +20,44 @@ import io.vertx.core.json.JsonObject;
  *
  */
 public final class RegistrationResult extends RequestResponseResult<JsonObject> {
+
     private RegistrationResult(final int status, final JsonObject payload) {
         super(status, payload);
     }
 
+    /**
+     * Creates a new result for a status code.
+     * 
+     * @param status The status code.
+     * @return The result.
+     */
     public static RegistrationResult from(final int status) {
         return new RegistrationResult(status, null);
     }
 
+    /**
+     * Creates a new result for a status code and a payload.
+     * 
+     * @param status The status code.
+     * @param payload The payload to include in the result.
+     * @return The result.
+     */
     public static RegistrationResult from(final int status, final JsonObject payload) {
         return new RegistrationResult(status, payload);
     }
 
-    public static RegistrationResult from(final int status, final String payloadString) {
-        if (payloadString != null) {
-            return new RegistrationResult(status, new JsonObject(payloadString));
+    /**
+     * Creates a new result for a status code and a payload.
+     * 
+     * @param status The status code.
+     * @param payload The string representation of the JSON payload
+     *                to include in the result (may be {@code null}).
+     * @return The result.
+     * @throws DecodeException if the given payload is not valid JSON.
+     */
+    public static RegistrationResult from(final int status, final String payload) {
+        if (payload != null) {
+            return new RegistrationResult(status, new JsonObject(payload));
         } else {
             return new RegistrationResult(status, null);
         }
