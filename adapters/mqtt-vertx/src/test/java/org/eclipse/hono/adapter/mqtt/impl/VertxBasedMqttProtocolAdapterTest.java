4cc794a6d66ae7610e824a313b02914e95ccddc0
Kai Hudalla <kai.hudalla@bosch-si.com>
2018-01-26 17:51:46 +0100

    [#416] Support devices acting as gateways in MQTT adapter.
    Add support for devices connecting to the MQTT adapter and publishing
    data "on behalf of" other devices.
    
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

--- adapters/mqtt-vertx/src/test/java/org/eclipse/hono/adapter/mqtt/impl/VertxBasedMqttProtocolAdapterTest.java
+++ adapters/mqtt-vertx/src/test/java/org/eclipse/hono/adapter/mqtt/impl/VertxBasedMqttProtocolAdapterTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017 Bosch Software Innovations GmbH.
+ * Copyright (c) 2017, 2018 Bosch Software Innovations GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -17,23 +17,24 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import org.apache.qpid.proton.message.Message;
-import org.eclipse.hono.adapter.mqtt.impl.VertxBasedMqttProtocolAdapter;
+import org.eclipse.hono.adapter.mqtt.MqttContext;
 import org.eclipse.hono.config.ProtocolAdapterProperties;
 import org.eclipse.hono.service.auth.device.Device;
 import org.eclipse.hono.util.EventConstants;
-import org.eclipse.hono.util.MessageHelper;
 import org.eclipse.hono.util.ResourceIdentifier;
 import org.eclipse.hono.util.TelemetryConstants;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
 
 import io.netty.handler.codec.mqtt.MqttQoS;
 import io.vertx.core.Future;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.mqtt.MqttEndpoint;
 import io.vertx.mqtt.messages.MqttPublishMessage;
 
 /**
@@ -43,19 +44,14 @@ import io.vertx.mqtt.messages.MqttPublishMessage;
 @RunWith(VertxUnitRunner.class)
 public class VertxBasedMqttProtocolAdapterTest {
 
+    /**
+     * Time out all tests after 5 seconds.
+     */
+    @Rule
+    public Timeout timeout = Timeout.seconds(5);
+
     private ProtocolAdapterProperties config;
     private VertxBasedMqttProtocolAdapter adapter;
-
-    /**
-     * Sets up the fixture.
-     */
-    @Before
-    public void setUp() {
-
-        config = new ProtocolAdapterProperties();
-        adapter = new VertxBasedMqttProtocolAdapter();
-        adapter.setConfig(config);
-    }
 
     /**
      * Verifies that the adapter rejects messages published to topics containing an endpoint
@@ -64,19 +60,15 @@ public class VertxBasedMqttProtocolAdapterTest {
      * @param ctx The helper to use for running tests on vert.x.
      */
     @Test
-    public void testGetDownstreamMessageFailsForUnknownEndpoint(final TestContext ctx) {
+    public void testMapTopicFailsForUnknownEndpoint(final TestContext ctx) {
 
-        // GIVEN an adapter
+        givenAnAdapter();
 
         // WHEN a device publishes a message to a topic with an unknown endpoint
         final MqttPublishMessage message = newMessage(MqttQoS.AT_MOST_ONCE, "unknown");
-        final Async determineAddressFailure = ctx.async();
-        adapter.getDownstreamMessage(message).recover(t -> {
-            determineAddressFailure.complete();
-            return Future.failedFuture(t);
-        });
-        // THEN no downstream sender can be created for the message
-        determineAddressFailure.await(2000);
+        adapter.mapTopic(message).setHandler(ctx.asyncAssertFailure(t -> {
+            // THEN no downstream sender can be created for the message
+        }));
     }
 
     /**
@@ -85,19 +77,15 @@ public class VertxBasedMqttProtocolAdapterTest {
      * @param ctx The helper to use for running tests on vert.x.
      */
     @Test
-    public void testGetDownstreamMessageFailsForQoS1TelemetryMessage(final TestContext ctx) {
+    public void testMapTopicFailsForQoS1TelemetryMessage(final TestContext ctx) {
 
-        // GIVEN an adapter
+        givenAnAdapter();
 
         // WHEN a device publishes a message with QoS 1 to a "telemetry" topic
         final MqttPublishMessage message = newMessage(MqttQoS.AT_LEAST_ONCE, TelemetryConstants.TELEMETRY_ENDPOINT);
-        final Async determineAddressFailure = ctx.async();
-        adapter.getDownstreamMessage(message).recover(t -> {
-            determineAddressFailure.complete();
-            return Future.failedFuture(t);
-        });
-        // THEN no downstream sender can be created for the message
-        determineAddressFailure.await(2000);
+        adapter.mapTopic(message).setHandler(ctx.asyncAssertFailure(t -> {
+            // THEN no downstream sender can be created for the message
+        }));
     }
 
     /**
@@ -106,19 +94,15 @@ public class VertxBasedMqttProtocolAdapterTest {
      * @param ctx The helper to use for running tests on vert.x.
      */
     @Test
-    public void testGetDownstreamMessageFailsForQoS0EventMessage(final TestContext ctx) {
+    public void testMapTopicFailsForQoS0EventMessage(final TestContext ctx) {
 
-        // GIVEN an adapter
+        givenAnAdapter();
 
         // WHEN a device publishes a message with QoS 0 to an "event" topic
         final MqttPublishMessage message = newMessage(MqttQoS.AT_MOST_ONCE, EventConstants.EVENT_ENDPOINT);
-        final Async messageFailure = ctx.async();
-        adapter.getDownstreamMessage(message).recover(t -> {
-            messageFailure.complete();
-            return Future.failedFuture(t);
-        });
-        // THEN no downstream sender can be created for the message
-        messageFailure.await(2000);
+        adapter.mapTopic(message).setHandler(ctx.asyncAssertFailure(t -> {
+            // THEN no downstream sender can be created for the message
+        }));
     }
 
     /**
@@ -127,20 +111,15 @@ public class VertxBasedMqttProtocolAdapterTest {
      * @param ctx The helper to use for running tests on vert.x.
      */
     @Test
-    public void testGetDownstreamMessageFailsForMissingTenant(final TestContext ctx) {
+    public void testOnPublishedMessageFailsForMissingTenant(final TestContext ctx) {
 
-        // GIVEN an adapter
+        givenAnAdapter();
 
         // WHEN an anonymous device publishes a message to a topic that does not contain a tenant ID
-        final MqttPublishMessage message = newMessage(MqttQoS.AT_MOST_ONCE, TelemetryConstants.TELEMETRY_ENDPOINT);
-        final Async determineAddressFailure = ctx.async();
-        adapter.getDownstreamMessage(message).recover(t -> {
-            determineAddressFailure.complete();
-            return Future.failedFuture(t);
-        });
-
-        // THEN the message cannot be mapped to an address
-        determineAddressFailure.await(2000);
+        final MqttContext context = newContext(MqttQoS.AT_MOST_ONCE, TelemetryConstants.TELEMETRY_ENDPOINT);
+        adapter.onPublishedMessage(context).setHandler(ctx.asyncAssertFailure(t -> {
+            // THEN the message cannot be mapped to an address
+        }));
     }
 
     /**
@@ -149,20 +128,35 @@ public class VertxBasedMqttProtocolAdapterTest {
      * @param ctx The helper to use for running tests on vert.x.
      */
     @Test
-    public void testGetDownstreamMessageFailsForMissingDeviceId(final TestContext ctx) {
+    public void testOnPublishedMessageFailsForMissingDeviceId(final TestContext ctx) {
 
-        // GIVEN an adapter
+        givenAnAdapter();
 
         // WHEN an anonymous device publishes a message to a topic that does not contain a device ID
-        final MqttPublishMessage message = newMessage(MqttQoS.AT_MOST_ONCE, TelemetryConstants.TELEMETRY_ENDPOINT + "/my-tenant");
-        final Async determineAddressFailure = ctx.async();
-        adapter.getDownstreamMessage(message).recover(t -> {
-            determineAddressFailure.complete();
-            return Future.failedFuture(t);
-        });
+        final MqttContext context = newContext(MqttQoS.AT_MOST_ONCE, TelemetryConstants.TELEMETRY_ENDPOINT + "/my-tenant");
+        adapter.onPublishedMessage(context).setHandler(ctx.asyncAssertFailure(t -> {
+            // THEN the message cannot be mapped to an address
+        }));
+    }
 
-        // THEN the message cannot be mapped to an address
-        determineAddressFailure.await(2000);
+    /**
+     * Verifies that the adapter fails to map a topic without a device ID received from an authenticated device.
+     * 
+     * @param ctx The helper to use for running tests on vert.x.
+     */
+    @Test
+    public void testOnPublishedAuthenticatedMessageFailsForMissingDeviceId(final TestContext ctx) {
+
+        givenAnAdapter();
+
+        // WHEN an authenticated device publishes a message to a topic that does not contain a device ID
+        final MqttContext context = newContext(
+                MqttQoS.AT_MOST_ONCE,
+                TelemetryConstants.TELEMETRY_ENDPOINT + "/my-tenant",
+                new Device("my-tenant", "device"));
+        adapter.onPublishedMessage(context).setHandler(ctx.asyncAssertFailure(t -> {
+            // THEN the message cannot be mapped to an address
+        }));
     }
 
     /**
@@ -171,30 +165,59 @@ public class VertxBasedMqttProtocolAdapterTest {
      * @param ctx The helper to use for running tests on vert.x.
      */
     @Test
-    public void testGetDownstreamMessageUsesDeviceIdentityForTopicWithoutTenant(final TestContext ctx) {
+    public void testOnPublishedMessageUsesDeviceIdentityForTopicWithoutTenant(final TestContext ctx) {
 
-        // GIVEN an adapter
+        givenAnAdapter();
 
         // WHEN an authenticated device publishes a message to a topic that does not contain a tenant ID
         final MqttPublishMessage message = newMessage(MqttQoS.AT_MOST_ONCE, TelemetryConstants.TELEMETRY_ENDPOINT);
-        final Async determineAddressSuccess = ctx.async();
-        Future<Message> downstreamMessage = adapter.getDownstreamMessage(message, new Device("my-tenant", "4711")).map(msg -> {
-            determineAddressSuccess.complete();
-            return msg;
-        });
+        final MqttContext context = newContext(message, new Device("my-tenant", "4711"));
+        final Async addressCheck = ctx.async();
+        final Future<ResourceIdentifier> checkedAddress = adapter.mapTopic(message)
+            .compose(address -> adapter.checkAddress(context, address))
+            .map(address -> {
+                addressCheck.complete();
+                return address;
+            });
 
         // THEN the mapped address contains the authenticated device's tenant and device ID
-        determineAddressSuccess.await(2000);
-        final ResourceIdentifier downstreamAddress = ResourceIdentifier.fromString(downstreamMessage.result().getAddress());
+        addressCheck.await();
+        final ResourceIdentifier downstreamAddress = checkedAddress.result();
         assertThat(downstreamAddress.getEndpoint(), is(TelemetryConstants.TELEMETRY_ENDPOINT));
         assertThat(downstreamAddress.getTenantId(), is("my-tenant"));
-        assertThat(MessageHelper.getDeviceId(downstreamMessage.result()), is("4711"));
+    }
+
+    private void givenAnAdapter() {
+
+        config = new ProtocolAdapterProperties();
+        adapter = new VertxBasedMqttProtocolAdapter();
+        adapter.setConfig(config);
     }
 
     private static MqttPublishMessage newMessage(final MqttQoS qosLevel, final String topic) {
+        return newMessage(qosLevel, topic, Buffer.buffer("test"));
+    }
+
+    private static MqttPublishMessage newMessage(final MqttQoS qosLevel, final String topic, final Buffer payload) {
+
         final MqttPublishMessage message = mock(MqttPublishMessage.class);
         when(message.qosLevel()).thenReturn(qosLevel);
         when(message.topicName()).thenReturn(topic);
+        when(message.payload()).thenReturn(payload);
         return message;
+    }
+
+    private static MqttContext newContext(final MqttQoS qosLevel, final String topic) {
+        return newContext(qosLevel, topic, null);
+    }
+
+    private static MqttContext newContext(final MqttQoS qosLevel, final String topic, final Device authenticatedDevice) {
+
+        final MqttPublishMessage message = newMessage(qosLevel, topic);
+        return newContext(message, authenticatedDevice);
+    }
+
+    private static MqttContext newContext(final MqttPublishMessage message, final Device authenticatedDevice) {
+        return new MqttContext(message, mock(MqttEndpoint.class), authenticatedDevice);
     }
 }
--- site/content/user-guide/mqtt-adapter.md
+++ site/content/user-guide/mqtt-adapter.md
@@ -223,21 +223,22 @@ The same holds true analogously for the *hono-service-device-registry.hono* addr
 
 ## Using the MQTT Adapter Topic Hierarchy
 
-The MQTT adapter by default requires devices to authenticate during connection establishment. In order to do so, clients need to provide a *username* and a *password* in the MQTT *CONNECT* packet. The *username* must have the form *auth-id@tenant*, e.g. `sensor1@DEFAULT_TENANT`. The adapter verifies the credentials provided by the device against the credentials the [configured Credentials service]({{< relref "#credentials-service-configuration" >}}) has on record for the device. The adapter uses the Credentials API's *get* operation to retrieve the credentials-on-record with the *tenant* and *auth-id* provided by the device in the *username* and `hashed-password` as the *type* of secret as query parameters.
+The MQTT adapter by default requires clients (devices or gateway components) to authenticate during connection establishment. In order to do so, clients need to provide a *username* and a *password* in the MQTT *CONNECT* packet. The *username* must have the form *auth-id@tenant*, e.g. `sensor1@DEFAULT_TENANT`. The adapter verifies the credentials provided by the client against the credentials the [configured Credentials service]({{< relref "#credentials-service-connection-configuration" >}}) has on record for the client. The adapter uses the Credentials API's *get* operation to retrieve the credentials on record with the *tenant* and *auth-id* provided by the client in the *username* and `hashed-password` as the *type* of secret as query parameters.
 
-When running the Hono example installation as described in the [Getting Started guide]({{< relref "getting-started.md" >}}), the demo Credentials service comes pre-configured with a `hashed-password` secret for device `4711` of tenant `DEFAULT_TENANT` having an *auth-id* of `sensor1` and (hashed) *password* `hono-secret`. These credentials are used in the following examples illustrating the usage of the adapter. Please refer to the [Credentials API]({{< relref "api/Credentials-API.md#standard-credential-types" >}}) for details regarding the different types of secrets.
+When running the Hono example installation as described in the [Getting Started guide]({{< relref "getting-started.md" >}}), the demo Credentials service comes pre-configured with a `hashed-password` secret for devices `4711` and `gw-1` of tenant `DEFAULT_TENANT` having *auth-ids* `sensor1` and `gw1` and (hashed) *passwords* `hono-secret` and `gw-secret` respectively. These credentials are used in the following examples illustrating the usage of the adapter. Please refer to the [Credentials API]({{< relref "api/Credentials-API.md#standard-credential-types" >}}) for details regarding the different types of secrets.
 
 {{% note %}}
 There is a subtle difference between the *device identifier* (*device-id*) and the *auth-id* a device uses for authentication. See [Device Identity]({{< relref "concepts/device-identity.md" >}}) for a discussion of the concepts.
 {{% /note %}}
 
-### Publish Telemetry Data
+### Publish Telemetry Data (authenticated Device)
 
 * Topic: `telemetry`
+* Authentication: required
 * Payload:
   * (required) Arbitrary payload
 
-This is the preferred way for devices to publish telemetry data. It is available only if the protocol adapter is configured to require devices to authenticate (which is the default).
+This is the preferred way for devices to publish telemetry data. It is available only if the protocol adapter is configured to require devices to authenticate (which is the default). When using this topic, the MQTT adapter determines the device's tenant and device identity as part of the authentication process.
 
 **Example**
 
@@ -245,16 +246,15 @@ Publish some JSON data for device `4711`:
 
     $ mosquitto_pub -u 'sensor1@DEFAULT_TENANT' -P hono-secret -t telemetry -m '{"temp": 5}'
 
-### Publish Telemetry Data (unauthenticated devices)
+### Publish Telemetry Data (unauthenticated Device)
 
 * Topic: `telemetry/${tenant-id}/${device-id}`
+* Authentication: none
 * Payload:
   * (required) Arbitrary payload
 
 This topic can be used by devices that have not authenticated to the protocol adapter. Note that this requires the
 `HONO_MQTT_AUTHENTICATION_REQUIRED` configuration property to be explicitly set to `false`.
-
-For reasons of completeness, this topic can also be used by devices that do have been authenticated. In this case the protocol adapter verifies that the values provided in the path parameters match the credentials that the device has provided during authentication. If they do not match, the connection to the client is closed.
 
 **Examples**
 
@@ -262,18 +262,30 @@ Publish some JSON data for device `4711`:
 
     $ mosquitto_pub -t telemetry/DEFAULT_TENANT/4711 -m '{"temp": 5}'
 
-An authenticated device may also use this topic. In this case the device needs to know both the *auth ID* as well as the corresponding *device identifier*:
 
-    $ mosquitto_pub -u 'sensor1@DEFAULT_TENANT' -P hono-secret -t telemetry/DEFAULT_TENANT/4711 -m '{"temp": 5}'
+### Publish Telemetry Data (authenticated Gateway)
 
-In real world scenarios this would usually only make sense if the *device identifier* is also used as the *auth ID*, i.e.
+* Topic: `telemetry/${tenant-id}/${device-id}`
+* Authentication: required
+* Payload:
+  * (required) Arbitrary payload
 
-    $ mosquitto_pub -u '4711@DEFAULT_TENANT' -P hono-secret -t telemetry/DEFAULT_TENANT/4711 -m '{"temp": 5}'
+This topic can be used by *gateway* components to publish data *on behalf of* other devices which do not connect to a protocol adapter directly but instead are connected to the gateway, e.g. using some low-bandwidth radio based technology like [SigFox](https://www.sigfox.com) or [LoRa](https://www.lora-alliance.org/). In this case the credentials provided by the gateway during connection establishment with the protocol adapter are used to authenticate the gateway whereas the parameters from the topic name are used to identify the device that the gateway publishes data for.
 
+The protocol adapter checks the gateway's authority to publish data on behalf of the device implicitly by means of retrieving a *registration assertion* for the device from the [configured Device Registration service]({{< relref "#device-registration-service-connection-configuration" >}}).
 
-### Publish an Event
+**Examples**
+
+Publish some JSON data for device `4712` via gateway `gw-1`:
+
+    $ mosquitto_pub -u 'gw@DEFAULT_TENANT' -P gw-secret -t telemetry/DEFAULT_TENANT/4712 -m '{"temp": 5}'
+
+**NB**: The example above assumes that a gateway device with ID `gw-1` has been registered with `hashed-password` credentials with *auth-id* `gw` and password `gw-secret`.
+
+### Publish an Event (authenticated Device)
 
 * Topic: `event`
+* Authentication: required
 * Payload:
   * (required) Arbitrary payload
 
@@ -283,32 +295,42 @@ This is the preferred way for devices to publish events. It is available only if
 
 Upload a JSON string for device `4711`:
 
-    $ mosquitto_pub -u 'sensor1@DEFAULT_TENANT' -P hono-secret -t event -m '{"alarm": 1}'
-    
-### Publish an Event (unauthenticated devices)
+    $ mosquitto_pub -u 'sensor1@DEFAULT_TENANT' -P hono-secret -t event -q 1 -m '{"alarm": 1}'
+
+### Publish an Event (unauthenticated Device)
 
 * Topic: `event/${tenant-id}/${device-id}`
+* Authentication: none
 * Payload:
   * (required) Arbitrary payload
 
 This topic can be used by devices that have not authenticated to the protocol adapter. Note that this requires the
 `HONO_MQTT_AUTHENTICATION_REQUIRED` configuration property to be explicitly set to `false`.
 
-For reasons of completeness, this topic can also be used by devices that do have been authenticated. In this case the protocol adapter verifies that the values provided in the path parameters match the credentials that the device has provided during authentication. If they do not match, the connection to the client is closed.
-
 **Examples**
 
 Publish some JSON data for device `4711`:
 
-    $ mosquitto_pub -t event/DEFAULT_TENANT/4711 -m '{"alarm": 1}'
+    $ mosquitto_pub -t event/DEFAULT_TENANT/4711 -q 1 -m '{"alarm": 1}'
 
-An authenticated device may also use this topic. In this case the device needs to know both the *auth ID* as well as the corresponding *device identifier*:
+### Publish an Event (authenticated Gateway)
 
-    $ mosquitto_pub -u 'sensor1@DEFAULT_TENANT' -P hono-secret -t event/DEFAULT_TENANT/4711 -m '{"alarm": 1}'
+* Topic: `event/${tenant-id}/${device-id}`
+* Authentication: required
+* Payload:
+  * (required) Arbitrary payload
 
-In real world scenarios this would usually only make sense if the *device identifier* is also used as the *auth ID*, i.e.
+This topic can be used by *gateway* components to publish data *on behalf of* other devices which do not connect to a protocol adapter directly but instead are connected to the gateway, e.g. using some low-bandwidth radio based technology like [SigFox](https://www.sigfox.com) or [LoRa](https://www.lora-alliance.org/). In this case the credentials provided by the gateway during connection establishment with the protocol adapter are used to authenticate the gateway whereas the parameters from the topic name are used to identify the device that the gateway publishes data for.
 
-    $ mosquitto_pub -u '4711@DEFAULT_TENANT' -P hono-secret -t event/DEFAULT_TENANT/4711 -m '{"alarm": 1}'
+The protocol adapter checks the gateway's authority to publish data on behalf of the device implicitly by means of retrieving a *registration assertion* for the device from the [configured Device Registration service]({{< relref "#device-registration-service-connection-configuration" >}}).
+
+**Examples**
+
+Publish some JSON data for device `4712` via gateway `gw-1`:
+
+    $ mosquitto_pub -u 'gw@DEFAULT_TENANT' -P gw-secret -t event/DEFAULT_TENANT/4712 -q 1 -m '{"temp": 5}'
+
+**NB**: The example above assumes that a gateway device with ID `gw-1` has been registered with `hashed-password` credentials with *auth-id* `gw` and password `gw-secret`.
 
 ## Downstream Meta Data
 
