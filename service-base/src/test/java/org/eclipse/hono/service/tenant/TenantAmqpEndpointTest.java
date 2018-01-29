cc0dc951a4d6cf0976f0f81369f54e88fe0e3688
Karsten Frank <Karsten.Frank@bosch-si.com>
2018-01-29 15:15:30 +0100

    [#388] Tests for Tenant API service implementation.
    Signed-off-by: Karsten Frank <Karsten.Frank@bosch-si.com>

---
+++ service-base/src/test/java/org/eclipse/hono/service/tenant/TenantAmqpEndpointTest.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 1.0 which is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * SPDX-License-Identifier: EPL-1.0
+ */
+
+package org.eclipse.hono.service.tenant;
+
+import static org.eclipse.hono.util.TenantConstants.Action;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.contains;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.apache.qpid.proton.message.Message;
+import org.eclipse.hono.util.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonObject;
+import io.vertx.proton.ProtonHelper;
+
+/**
+ * Tests verifying behavior of {@link TenantAmqpEndpoint}.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class TenantAmqpEndpointTest {
+
+    private static final ResourceIdentifier resource = ResourceIdentifier.from(TenantConstants.TENANT_ENDPOINT, Constants.DEFAULT_TENANT, null);
+
+    @Mock private EventBus eventBus;
+    @Mock private Vertx    vertx;
+
+    private TenantAmqpEndpoint endpoint;
+
+    /**
+     * Sets up the fixture.
+     */
+    @Before
+    public void setUp() {
+
+        when(vertx.eventBus()).thenReturn(eventBus);
+
+        endpoint = new TenantAmqpEndpoint(vertx);
+    }
+
+    /**
+     * Verifies that the endpoint forwards a request message via the event bus.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testProcessMessageSendsRequestViaEventBus() {
+
+        Message msg = ProtonHelper.message();
+        msg.setSubject(Action.ACTION_GET.toString());
+        MessageHelper.addTenantId(msg, Constants.DEFAULT_TENANT);
+        MessageHelper.annotate(msg, resource);
+
+        endpoint.processRequest(msg, resource, Constants.PRINCIPAL_ANONYMOUS);
+
+        verify(eventBus).send(contains(TenantConstants.EVENT_BUS_ADDRESS_TENANT_IN), any(JsonObject.class), any(Handler.class));
+    }
+
+    /**
+     * Verifies that the JsonObject constructed for a tenant message on the event bus contains the tenantId
+     * as defined in the {@link RequestResponseApiConstants} class.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testTenantMessageForEventBus() {
+
+        Message msg = ProtonHelper.message();
+        msg.setSubject(Action.ACTION_GET.toString());
+        MessageHelper.addTenantId(msg, Constants.DEFAULT_TENANT);
+        MessageHelper.annotate(msg, resource);
+
+        final JsonObject tenantMsg = TenantConstants.getTenantMsg(msg);
+        assertNotNull(tenantMsg);
+        assertTrue(tenantMsg.containsKey(RequestResponseApiConstants.FIELD_TENANT_ID));
+    }
+
+}
---
+++ service-base/src/test/java/org/eclipse/hono/service/tenant/TenantMessageFilterTest.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 1.0 which is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * SPDX-License-Identifier: EPL-1.0
+ */
+
+package org.eclipse.hono.service.tenant;
+
+import static org.eclipse.hono.util.MessageHelper.APP_PROPERTY_RESOURCE;
+import static org.eclipse.hono.util.MessageHelper.APP_PROPERTY_TENANT_ID;
+import static org.eclipse.hono.util.TenantConstants.Action;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.apache.qpid.proton.amqp.Symbol;
+import org.apache.qpid.proton.message.Message;
+import org.eclipse.hono.util.*;
+import org.junit.Test;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.proton.ProtonHelper;
+
+/**
+ * Test verifying that the filter complies with the tenant message format specification.
+ */
+public class TenantMessageFilterTest {
+
+    private static final String DEFAULT_TENANT = "DEFAULT_TENANT";
+
+    @Test
+    public void testVerifyDetectsMissingTenantProperty() {
+        // GIVEN a valid tenant GET message without an AMQP value
+        final Message msg = givenAMessageHavingProperties(Action.ACTION_GET);
+        // WHEN receiving the message via a link with any tenant
+        final ResourceIdentifier linkTarget = getResourceIdentifier(DEFAULT_TENANT);
+
+        // THEN message validation fails
+        assertFalse(TenantMessageFilter.verify(linkTarget, msg));
+    }
+
+    @Test
+    public void testVerifySucceedsForValidGetAction() {
+        // GIVEN a tenant GET message for tenant DEFAULT_TENANT
+        final Message msg = givenAMessageHavingProperties(Action.ACTION_GET);
+        MessageHelper.addProperty(msg, APP_PROPERTY_TENANT_ID, DEFAULT_TENANT);
+        // WHEN receiving the message via a link with matching target address
+        final ResourceIdentifier linkTarget = getResourceIdentifier(DEFAULT_TENANT);
+
+        // THEN message validation succeeds
+        assertTrue(TenantMessageFilter.verify(linkTarget, msg));
+        assertMessageAnnotationsContainProperties(msg, DEFAULT_TENANT);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testTenantMessageForEventBus() {
+        Message msg = ProtonHelper.message();
+        msg.setSubject(Action.ACTION_GET.toString());
+        MessageHelper.addTenantId(msg, Constants.DEFAULT_TENANT);
+
+        ResourceIdentifier resource = ResourceIdentifier.from(TenantConstants.TENANT_ENDPOINT, Constants.DEFAULT_TENANT, null);
+        MessageHelper.annotate(msg, resource);
+
+        final JsonObject tenantMsg = TenantConstants.getTenantMsg(msg);
+        assertNotNull(tenantMsg);
+        assertTrue(tenantMsg.containsKey(RequestResponseApiConstants.FIELD_TENANT_ID));
+    }
+
+    private void assertMessageAnnotationsContainProperties(final Message msg, final String tenantId) {
+        assertNotNull(msg.getMessageAnnotations());
+        assertThat(msg.getMessageAnnotations().getValue().get(Symbol.valueOf(MessageHelper.APP_PROPERTY_TENANT_ID)),
+                is(tenantId));
+        final ResourceIdentifier expectedResourceIdentifier = getResourceIdentifier(DEFAULT_TENANT);
+        assertThat(msg.getMessageAnnotations().getValue().get(Symbol.valueOf(APP_PROPERTY_RESOURCE)),
+                is(expectedResourceIdentifier.toString()));
+    }
+
+    private ResourceIdentifier getResourceIdentifier(final String tenant) {
+        return ResourceIdentifier.from(TenantConstants.TENANT_ENDPOINT, tenant, null);
+    }
+
+    private Message givenAMessageHavingProperties(final Action action) {
+        final Message msg = ProtonHelper.message();
+        msg.setMessageId("msg");
+        msg.setReplyTo("reply");
+        msg.setSubject(action.toString());
+        return msg;
+    }
+}
