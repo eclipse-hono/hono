c6b289d6252e027c1b57ba090a847d1181a39d97
Kai Hudalla <kai.hudalla@bosch-si.com>
2018-01-29 17:02:48 +0100

    Pull up default detail message provider method.
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

--- client/src/main/java/org/eclipse/hono/client/ServerErrorException.java
+++ client/src/main/java/org/eclipse/hono/client/ServerErrorException.java
@@ -62,26 +62,9 @@ public class ServerErrorException extends ServiceInvocationException {
      * @throws IllegalArgumentException if the code is not &ge; 500 and &lt; 600.
      */
     public ServerErrorException(final int errorCode, final String msg, final Throwable cause) {
-        super(errorCode, providedOrDefaultMessage(errorCode, msg), cause);
+        super(errorCode, msg, cause);
         if (errorCode < 500 || errorCode >= 600) {
             throw new IllegalArgumentException("client error code must be >= 500 and < 600");
         }
     }
-
-    /**
-     * Provide a default message if none is provided.
-     * 
-     * @param errorCode The error code
-     * @param msg The detail message. May be {@code null}.
-     * @return The provided message or the default message derived from the error code if {@code null} was provided as a
-     *         message.
-     */
-    private static String providedOrDefaultMessage(final int errorCode, final String msg) {
-        if (msg != null) {
-            return msg;
-        }
-
-        return "Error Code: " + errorCode;
-    }
-
 }
