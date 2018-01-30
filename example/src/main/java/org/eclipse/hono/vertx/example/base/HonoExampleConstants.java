f217b5658e01e2a337c5d5d840d61cdc0c2b5c9a
Kai Hudalla <kai.hudalla@bosch-si.com>
2018-01-30 13:35:36 +0100

    Add missing header comment.
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

--- example/src/main/java/org/eclipse/hono/vertx/example/base/HonoExampleConstants.java
+++ example/src/main/java/org/eclipse/hono/vertx/example/base/HonoExampleConstants.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright (c) 2017, 2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 1.0 which is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * SPDX-License-Identifier: EPL-1.0
+ */
+
 package org.eclipse.hono.vertx.example.base;
 
 /**
