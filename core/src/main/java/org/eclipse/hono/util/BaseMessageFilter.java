594a26171e8d674be6beb503834dd5acabdc27e3
Kai Hudalla <kai.hudalla@bosch-si.com>
2018-02-07 12:03:15 +0100

    [#469] Add methods for checking device ID and correlation ID.
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

--- core/src/main/java/org/eclipse/hono/util/BaseMessageFilter.java
+++ core/src/main/java/org/eclipse/hono/util/BaseMessageFilter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016 Bosch Software Innovations GmbH.
+ * Copyright (c) 2016, 2018 Bosch Software Innovations GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -24,13 +24,10 @@ public class BaseMessageFilter {
     private static final Logger LOG = LoggerFactory.getLogger(BaseMessageFilter.class);
 
     /**
-     * Checks whether a given registration message contains required standard properties.
+     * Checks whether an AMQP message contains required standard properties.
      * <p>
-     * In particular, the following conditions need to be met in order for the message to pass:
-     * <ol>
-     * <li>The message contains an application property {@link MessageHelper#APP_PROPERTY_DEVICE_ID}.</li>
-     * <li>If the given link target contains a device id in its path, it must match the id from the property.</li>
-     * </ol>
+     * This method checks if the message contains a valid device identifier using
+     * the {@link #hasValidDeviceId(ResourceIdentifier, Message)} method.
      * <p>
      * After successful verification the following properties are added to the message's <em>annotations</em>:
      * <ul>
@@ -45,20 +42,56 @@ public class BaseMessageFilter {
      */
      protected static final boolean verifyStandardProperties(final ResourceIdentifier linkTarget, final Message msg) {
 
+         if (!hasValidDeviceId(linkTarget, msg)) {
+             return false;
+         } else {
+             final ResourceIdentifier targetResource = ResourceIdentifier
+                     .from(linkTarget.getEndpoint(), linkTarget.getTenantId(), MessageHelper.getDeviceId(msg));
+             MessageHelper.annotate(msg, targetResource);
+             return true;
+         }
+    }
+
+     /**
+      * Checks if an AMQP message contains a valid device identifier.
+      * 
+      * @param linkTarget The resource path to check the message's properties against for consistency.
+      * @param msg The AMQP 1.0 message to perform the checks on.
+      * @return {@code true} if the following conditions are met:
+      *                      <ul>
+      *                      <li>The message contains an application property {@link MessageHelper#APP_PROPERTY_DEVICE_ID}.</li>
+      *                      <li>If the link target contains a resource ID, it matches the ID from the property.</li>
+      *                      </ul>
+      */
+     protected static final boolean hasValidDeviceId(final ResourceIdentifier linkTarget, final Message msg) {
+
          final String deviceIdProperty = MessageHelper.getDeviceId(msg);
 
          if (deviceIdProperty == null) {
              LOG.trace("message [{}] contains no {} application property", msg.getMessageId(), MessageHelper.APP_PROPERTY_DEVICE_ID);
              return false;
          } else if (linkTarget.getResourceId() != null && !deviceIdProperty.equals(linkTarget.getResourceId())) {
-             LOG.trace("message property contains invalid device ID [expected: {}, but was: {}]",
-                     linkTarget.getResourceId(), deviceIdProperty);
+             LOG.trace("message's {} property contains invalid device ID [expected: {}, but was: {}]",
+                     MessageHelper.APP_PROPERTY_DEVICE_ID, linkTarget.getResourceId(), deviceIdProperty);
              return false;
          } else {
-             final ResourceIdentifier targetResource = ResourceIdentifier
-                     .from(linkTarget.getEndpoint(), linkTarget.getTenantId(), deviceIdProperty);
-             MessageHelper.annotate(msg, targetResource);
              return true;
          }
     }
+
+     /**
+      * Checks if an AMQP message contains either a message ID or a correlation ID.
+      * 
+      * @param msg The message.
+      * @return {@code true} if the message has an ID that can be used for correlation.
+      */
+     protected static final boolean hasCorrelationId(final Message msg) {
+
+         if (msg.getMessageId() == null && msg.getCorrelationId() == null) {
+             LOG.trace("message has neither a message-id nor correlation-id");
+             return false;
+         } else {
+             return true;
+         }
+     }
 }
