2cc0824fc7a5c7070cb8aa8b48910fed479bf425
Kai Hudalla <kai.hudalla@bosch-si.com>
2018-02-13 15:43:46 +0100

    Add missing JavaDoc.
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

--- core/src/test/java/org/eclipse/hono/config/PemReaderTest.java
+++ core/src/test/java/org/eclipse/hono/config/PemReaderTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017 Red Hat Inc and others.
+ * Copyright (c) 2017, 2018 Red Hat Inc and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -22,11 +22,20 @@ import java.util.List;
 import org.eclipse.hono.config.PemReader.Entry;
 import org.junit.Test;
 
+/**
+ * Tests verifying behavior of {@link PemReader}.
+ *
+ */
 public class PemReaderTest {
 
     private final Path basePath1 = Paths.get("target/certs/");
     private final Path basePath2 = Paths.get("src/test/resources/testpem/");
 
+    /**
+     * Verifies that the reader can read a PKCS8 encoded certificate.
+     * 
+     * @throws IOException if the test fails.
+     */
     @Test
     public void testCertPemPkcs8() throws IOException {
         final List<Entry> result = PemReader.readAll(this.basePath1.resolve("artemis-cert.pem"));
@@ -39,6 +48,11 @@ public class PemReaderTest {
         assertEquals("CERTIFICATE", second.getType());
     }
 
+    /**
+     * Verifies that the reader can read a PKCS8 encoded private key.
+     * 
+     * @throws IOException if the test fails.
+     */
     @Test
     public void testKeyPemPkcs8() throws IOException {
         final List<Entry> result = PemReader.readAll(this.basePath1.resolve("artemis-key.pem"));
@@ -48,6 +62,11 @@ public class PemReaderTest {
         assertEquals("PRIVATE KEY", first.getType());
     }
 
+    /**
+     * Verifies that the reader can read a PKCS1 encoded private key.
+     * 
+     * @throws IOException if the test fails.
+     */
     @Test
     public void testKeyPemPkcs1Private() throws IOException {
         final List<Entry> result = PemReader.readAll(this.basePath2.resolve("pkcs1-private-key.pem"));
@@ -57,6 +76,11 @@ public class PemReaderTest {
         assertEquals("RSA PRIVATE KEY", first.getType());
     }
 
+    /**
+     * Verifies that the reader can read a PKCS1 encoded public key.
+     * 
+     * @throws IOException if the test fails.
+     */
     @Test
     public void testKeyPemPkcs1Public() throws IOException {
         final List<Entry> result = PemReader.readAll(this.basePath2.resolve("pkcs1-public-key.pem"));
@@ -66,6 +90,11 @@ public class PemReaderTest {
         assertEquals("PUBLIC KEY", first.getType());
     }
 
+    /**
+     * Verifies that the reader ignores non resolvable paths.
+     * 
+     * @throws IOException if the test fails.
+     */
     @Test
     public void testEmpty() throws IOException {
         final List<Entry> result = PemReader.readAll(this.basePath2.resolve("empty.pem"));
@@ -74,6 +103,8 @@ public class PemReaderTest {
 
     /**
      * Bogus payload in the middle
+     * 
+     * @throws IOException if the test fails.
      */
     @Test(expected = IOException.class)
     public void testError1() throws IOException {
@@ -82,6 +113,8 @@ public class PemReaderTest {
 
     /**
      * Missing END statement
+     * 
+     * @throws IOException if the test fails.
      */
     @Test(expected = IOException.class)
     public void testError2() throws IOException {
@@ -90,6 +123,8 @@ public class PemReaderTest {
 
     /**
      * Only end END statement
+     * 
+     * @throws IOException if the test fails.
      */
     @Test(expected = IOException.class)
     public void testError3() throws IOException {
@@ -98,6 +133,8 @@ public class PemReaderTest {
 
     /**
      * Duplicate BEGIN
+     * 
+     * @throws IOException if the test fails.
      */
     @Test(expected = IOException.class)
     public void testError4() throws IOException {
@@ -106,6 +143,8 @@ public class PemReaderTest {
 
     /**
      * Missing BEGIN
+     * 
+     * @throws IOException if the test fails.
      */
     @Test(expected = IOException.class)
     public void testError5() throws IOException {
