d7d014c0dfe20be6b853b8b65d3b4ee50f20eed6
Jens Reimann <jreimann@redhat.com>
2018-03-26 14:36:41 +0200

    Fix javadoc after re-basing javadoc codestyle checks.

--- core/src/main/java/org/eclipse/hono/util/EndpointType.java
+++ core/src/main/java/org/eclipse/hono/util/EndpointType.java
@@ -11,21 +11,29 @@
 package org.eclipse.hono.util;
 
 /**
- * Utility used to determine type of the endpoint
+ * Utility used to determine type of the endpoint.
  */
 public enum EndpointType {
     TELEMETRY, EVENT, UNKNOWN;
 
+    /**
+     * Gets the endpoint type from a string value.
+     * 
+     * @param name The name of the endpoint type.
+     * 
+     * @return The enum literal of the endpoint type. Returns {@value #UNKNOWN} if it cannot find the endpoint type.
+     *         Never returns {@code null}.
+     */
     public static EndpointType fromString(String name) {
         switch (name) {
-            case TelemetryConstants.TELEMETRY_ENDPOINT:
-            case TelemetryConstants.TELEMETRY_ENDPOINT_SHORT:
-                return TELEMETRY;
-            case EventConstants.EVENT_ENDPOINT:
-            case EventConstants.EVENT_ENDPOINT_SHORT:
-                return EVENT;
-            default:
-                return UNKNOWN;
+        case TelemetryConstants.TELEMETRY_ENDPOINT:
+        case TelemetryConstants.TELEMETRY_ENDPOINT_SHORT:
+            return TELEMETRY;
+        case EventConstants.EVENT_ENDPOINT:
+        case EventConstants.EVENT_ENDPOINT_SHORT:
+            return EVENT;
+        default:
+            return UNKNOWN;
         }
     }
 }
