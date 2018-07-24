ab2c2663f6f33f999f274027f7edc0d6ec4a09f5
Kai Hudalla <kai.hudalla@bosch-si.com>
2018-07-24 17:46:07 +0200

    [#730] Migrate Kura adapter module to EPL 2.0.
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

--- adapters/kura/src/main/java/org/eclipse/hono/adapter/kura/KuraAdapterProperties.java
+++ adapters/kura/src/main/java/org/eclipse/hono/adapter/kura/KuraAdapterProperties.java
@@ -1,15 +1,15 @@
-/**
- * Copyright (c) 2017 Contributors to the Eclipse Foundation
+/*******************************************************************************
+ * Copyright (c) 2016, 2018 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
  *
  * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 1.0 which is available at
- * https://www.eclipse.org/legal/epl-v10.html
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
  *
- * SPDX-License-Identifier: EPL-1.0
- */
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
 
 package org.eclipse.hono.adapter.kura;
 
