e863b3f6fe2ac0443b46552129ed10040182827b
Kai Hudalla <kai.hudalla@bosch-si.com>
2018-07-25 18:30:47 +0200

    [#730] Fix license header.
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

--- core/src/main/java/org/eclipse/hono/util/MessageTap.java
+++ core/src/main/java/org/eclipse/hono/util/MessageTap.java
@@ -5,10 +5,10 @@
  * information regarding copyright ownership.
  *
  * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 1.0 which is available at
- * https://www.eclipse.org/legal/epl-v10.html
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
  *
- * SPDX-License-Identifier: EPL-1.0
+ * SPDX-License-Identifier: EPL-2.0
  */
 package org.eclipse.hono.util;
 
