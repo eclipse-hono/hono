fb204c68a20ef052325b1576c8b86fc3f91a6310
Trystram Jean-Baptiste <jbtrystram@redhat.com>
2018-08-27 15:06:55 +0200

    split tenant API regarding mandatory and optional operation
    Signed-off-by: Trystram Jean-Baptiste <jbtrystram@redhat.com>

--- service-base/src/test/java/org/eclipse/hono/service/tenant/BaseTenantServiceTest.java
+++ service-base/src/test/java/org/eclipse/hono/service/tenant/BaseTenantServiceTest.java
@@ -17,7 +17,6 @@ import java.net.HttpURLConnection;
 
 import javax.security.auth.x500.X500Principal;
 
-import org.eclipse.hono.client.ServiceInvocationException;
 import org.eclipse.hono.config.ServiceConfigProperties;
 import org.eclipse.hono.util.EventBusMessage;
 import org.eclipse.hono.util.TenantConstants;
@@ -31,7 +30,6 @@ import org.junit.runner.RunWith;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
-import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -59,77 +57,6 @@ public class BaseTenantServiceTest {
     @BeforeClass
     public static void setUp() {
         tenantService = createBaseTenantService();
-    }
-
-    /**
-     * Verifies that the base service accepts a request for adding
-     * a tenant that contains the minimum required properties.
-     * 
-     * @param ctx The vert.x test context.
-     */
-    @Test
-    public void testAddSucceedsForMinimalData(final TestContext ctx) {
-
-        final JsonObject testPayload = createValidTenantPayload();
-
-        final EventBusMessage request = createRequest(TenantConstants.TenantAction.add, testPayload);
-        tenantService.processRequest(request).setHandler(ctx.asyncAssertSuccess(response -> {
-            ctx.assertEquals(HttpURLConnection.HTTP_CREATED, response.getStatus());
-            ctx.assertEquals(TEST_TENANT, response.getTenant());
-        }));
-    }
-
-    /**
-     * Verifies that the base service fails for an incomplete message that does not contain mandatory fields.
-     * 
-     * @param ctx The vert.x test context.
-     */
-    @Test
-    public void testAddFailsForIncompleteMessage(final TestContext ctx) {
-
-        final EventBusMessage msg = EventBusMessage.forOperation(TenantConstants.TenantAction.add.toString());
-        tenantService.processRequest(msg).setHandler(ctx.asyncAssertFailure(t -> {
-            ctx.assertEquals(HttpURLConnection.HTTP_BAD_REQUEST, ((ServiceInvocationException) t).getErrorCode());
-        }));
-    }
-
-    /**
-     * Verifies that the base service fails for a payload that defines an empty adapter array (must be null or has to
-     * contain at least one element).
-     * 
-     * @param ctx The vert.x test context.
-     */
-    @Test
-    public void testAddFailsForEmptyAdapterArray(final TestContext ctx) {
-
-        final JsonObject testPayload = createValidTenantPayload();
-        testPayload.put(TenantConstants.FIELD_ADAPTERS, new JsonArray());
-
-        final EventBusMessage msg = createRequest(TenantConstants.TenantAction.add, testPayload);
-        tenantService.processRequest(msg).setHandler(ctx.asyncAssertFailure(t -> {
-            ctx.assertEquals(HttpURLConnection.HTTP_BAD_REQUEST, ((ServiceInvocationException) t).getErrorCode());
-        }));
-    }
-
-    /**
-     * Verifies that the base service fails for a payload that defines an adapter entry, but does not provide the
-     * mandatory field {@link TenantConstants#FIELD_ADAPTERS_TYPE}.
-     * 
-     * @param ctx The vert.x test context.
-     */
-    @Test
-    public void testAddFailsForAdapterConfigWithoutType(final TestContext ctx) {
-
-        final JsonObject testPayload = createValidTenantPayload();
-        final JsonArray adapterArray = new JsonArray();
-        // no type specified (which is a violation of the API)
-        adapterArray.add(new JsonObject());
-        testPayload.put(TenantConstants.FIELD_ADAPTERS, adapterArray);
-
-        final EventBusMessage msg = createRequest(TenantConstants.TenantAction.add, testPayload);
-        tenantService.processRequest(msg).setHandler(ctx.asyncAssertFailure(t -> {
-            ctx.assertEquals(HttpURLConnection.HTTP_BAD_REQUEST, ((ServiceInvocationException) t).getErrorCode());
-        }));
     }
 
     /**
@@ -201,11 +128,6 @@ public class BaseTenantServiceTest {
     private static BaseTenantService<ServiceConfigProperties> createBaseTenantService() {
 
         return new BaseTenantService<ServiceConfigProperties>() {
-
-            @Override
-            public void add(final String tenantId, final JsonObject tenantObj, final Handler<AsyncResult<TenantResult<JsonObject>>> resultHandler) {
-                resultHandler.handle(Future.succeededFuture(TenantResult.from(HttpURLConnection.HTTP_CREATED)));
-            }
 
             @Override
             public void get(final String tenantId, final Handler<AsyncResult<TenantResult<JsonObject>>> resultHandler) {
