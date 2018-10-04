b0f83fd49a7b113939b1a13240c386dc05eb87a4
Karthees Kalidass <Kartheeswaran.Kalidass@bosch-si.com>
2018-10-04 18:03:19 +0200

    [#557] Implement Hono-CI JIPP job as Pipeline
    Signed-off-by: Kartheeswaran Kalidass <Kartheeswaran.Kalidass@bosch-si.com>

---
+++ jenkins/Hono-CI-Pipeline.groovy
@@ -0,0 +1,40 @@
+#!/usr/bin/env groovy
+
+/*******************************************************************************
+ * Copyright (c) 2016, 2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+/**
+ * Jenkins pipeline script for Continuous integration build of Hono master.
+ * Checks GitHub repository every minute for changes.
+ *
+ */
+
+node {
+    def utils = evaluate readTrusted("jenkins/Hono-PipelineUtils.groovy")
+    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '5', daysToKeepStr: '', numToKeepStr: '')),
+                pipelineTriggers([pollSCM('* * * * *')])])
+    try {
+        utils.checkOutHonoRepoMaster()
+        utils.build()
+        utils.aggregateJunitResults()
+        utils.captureCodeCoverageReport()
+        currentBuild.result = 'SUCCESS'
+    } catch (err) {
+        currentBuild.result = 'FAILURE'
+        echo "Error: ${err}"
+    }
+    finally {
+        echo "Build status: ${currentBuild.result}"
+        utils.notifyBuildStatus()
+    }
+}
---
+++ jenkins/Hono-PipelineUtils.groovy
@@ -0,0 +1,94 @@
+#!/usr/bin/env groovy
+
+/*******************************************************************************
+ * Copyright (c) 2016, 2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0checkOutGitRepoMaster
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+/**
+ * A collection of utility methods to build pipelines
+ *
+ */
+
+/**
+ * Checks out the specified branch from hono github repo
+ *
+ * @param branch Branch to be checked out
+ */
+void checkOutHonoRepo(String branch) {
+    stage('Checkout') {
+        echo "Check out branch: $branch"
+        git branch: "$branch", url: "https://github.com/eclipse/hono.git"
+    }
+}
+
+/**
+ * Checks out the master branch from hono github repo
+ *
+ */
+void checkOutHonoRepoMaster() {
+    checkOutHonoRepo("master")
+}
+
+/**
+ * Build with maven (with jdk1.8.0-latest and apache-maven-latest as configured in 'Global Tool Configuration' in Jenkins).
+ *
+ */
+void build() {
+    stage('Build') {
+        withMaven(maven: 'apache-maven-latest', jdk: 'jdk1.8.0-latest', options: [jacocoPublisher(disabled: true)]) {
+            sh 'mvn -B clean install'
+        }
+    }
+}
+
+/**
+ * Aggregate junit test results.
+ *
+ */
+void aggregateJunitResults() {
+    stage('Aggregate Junit Test Results') {
+        junit '**/surefire-reports/*.xml'
+    }
+}
+
+/**
+ * Notify build status via email to 'hono-dev@eclipse.org'.
+ *
+ */
+void notifyBuildStatus() {
+    try {
+        step([$class                  : 'Mailer',
+              notifyEveryUnstableBuild: true,
+              recipients              : 'hono-dev@eclipse.org',
+              sendToIndividuals       : false])
+    } catch (error) {
+        echo "Error notifying build status via Email"
+        echo error.getMessage()
+        throw error
+    }
+}
+
+/**
+ * Capture code coverage reports using Jacoco jenkins plugin.
+ *
+ */
+void captureCodeCoverageReport() {
+    stage('Capture Code Coverage Report') {
+        step([$class       : 'JacocoPublisher',
+              execPattern  : '**/**.exec',
+              classPattern : '**/classes',
+              sourcePattern: '**/src/main/java'
+        ])
+    }
+}
+
+return this
