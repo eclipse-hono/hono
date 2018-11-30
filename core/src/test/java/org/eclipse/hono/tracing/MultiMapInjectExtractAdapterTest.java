e943df4ff2b0ab47ccf6ae286b840dc0b1519c0d
Carsten Lohmann <carsten.lohmann@bosch-si.com>
2018-11-30 13:42:26 +0100

    [#914] Put extracted SpanContext into EventBusMessage delivery headers.
    Signed-off-by: Carsten Lohmann <carsten.lohmann@bosch-si.com>

---
+++ core/src/test/java/org/eclipse/hono/tracing/MultiMapInjectExtractAdapterTest.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.hono.tracing;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.vertx.core.http.CaseInsensitiveHeaders;
+import org.junit.Test;
+
+import io.vertx.core.MultiMap;
+
+/**
+ * Tests verifying the behavior of {@link MultiMapInjectAdapter} and {@link MultiMapExtractAdapter}.
+ *
+ */
+public class MultiMapInjectExtractAdapterTest {
+
+    /**
+     * Verifies that the same entries injected via the {@code MultiMapInjectAdapter} are extracted via the
+     * {@code MultiMapExtractAdapter}.
+     */
+    @Test
+    public void testInjectAndExtract() {
+        final Map<String, String> testEntries = new HashMap<>();
+        testEntries.put("key1", "value1");
+        testEntries.put("key2", "value2");
+
+        final MultiMap multiMap = new CaseInsensitiveHeaders();
+        final MultiMapInjectAdapter injectAdapter = new MultiMapInjectAdapter(multiMap);
+        testEntries.forEach((key, value) -> {
+            injectAdapter.put(key, value);
+        });
+
+        final MultiMapExtractAdapter extractAdapter = new MultiMapExtractAdapter(multiMap);
+        extractAdapter.iterator().forEachRemaining(extractedEntry -> {
+            assertThat(extractedEntry.getValue(), is(testEntries.get(extractedEntry.getKey())));
+        });
+    }
+}
