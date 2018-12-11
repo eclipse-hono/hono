3657c3a0a0318a4a3c5450c593406e255ab26fb8
Carsten Lohmann <carsten.lohmann@bosch-si.com>
2018-12-11 15:26:15 +0100

    [#914] Add SpanContext param to RegistrationService methods.
    Signed-off-by: Carsten Lohmann <carsten.lohmann@bosch-si.com>

--- service-base/src/main/java/org/eclipse/hono/service/registration/RegistrationService.java
+++ service-base/src/main/java/org/eclipse/hono/service/registration/RegistrationService.java
@@ -15,6 +15,7 @@ package org.eclipse.hono.service.registration;
 
 import org.eclipse.hono.util.RegistrationResult;
 
+import io.opentracing.SpanContext;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.Verticle;
@@ -48,6 +49,34 @@ public interface RegistrationService extends Verticle {
     void assertRegistration(String tenantId, String deviceId, Handler<AsyncResult<RegistrationResult>> resultHandler);
 
     /**
+     * Asserts that a device is registered with a given tenant and is enabled.
+     * <p>
+     * This default implementation simply returns the result of {@link #assertRegistration(String, String, Handler)}.
+     *
+     * @param tenantId The tenant the device belongs to.
+     * @param deviceId The ID of the device to get the assertion for.
+     * @param spanContext The currently active OpenTracing span (may be {@code null}). An implementation
+     *                    should use this as the parent for any span it creates for tracing
+     *                    the execution of this operation.
+     * @param resultHandler The handler to invoke with the result of the operation.
+     *             The <em>status</em> will be
+     *             <ul>
+     *             <li><em>200 OK</em> if a device with the given ID is registered for
+     *             the tenant and its <em>enabled</em> property is {@code true}.
+     *             The <em>payload</em> will contain a JWT token asserting the registration status.</li>
+     *             <li><em>404 Not Found</em> if no device with the given identifier is
+     *             registered for the tenant or its <em>enabled</em> property is {@code false}.</li>
+     *             </ul>
+     * @throws NullPointerException if any of the parameters (except spanContext) is {@code null}.
+     * @see <a href="https://www.eclipse.org/hono/api/device-registration-api/#assert-device-registration">
+     *      Device Registration API - Assert Device Registration</a>
+     */
+    default void assertRegistration(final String tenantId, final String deviceId, final SpanContext spanContext,
+            final Handler<AsyncResult<RegistrationResult>> resultHandler) {
+        assertRegistration(tenantId, deviceId, resultHandler);
+    }
+
+    /**
      * Asserts that a device is authorized to act as a <em>gateway</em> for another device.
      * <p>
      * In particular, this means that the gateway and the device are registered with the tenant, are enabled
@@ -76,4 +105,44 @@ public interface RegistrationService extends Verticle {
      *      Device Registration API - Assert Device Registration</a>
      */
     void assertRegistration(String tenantId, String deviceId, String gatewayId, Handler<AsyncResult<RegistrationResult>> resultHandler);
+
+    /**
+     * Asserts that a device is authorized to act as a <em>gateway</em> for another device.
+     * <p>
+     * In particular, this means that the gateway and the device are registered with the tenant, are enabled
+     * and that the gateway device is allowed to <em>act on behalf of</em> the other device.
+     * <p>
+     * Implementing classes should verify, that the gateway is authorized to get an assertion for the device.
+     * Such a check might be based on a specific role that the client needs to have or on an
+     * explicitly defined relation between the gateway and the device(s).
+     * <p>
+     * This default implementation simply returns the result of {@link #assertRegistration(String, String, String, Handler)}.
+     *
+     * @param tenantId The tenant the device belongs to.
+     * @param deviceId The ID of the device to get the assertion for.
+     * @param gatewayId The gateway that wants to act on behalf of the device.
+     * @param spanContext The currently active OpenTracing span (may be {@code null}). An implementation
+     *                    should use this as the parent for any span it creates for tracing
+     *                    the execution of this operation.
+     * @param resultHandler The handler to invoke with the result of the operation.
+     *             The <em>status</em> will be
+     *             <ul>
+     *             <li><em>200 OK</em> if a device with the given ID is registered for
+     *             the tenant and its <em>enabled</em> property is {@code true}.
+     *             The <em>payload</em> will contain a JWT token asserting the registration status.</li>
+     *             <li><em>403 Forbidden</em> if the gateway is not authorized to get an assertion
+     *             for the device.</li>
+     *             <li><em>404 Not Found</em> if no device with the given identifier is
+     *             registered for the tenant or its <em>enabled</em> property is {@code false}.</li>
+     *             </ul>
+     * @throws NullPointerException if any of the parameters (except spanContext) is {@code null}.
+     * @see <a href="https://www.eclipse.org/hono/api/device-registration-api/#assert-device-registration">
+     *      Device Registration API - Assert Device Registration</a>
+     */
+    default void assertRegistration(final String tenantId, final String deviceId, final String gatewayId,
+            final SpanContext spanContext,
+            final Handler<AsyncResult<RegistrationResult>> resultHandler) {
+        assertRegistration(tenantId, deviceId, gatewayId, resultHandler);
+    }
+
 }
