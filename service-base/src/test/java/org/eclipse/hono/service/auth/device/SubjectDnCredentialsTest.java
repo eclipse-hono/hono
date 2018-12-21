3b1ed26401d56d190884fcb0e8089fad5b834c56
Kai Hudalla <kai.hudalla@bosch-si.com>
2018-12-21 13:02:59 +0100

    Add test case.
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

---
+++ service-base/src/test/java/org/eclipse/hono/service/auth/device/SubjectDnCredentialsTest.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+
+package org.eclipse.hono.service.auth.device;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+
+/**
+ * Verifies behavior of {@link SubjectDnCredentials}.
+ *
+ */
+public class SubjectDnCredentialsTest {
+
+    /**
+     * Verifies that the auth-id created from a subject DN is normalized.
+     */
+    @Test
+    public void testAuthIdIsRFC2253Compliant() {
+        final String subjectDn = "emailAddress=hoge@acme.com, CN=devices, OU=ACME Department, O=ACME Corporation, L=Chiyoda, ST=Tokyo, C=JP";
+        final SubjectDnCredentials credentials = SubjectDnCredentials.create("tenant", subjectDn);
+        System.out.println(credentials.getAuthId());
+        assertThat(credentials.getAuthId())
+            .isEqualTo("1.2.840.113549.1.9.1=#160d686f67654061636d652e636f6d,CN=devices,OU=ACME Department,O=ACME Corporation,L=Chiyoda,ST=Tokyo,C=JP");
+    }
+
+}
