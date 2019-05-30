43b17c81579da572b46b63cb26bcd4e84fd1dcdf
Kai Hudalla <kai.hudalla@bosch-si.com>
2019-05-30 09:29:07 +0200

    Fix file pattern.
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

--- jenkins/Hono-Release-Pipeline.groovy
+++ jenkins/Hono-Release-Pipeline.groovy
@@ -136,7 +136,7 @@ def commitAndPush() {
 def copyArtifacts() {
     stage("Copy Artifacts") {
         sh ''' 
-            chmod +r deploy/target/eclipse-hono-deploy-${RELEASE_VERSION}.tar.gz
+            chmod +r deploy/target/eclipse-hono-${RELEASE_VERSION}-*.tar.gz
             chmod +r cli/target/hono-cli-${RELEASE_VERSION}-exec.jar
             cp deploy/target/eclipse-hono-${RELEASE_VERSION}-*.tar.gz /home/data/httpd/download.eclipse.org/hono/
             cp cli/target/hono-cli-${RELEASE_VERSION}-exec.jar /home/data/httpd/download.eclipse.org/hono/
