4f9fd2458241f9e8bda00a933e0bd2b82267cbb5
Jens Reimann <jreimann@redhat.com>
2019-07-12 10:37:11 +0200

    Add checkstyle check for missing "break" in switch
    Also fix existing violations.

--- service-base/src/main/java/org/eclipse/hono/service/credentials/EventBusCompleteCredentialsAdapter.java
+++ service-base/src/main/java/org/eclipse/hono/service/credentials/EventBusCompleteCredentialsAdapter.java
@@ -277,6 +277,7 @@ public abstract class EventBusCompleteCredentialsAdapter<T> extends EventBusCred
             default:
                 // pass
             }
+            break;
         default:
             // pass
         }
