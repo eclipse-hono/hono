62519da32cf7e9363b8f0cbb42bd31a5dd7b9c68
Abel Buechner-Mihaljevic <Abel.Buechner@bosch-si.com>
2019-08-28 11:12:53 +0200

    [#1227] Fix error in test from last commit.
    Signed-off-by: Abel Buechner-Mihaljevic <Abel.Buechner@bosch-si.com>

--- service-base/src/test/java/org/eclipse/hono/service/AbstractProtocolAdapterBaseTest.java
+++ service-base/src/test/java/org/eclipse/hono/service/AbstractProtocolAdapterBaseTest.java
@@ -24,6 +24,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -633,8 +634,8 @@ public class AbstractProtocolAdapterBaseTest {
     @Test
     public void testDoNotRegisterForNoopMetrics() {
         // GIVEN an adapter with a metrics object that is an instance of NoopBasedMetrics
-        final Metrics noopBasedMetrics = new NoopBasedMetrics() {
-        };
+        final Metrics noopBasedMetrics = spy(new NoopBasedMetrics() {
+        });
         adapter.setMetrics(noopBasedMetrics);
 
         // WHEN starting the adapter
