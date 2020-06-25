a99f68944337f25c2ea75a1f9dc5058a8de696e8
Carsten Lohmann <carsten.lohmann@bosch.io>
2020-06-25 21:46:26 +0200

    [#2030] Handle closed connection during CONNECT processing.
    Signed-off-by: Carsten Lohmann <carsten.lohmann@bosch.io>

--- adapters/mqtt-vertx-base/src/test/java/org/eclipse/hono/adapter/mqtt/AbstractVertxBasedMqttProtocolAdapterTest.java
+++ adapters/mqtt-vertx-base/src/test/java/org/eclipse/hono/adapter/mqtt/AbstractVertxBasedMqttProtocolAdapterTest.java
@@ -427,7 +427,7 @@ public class AbstractVertxBasedMqttProtocolAdapterTest {
         verify(authHandler).authenticateDevice(any(MqttContext.class));
         verify(endpoint).accept(false);
         verify(endpoint).publishHandler(any(Handler.class));
-        verify(endpoint).closeHandler(any(Handler.class));
+        verify(endpoint, times(2)).closeHandler(any(Handler.class));
     }
 
     /**
@@ -480,7 +480,7 @@ public class AbstractVertxBasedMqttProtocolAdapterTest {
         // THEN the connection is established and handlers are registered
         verify(authHandler, never()).authenticateDevice(any(MqttContext.class));
         verify(endpoint).publishHandler(any(Handler.class));
-        verify(endpoint).closeHandler(any(Handler.class));
+        verify(endpoint, times(2)).closeHandler(any(Handler.class));
         verify(endpoint).accept(false);
     }
 
@@ -1095,7 +1095,7 @@ public class AbstractVertxBasedMqttProtocolAdapterTest {
 
         verify(metrics).incrementConnections("DEFAULT_TENANT");
         final ArgumentCaptor<Handler<Void>> closeHandlerCaptor = ArgumentCaptor.forClass(Handler.class);
-        verify(endpoint).closeHandler(closeHandlerCaptor.capture());
+        verify(endpoint, times(2)).closeHandler(closeHandlerCaptor.capture());
         closeHandlerCaptor.getValue().handle(null);
         verify(metrics).decrementConnections("DEFAULT_TENANT");
     }
@@ -1126,7 +1126,7 @@ public class AbstractVertxBasedMqttProtocolAdapterTest {
         verify(metrics).incrementUnauthenticatedConnections();
         // and when the device closes the connection
         final ArgumentCaptor<Handler<Void>> closeHandlerCaptor = ArgumentCaptor.forClass(Handler.class);
-        verify(endpoint).closeHandler(closeHandlerCaptor.capture());
+        verify(endpoint, times(2)).closeHandler(closeHandlerCaptor.capture());
         closeHandlerCaptor.getValue().handle(null);
         // the number of unauthenticated connections is decremented again
         verify(metrics).decrementUnauthenticatedConnections();
