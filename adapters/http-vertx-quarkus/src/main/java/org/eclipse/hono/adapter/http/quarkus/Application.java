65354b3611da2a353cb7fb44ec06f3fdf40c00ef
Kai Hudalla <kai.hudalla@bosch.io>
2020-11-16 08:34:15 +0100

    Incorporate feedback
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch.io>

--- adapters/http-vertx-quarkus/src/main/java/org/eclipse/hono/adapter/http/quarkus/Application.java
+++ adapters/http-vertx-quarkus/src/main/java/org/eclipse/hono/adapter/http/quarkus/Application.java
@@ -12,36 +12,23 @@
  */
 package org.eclipse.hono.adapter.http.quarkus;
 
-import java.time.Duration;
-import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
-import org.eclipse.hono.adapter.client.command.DeviceConnectionClient;
-import org.eclipse.hono.adapter.client.registry.DeviceRegistrationClient;
 import org.eclipse.hono.adapter.http.HttpAdapterMetrics;
 import org.eclipse.hono.adapter.http.HttpProtocolAdapterProperties;
 import org.eclipse.hono.adapter.http.impl.VertxBasedHttpProtocolAdapter;
-import org.eclipse.hono.client.CommandTargetMapper;
-import org.eclipse.hono.client.CommandTargetMapper.CommandTargetMapperContext;
-import org.eclipse.hono.client.ProtocolAdapterCommandConsumerFactory;
-import org.eclipse.hono.client.ProtocolAdapterCommandConsumerFactory.CommandHandlingAdapterInfoAccess;
 import org.eclipse.hono.service.quarkus.AbstractProtocolAdapterApplication;
-import org.eclipse.hono.util.RegistrationAssertion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.opentracing.SpanContext;
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
 import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Future;
 import io.vertx.core.Promise;
-import io.vertx.core.json.JsonObject;
 
 /**
  * The Hono HTTP adapter main application class.
@@ -92,77 +79,10 @@ public class Application extends AbstractProtocolAdapterApplication {
 
     private VertxBasedHttpProtocolAdapter adapter() {
 
-        final DeviceRegistrationClient registrationClient = registrationClient();
-        final DeviceConnectionClient deviceConnectionClient = deviceConnectionClient();
-        final CommandTargetMapper commandTargetMapper = commandTargetMapper();
-
-        commandTargetMapper.initialize(new CommandTargetMapperContext() {
-
-            @Override
-            public Future<List<String>> getViaGateways(
-                    final String tenant,
-                    final String deviceId,
-                    final SpanContext context) {
-
-                Objects.requireNonNull(tenant);
-                Objects.requireNonNull(deviceId);
-
-                return registrationClient.assertRegistration(tenant, deviceId, null, context)
-                        .map(RegistrationAssertion::getAuthorizedGateways);
-            }
-
-            @Override
-            public Future<JsonObject> getCommandHandlingAdapterInstances(
-                    final String tenant,
-                    final String deviceId,
-                    final List<String> viaGateways,
-                    final SpanContext context) {
-
-                Objects.requireNonNull(tenant);
-                Objects.requireNonNull(deviceId);
-                Objects.requireNonNull(viaGateways);
-
-                return deviceConnectionClient.getCommandHandlingAdapterInstances(
-                        tenant, deviceId, viaGateways, context);
-            }
-        });
-
-        final ProtocolAdapterCommandConsumerFactory commandConsumerFactory = commandConsumerFactory();
-        commandConsumerFactory.initialize(commandTargetMapper, new CommandHandlingAdapterInfoAccess() {
-
-            @Override
-            public Future<Void> setCommandHandlingAdapterInstance(
-                    final String tenant,
-                    final String deviceId,
-                    final String adapterInstanceId,
-                    final Duration lifespan,
-                    final SpanContext context) {
-                return deviceConnectionClient.setCommandHandlingAdapterInstance(tenant, deviceId, adapterInstanceId, lifespan, context);
-            }
-
-            @Override
-            public Future<Void> removeCommandHandlingAdapterInstance(
-                    final String tenant,
-                    final String deviceId,
-                    final String adapterInstanceId,
-                    final SpanContext context) {
-                return deviceConnectionClient.removeCommandHandlingAdapterInstance(tenant, deviceId, adapterInstanceId, context);
-            }
-        });
-
         final VertxBasedHttpProtocolAdapter adapter = new VertxBasedHttpProtocolAdapter();
-        adapter.setCommandConsumerFactory(commandConsumerFactory);
         adapter.setConfig(adapterProperties);
-        adapter.setCredentialsClient(credentialsClient());
-        adapter.setDeviceConnectionClient(deviceConnectionClient);
-        adapter.setEventSender(downstreamSender());
-        adapter.setHealthCheckServer(healthCheckServer);
         adapter.setMetrics(metrics);
-        adapter.setRegistrationClient(registrationClient);
-        adapter.setTelemetrySender(downstreamSender());
-        adapter.setTenantClient(tenantClient());
-        adapter.setTracer(tracer);
-        adapter.setResourceLimitChecks(resourceLimitChecks);
+        setCollaborators(adapter);
         return adapter;
     }
 }
--- adapters/mqtt-vertx-quarkus/src/main/java/org/eclipse/hono/adapter/mqtt/quarkus/Application.java
+++ adapters/mqtt-vertx-quarkus/src/main/java/org/eclipse/hono/adapter/mqtt/quarkus/Application.java
@@ -12,39 +12,26 @@
  */
 package org.eclipse.hono.adapter.mqtt.quarkus;
 
-import java.time.Duration;
-import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
-import org.eclipse.hono.adapter.client.command.DeviceConnectionClient;
-import org.eclipse.hono.adapter.client.registry.DeviceRegistrationClient;
 import org.eclipse.hono.adapter.mqtt.MessageMapping;
 import org.eclipse.hono.adapter.mqtt.MqttAdapterMetrics;
 import org.eclipse.hono.adapter.mqtt.MqttContext;
 import org.eclipse.hono.adapter.mqtt.MqttProtocolAdapterProperties;
 import org.eclipse.hono.adapter.mqtt.impl.HttpBasedMessageMapping;
 import org.eclipse.hono.adapter.mqtt.impl.VertxBasedMqttProtocolAdapter;
-import org.eclipse.hono.client.CommandTargetMapper;
-import org.eclipse.hono.client.CommandTargetMapper.CommandTargetMapperContext;
-import org.eclipse.hono.client.ProtocolAdapterCommandConsumerFactory;
-import org.eclipse.hono.client.ProtocolAdapterCommandConsumerFactory.CommandHandlingAdapterInfoAccess;
 import org.eclipse.hono.service.quarkus.AbstractProtocolAdapterApplication;
-import org.eclipse.hono.util.RegistrationAssertion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.opentracing.SpanContext;
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
 import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Future;
 import io.vertx.core.Promise;
-import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
 
 /**
@@ -96,78 +83,11 @@ public class Application extends AbstractProtocolAdapterApplication {
 
     private VertxBasedMqttProtocolAdapter adapter() {
 
-        final DeviceRegistrationClient registrationClient = registrationClient();
-        final DeviceConnectionClient deviceConnectionClient = deviceConnectionClient();
-        final CommandTargetMapper commandTargetMapper = commandTargetMapper();
-
-        commandTargetMapper.initialize(new CommandTargetMapperContext() {
-
-            @Override
-            public Future<List<String>> getViaGateways(
-                    final String tenant,
-                    final String deviceId,
-                    final SpanContext context) {
-
-                Objects.requireNonNull(tenant);
-                Objects.requireNonNull(deviceId);
-
-                return registrationClient.assertRegistration(tenant, deviceId, null, context)
-                        .map(RegistrationAssertion::getAuthorizedGateways);
-            }
-
-            @Override
-            public Future<JsonObject> getCommandHandlingAdapterInstances(
-                    final String tenant,
-                    final String deviceId,
-                    final List<String> viaGateways,
-                    final SpanContext context) {
-
-                Objects.requireNonNull(tenant);
-                Objects.requireNonNull(deviceId);
-                Objects.requireNonNull(viaGateways);
-
-                return deviceConnectionClient.getCommandHandlingAdapterInstances(
-                        tenant, deviceId, viaGateways, context);
-            }
-        });
-
-        final ProtocolAdapterCommandConsumerFactory commandConsumerFactory = commandConsumerFactory();
-        commandConsumerFactory.initialize(commandTargetMapper, new CommandHandlingAdapterInfoAccess() {
-
-            @Override
-            public Future<Void> setCommandHandlingAdapterInstance(
-                    final String tenant,
-                    final String deviceId,
-                    final String adapterInstanceId,
-                    final Duration lifespan,
-                    final SpanContext context) {
-                return deviceConnectionClient.setCommandHandlingAdapterInstance(tenant, deviceId, adapterInstanceId, lifespan, context);
-            }
-
-            @Override
-            public Future<Void> removeCommandHandlingAdapterInstance(
-                    final String tenant,
-                    final String deviceId,
-                    final String adapterInstanceId,
-                    final SpanContext context) {
-                return deviceConnectionClient.removeCommandHandlingAdapterInstance(tenant, deviceId, adapterInstanceId, context);
-            }
-        });
-
         final VertxBasedMqttProtocolAdapter adapter = new VertxBasedMqttProtocolAdapter();
-        adapter.setCommandConsumerFactory(commandConsumerFactory);
         adapter.setConfig(adapterProperties);
-        adapter.setCredentialsClient(credentialsClient());
-        adapter.setDeviceConnectionClient(deviceConnectionClient);
-        adapter.setEventSender(downstreamSender());
-        adapter.setHealthCheckServer(healthCheckServer);
         adapter.setMetrics(metrics);
-        adapter.setRegistrationClient(registrationClient);
-        adapter.setTelemetrySender(downstreamSender());
-        adapter.setTenantClient(tenantClient());
-        adapter.setTracer(tracer);
-        adapter.setResourceLimitChecks(resourceLimitChecks);
         adapter.setMessageMapping(messageMapping());
+        setCollaborators(adapter);
         return adapter;
     }
 
