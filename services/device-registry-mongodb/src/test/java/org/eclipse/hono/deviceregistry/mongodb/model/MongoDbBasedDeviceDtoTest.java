76a98d2429a26979d542aea6b747c68391c69336
Florian Kaltner <florian.kaltner@bosch.io>
2021-01-11 10:20:12 +0100

    Review suggestions: - Using forCreation()/forUpdate() in MongoDbBasedDeviceDtoTest
    Signed-off-by: Florian Kaltner <florian.kaltner@bosch.io>

--- services/device-registry-mongodb/src/test/java/org/eclipse/hono/deviceregistry/mongodb/model/MongoDbBasedDeviceDtoTest.java
+++ services/device-registry-mongodb/src/test/java/org/eclipse/hono/deviceregistry/mongodb/model/MongoDbBasedDeviceDtoTest.java
@@ -30,38 +30,67 @@ import io.vertx.core.json.JsonObject;
 class MongoDbBasedDeviceDtoTest {
 
     /**
-     * Tests that the DTO is serialized to JSON as expected in order to verify that the document persisted is
-     * written as expected.
+     * Tests that a DTO created for inserting a new entry is serialized to JSON as expected in order to verify that
+     * the document to be persisted is written as expected.
      */
     @Test
-    public void testEncode() {
+    public void testEncodeForCreate() {
         final String tenantId = "barfoo";
         final String deviceId = "foobar";
-        final Device device = new Device();
         final DeviceStatus deviceStatus = new DeviceStatus()
-                .setAutoProvisioned(true)
-                .setAutoProvisioningNotificationSent(true)
                 // make sure that these values do not interfere with the corresponding properties of DeviceDto's parent classes
-                .setCreationTime(Instant.now().plusSeconds(3600))
-                .setLastUpdate(Instant.now().plusSeconds(3600));
-        final Instant created = Instant.now().minusSeconds(1).truncatedTo(ChronoUnit.SECONDS);
-        final Instant updated = Instant.now().truncatedTo(ChronoUnit.SECONDS);;
+                .setAutoProvisioned(true)
+                .setCreationTime(Instant.now().plusSeconds(3600));
+        final Device device = new Device();
+        device.setStatus(deviceStatus);
         final String version = "spam";
 
-        final var deviceDto = MongoDbBasedDeviceDto.forRead(MongoDbBasedDeviceDto::new, tenantId, deviceId, device, deviceStatus,
-                created, updated, version);
+        final var deviceDto = MongoDbBasedDeviceDto.forCreation(MongoDbBasedDeviceDto::new, tenantId, deviceId, true, device, version);
         final var json = JsonObject.mapFrom(deviceDto);
 
         assertThat(json).isNotNull();
-        // size should match number of assertions below
-        assertThat(json.getMap()).hasSize(8);
+        // size should match number of assertions below, to make sure no unexpected properties are serialized
+        assertThat(json.getMap()).hasSize(7);
 
         assertThat(json.getString(RegistryManagementConstants.FIELD_PAYLOAD_TENANT_ID)).isEqualTo(tenantId);
         assertThat(json.getString(RegistryManagementConstants.FIELD_PAYLOAD_DEVICE_ID)).isEqualTo(deviceId);
-        assertThat(json.getInstant(RegistryManagementConstants.FIELD_STATUS_CREATION_DATE)).isEqualTo(created);
-        assertThat(json.getInstant(BaseDto.FIELD_UPDATED_ON)).isEqualTo(updated);
+        assertThat(json.getInstant(RegistryManagementConstants.FIELD_STATUS_CREATION_DATE)).isNotNull();
         assertThat(json.getString(BaseDto.FIELD_VERSION)).isEqualTo(version);
         assertThat(json.getBoolean(RegistryManagementConstants.FIELD_AUTO_PROVISIONED)).isTrue();
+        assertThat(json.getBoolean(RegistryManagementConstants.FIELD_AUTO_PROVISIONING_NOTIFICATION_SENT)).isFalse();
+        assertThat(json.getJsonObject(MongoDbDeviceRegistryUtils.FIELD_DEVICE)).isNotNull();
+    }
+
+
+    /**
+     * Tests that the a DTO created for updating an entry is serialized to JSON as expected in order to verify that
+     * the document to be persisted is written as expected.
+     */
+    @Test
+    public void testEncodeForUpdate() {
+        final String tenantId = "barfoo";
+        final String deviceId = "foobar";
+        final DeviceStatus deviceStatus = new DeviceStatus()
+                // make sure that these values do not interfere with the corresponding properties of DeviceDto's parent classes
+                .setAutoProvisioningNotificationSent(false)
+                .setLastUpdate(Instant.now().plusSeconds(3600));
+        final Device device = new Device();
+        device.setStatus(deviceStatus);
+        final Instant updated = Instant.now().truncatedTo(ChronoUnit.SECONDS);
+        final String version = "spam";
+
+        final var deviceDto = MongoDbBasedDeviceDto.forUpdate(MongoDbBasedDeviceDto::new, tenantId, deviceId, true, device, version);
+        final var json = JsonObject.mapFrom(deviceDto);
+
+        assertThat(json).isNotNull();
+        // size should match number of assertions below, to make sure no unexpected properties are serialized
+        assertThat(json.getMap()).hasSize(7);
+
+        assertThat(json.getString(RegistryManagementConstants.FIELD_PAYLOAD_TENANT_ID)).isEqualTo(tenantId);
+        assertThat(json.getString(RegistryManagementConstants.FIELD_PAYLOAD_DEVICE_ID)).isEqualTo(deviceId);
+        assertThat(json.getInstant(BaseDto.FIELD_UPDATED_ON)).isEqualTo(updated);
+        assertThat(json.getString(BaseDto.FIELD_VERSION)).isEqualTo(version);
+        assertThat(json.getBoolean(RegistryManagementConstants.FIELD_AUTO_PROVISIONED)).isFalse();
         assertThat(json.getBoolean(RegistryManagementConstants.FIELD_AUTO_PROVISIONING_NOTIFICATION_SENT)).isTrue();
         assertThat(json.getJsonObject(MongoDbDeviceRegistryUtils.FIELD_DEVICE)).isNotNull();
     }
