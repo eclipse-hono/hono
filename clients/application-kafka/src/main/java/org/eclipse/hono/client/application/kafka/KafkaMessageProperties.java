30644ac80a377eeb1cd92e468a88fe4e65c991dc
Abel Buechner-Mihaljevic <abel.buechner-mihaljevic@bosch.io>
2021-01-13 13:45:53 +0100

    [#2392] Adds interface for northbound clients, agnostic to messaging protocol.
    This adds an interface for clients to be used by northbound applications
    which removes the dependency from AMQP-specific types and allows to easily
    switch between AMQP-based and Kafka-based messaging.
    This currently only supports downstream messaging. Support for command &
    control is to be added later.
    
    Signed-off-by: Abel Buechner-Mihaljevic <abel.buechner-mihaljevic@bosch.io>

---
+++ clients/application-kafka/src/main/java/org/eclipse/hono/client/application/kafka/KafkaMessageProperties.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package org.eclipse.hono.client.application.kafka;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.eclipse.hono.client.application.Message;
+import org.eclipse.hono.client.application.MessageProperties;
+
+import io.vertx.core.buffer.Buffer;
+
+/**
+ * The metadata of a {@link Message} created from a {@link ConsumerRecord}.
+ */
+public class KafkaMessageProperties implements MessageProperties {
+
+    private final Map<String, Object> properties = new HashMap<>();
+
+    /**
+     * Creates message properties from a Kafka consumer record.
+     *
+     * @param record The consumer record to extract the metadata from.
+     * @throws NullPointerException if record is {@code null}.
+     */
+    public KafkaMessageProperties(final ConsumerRecord<String, Buffer> record) {
+        Objects.requireNonNull(record);
+
+        record.headers().forEach(header -> properties.put(header.key(), header.value()));
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return The headers of the {@link ConsumerRecord}.
+     */
+    @Override
+    public final Map<String, Object> getPropertiesMap() {
+        return properties;
+    }
+
+}
