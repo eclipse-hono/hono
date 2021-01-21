e74e50ba8cacebc418ff7075511af687181b1d2b
Carsten Lohmann <carsten.lohmann@bosch.io>
2021-01-21 14:51:53 +0100

    Enable checkstyle plugin in hono-service-base-jdbc module.
    Signed-off-by: Carsten Lohmann <carsten.lohmann@bosch.io>

--- services/base-jdbc/src/main/java/org/eclipse/hono/service/base/jdbc/store/StatementConfiguration.java
+++ services/base-jdbc/src/main/java/org/eclipse/hono/service/base/jdbc/store/StatementConfiguration.java
@@ -21,12 +21,9 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
