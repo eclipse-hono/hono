4f0e1bcdc9843252554a409bfd61fb58f945f5e9
Carsten Lohmann <carsten.lohmann@bosch.io>
2021-08-02 13:33:52 +0200

    [#2028] Change adapter instance id format.
    Format is now "[pod_name]_[container_id]_[counter]".
    
    Signed-off-by: Carsten Lohmann <carsten.lohmann@bosch.io>

--- clients/command/src/main/java/org/eclipse/hono/client/command/CommandRouterCommandConsumerFactory.java
+++ clients/command/src/main/java/org/eclipse/hono/client/command/CommandRouterCommandConsumerFactory.java
@@ -19,13 +19,16 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 import org.eclipse.hono.client.ClientErrorException;
 import org.eclipse.hono.client.ConnectionLifecycle;
 import org.eclipse.hono.client.ServiceInvocationException;
+import org.eclipse.hono.util.KubernetesContainerUtil;
 import org.eclipse.hono.util.Lifecycle;
 import org.eclipse.hono.util.Strings;
 import org.eclipse.hono.util.TenantConstants;
@@ -46,7 +49,10 @@ import io.vertx.core.Vertx;
  */
 public class CommandRouterCommandConsumerFactory implements CommandConsumerFactory {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
+    private static final Logger LOG = LoggerFactory.getLogger(CommandRouterCommandConsumerFactory.class);
+
+    private static final AtomicInteger ADAPTER_INSTANCE_ID_COUNTER = new AtomicInteger();
+
     /**
      * Identifier that has to be unique to this factory instance.
      * Will be used to represent the protocol adapter (verticle) instance that this factory instance is used in,
@@ -96,10 +102,25 @@ public class CommandRouterCommandConsumerFactory implements CommandConsumerFacto
     }
 
     private static String getNewAdapterInstanceId(final String adapterName) {
+        final String k8sContainerId = KubernetesContainerUtil.getContainerId();
+        if (k8sContainerId == null || k8sContainerId.length() < 12) {
+            LOG.info("container id could not be identified, using random ID as adapterInstanceId");
+            final String prefix = Strings.isNullOrEmpty(adapterName) ? ""
+                    : adapterName.replaceAll("[^a-zA-Z0-9._-]", "") + "-";
+            return prefix + UUID.randomUUID();
+        }
+        // prefer HOSTNAME env var containing the pod name if running in Kubernetes
+        String adapterInstanceIdPrefix = System.getenv("HOSTNAME");
+        if (Strings.isNullOrEmpty(adapterInstanceIdPrefix)) {
+            adapterInstanceIdPrefix = adapterName;
+        }
         // replace special characters so that the id can be used in a Kafka topic name
-        final String prefix = Strings.isNullOrEmpty(adapterName) ? ""
-                : adapterName.replaceAll("[^a-zA-Z0-9._-]", "") + "-";
-        return prefix + UUID.randomUUID();
+        adapterInstanceIdPrefix = Optional.ofNullable(adapterInstanceIdPrefix)
+                .map(prefix -> prefix.replaceAll("[^a-zA-Z0-9._-]", "")).orElse("");
+        return String.format("%s_%s_%d",
+                adapterInstanceIdPrefix,
+                k8sContainerId.substring(0, 12),
+                ADAPTER_INSTANCE_ID_COUNTER.getAndIncrement());
     }
 
     /**
@@ -116,7 +137,7 @@ public class CommandRouterCommandConsumerFactory implements CommandConsumerFacto
     public void registerInternalCommandConsumer(
             final BiFunction<String, CommandHandlers, Lifecycle> internalCommandConsumerSupplier) {
         final Lifecycle consumer = internalCommandConsumerSupplier.apply(adapterInstanceId, commandHandlers);
-        log.info("register internal command consumer {}", consumer.getClass().getSimpleName());
+        LOG.info("register internal command consumer {}", consumer.getClass().getSimpleName());
         internalCommandConsumers.add(consumer);
     }
 
@@ -191,7 +212,7 @@ public class CommandRouterCommandConsumerFactory implements CommandConsumerFacto
         // lifespan greater than what can be expressed in nanoseconds (i.e. 292 years) is considered unlimited, preventing ArithmeticExceptions down the road
         final Duration sanitizedLifespan = lifespan == null || lifespan.isNegative()
                 || lifespan.getSeconds() > (Long.MAX_VALUE / 1000_000_000L) ? Duration.ofSeconds(-1) : lifespan;
-        log.trace("create command consumer [tenant-id: {}, device-id: {}, gateway-id: {}]", tenantId, deviceId, gatewayId);
+        LOG.trace("create command consumer [tenant-id: {}, device-id: {}, gateway-id: {}]", tenantId, deviceId, gatewayId);
 
         // register the command handler
         // for short-lived command consumers, let the consumer creation span context be used as reference in the command span
@@ -205,7 +226,7 @@ public class CommandRouterCommandConsumerFactory implements CommandConsumerFacto
         return commandRouterClient
                 .registerCommandConsumer(tenantId, deviceId, adapterInstanceId, sanitizedLifespan, context)
                 .onFailure(thr -> {
-                    log.info("error registering consumer with the command router service [tenant: {}, device: {}]",
+                    LOG.info("error registering consumer with the command router service [tenant: {}, device: {}]",
                             tenantId, deviceId, thr);
                     // handler association failed - unregister the handler
                     commandHandlers.removeCommandHandler(tenantId, deviceId);
@@ -230,7 +251,7 @@ public class CommandRouterCommandConsumerFactory implements CommandConsumerFacto
         final String tenantId = commandHandlerWrapper.getTenantId();
         final String deviceId = commandHandlerWrapper.getDeviceId();
 
-        log.trace("remove command consumer [tenant-id: {}, device-id: {}]", tenantId, deviceId);
+        LOG.trace("remove command consumer [tenant-id: {}, device-id: {}]", tenantId, deviceId);
         if (!commandHandlers.removeCommandHandler(commandHandlerWrapper)) {
             // This case happens when trying to remove a command consumer which has been overwritten since its creation
             // via a 2nd invocation of 'createCommandConsumer' with the same device/tenant id. Since the 2nd 'createCommandConsumer'
@@ -241,7 +262,7 @@ public class CommandRouterCommandConsumerFactory implements CommandConsumerFacto
             // reached the *same* adapter instance and verticle, using this CommandConsumerFactory. Invoking 'removeCommandConsumer'
             // on the 1st (obsolete and overwritten) command subscription shall have no impact. Throwing an explicit exception
             // here will enable the protocol adapter to detect this case and skip an (incorrect) "disconnectedTtd" event message.
-            log.debug("command consumer not removed - handler already replaced or removed [tenant: {}, device: {}]",
+            LOG.debug("command consumer not removed - handler already replaced or removed [tenant: {}, device: {}]",
                     tenantId, deviceId);
             return Future.failedFuture(new ClientErrorException(HttpURLConnection.HTTP_PRECON_FAILED,
                     "local command handler already replaced or removed"));
@@ -255,7 +276,7 @@ public class CommandRouterCommandConsumerFactory implements CommandConsumerFacto
                     if (ServiceInvocationException.extractStatusCode(thr) == HttpURLConnection.HTTP_PRECON_FAILED) {
                         final boolean entryMayHaveExpired = !lifespan.isNegative() && Instant.now().isAfter(lifespanStart.plus(lifespan));
                         if (entryMayHaveExpired) {
-                            log.trace("ignoring 412 error when unregistering consumer with the command router service; entry may have already expired [tenant: {}, device: {}]",
+                            LOG.trace("ignoring 412 error when unregistering consumer with the command router service; entry may have already expired [tenant: {}, device: {}]",
                                     tenantId, deviceId);
                             return Future.succeededFuture();
                         } else {
@@ -266,13 +287,13 @@ public class CommandRouterCommandConsumerFactory implements CommandConsumerFacto
                             // on the 1st subscription fails because of the non-matching adapterInstanceId parameter.
                             // Throwing an explicit exception here will enable the protocol adapter to detect this case
                             // and skip sending an (incorrect) "disconnectedTtd" event message.
-                            log.debug("consumer not unregistered - not matched or already removed [tenant: {}, device: {}]",
+                            LOG.debug("consumer not unregistered - not matched or already removed [tenant: {}, device: {}]",
                                     tenantId, deviceId);
                             return Future.failedFuture(new ClientErrorException(HttpURLConnection.HTTP_PRECON_FAILED,
                                     "no matching command consumer mapping found to be removed"));
                         }
                     } else {
-                        log.info("error unregistering consumer with the command router service [tenant: {}, device: {}]", tenantId,
+                        LOG.info("error unregistering consumer with the command router service [tenant: {}, device: {}]", tenantId,
                                 deviceId, thr);
                         return Future.failedFuture(thr);
                     }
