9d940edd6416daf68ef59cb48e277bb0e820ea30
Bob Claerhout <claerhout.bob@gmail.com>
2021-11-10 11:30:47 +0100

    Add support for current payload format used by Kerlink Lora provider
    The Lora adapter has been extended with an additional provider implementation that
    supports the uplink payload format produced by the current version of the Kerlink network server.
    
    Signed-off-by: Bob Claerhout <claerhout.bob@gmail.com>

--- adapters/lora-vertx-base/src/test/java/org/eclipse/hono/adapter/lora/providers/KerlinkProviderTest.java
+++ adapters/lora-vertx-base/src/test/java/org/eclipse/hono/adapter/lora/providers/KerlinkProviderTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2020 Contributors to the Eclipse Foundation
+ * Copyright (c) 2019, 2021 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -13,6 +13,10 @@
 
 package org.eclipse.hono.adapter.lora.providers;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import org.eclipse.hono.adapter.lora.LoraMetaData;
+import org.eclipse.hono.adapter.lora.UplinkLoraMessage;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import io.vertx.junit5.VertxExtension;
@@ -29,5 +33,39 @@ public class KerlinkProviderTest extends LoraProviderTestBase<KerlinkProvider> {
     @Override
     protected KerlinkProvider newProvider() {
         return new KerlinkProvider();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void assertMetaDataForUplinkMessage(final UplinkLoraMessage loraMessage) {
+
+        final LoraMetaData metaData = loraMessage.getMetaData();
+        assertThat(metaData.getAdaptiveDataRateEnabled()).isTrue();
+        assertThat(metaData.getBandwidth()).isEqualTo(125);
+        assertThat(metaData.getCodingRate()).isEqualTo("4/5");
+        assertThat(metaData.getFrameCount()).isEqualTo(2602);
+        assertThat(metaData.getFrequency()).isEqualTo(868.3);
+        assertThat(metaData.getFunctionPort()).isEqualTo(2);
+        assertThat(metaData.getSpreadingFactor()).isEqualTo(7);
+        assertThat(metaData.getGatewayInfo()).hasSize(2);
+        assertThat(metaData.getLocation().getLongitude()).isEqualTo(53.108805);
+        assertThat(metaData.getLocation().getLatitude()).isEqualTo(9.193430);
+        assertThat(metaData.getLocation().getAltitude()).isNull();
+        assertThat(metaData.getGatewayInfo().get(0).getGatewayId()).isEqualTo("0101010101010101");
+        assertThat(metaData.getGatewayInfo().get(0).getRssi()).isEqualTo(-84);
+        assertThat(metaData.getGatewayInfo().get(0).getSnr()).isEqualTo(9.8);
+        assertThat(metaData.getGatewayInfo().get(0).getChannel()).isEqualTo(3);
+        assertThat(metaData.getGatewayInfo().get(0).getLocation().getLongitude()).isEqualTo(53.108804);
+        assertThat(metaData.getGatewayInfo().get(0).getLocation().getLatitude()).isEqualTo(9.193431);
+        assertThat(metaData.getGatewayInfo().get(0).getLocation().getAltitude()).isNull();
+        assertThat(metaData.getGatewayInfo().get(1).getGatewayId()).isEqualTo("0202020202020202");
+        assertThat(metaData.getGatewayInfo().get(1).getRssi()).isEqualTo(-116);
+        assertThat(metaData.getGatewayInfo().get(1).getSnr()).isEqualTo(-3.2);
+        assertThat(metaData.getGatewayInfo().get(1).getChannel()).isEqualTo(4);
+        assertThat(metaData.getGatewayInfo().get(1).getLocation().getLongitude()).isEqualTo(53.108806);
+        assertThat(metaData.getGatewayInfo().get(1).getLocation().getLatitude()).isEqualTo(9.193429);
+        assertThat(metaData.getGatewayInfo().get(1).getLocation().getAltitude()).isNull();
     }
 }
