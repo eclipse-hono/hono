91aef91e431fd024bfd82ff71f7d641c0c6f2109
Kai Hudalla <kai.hudalla@bosch.io>
2022-02-11 12:42:19 +0100

    Use annotation to register service-base classes for reflection
    The RegisterForReflection annotation provided by Quarkus is now used for
    marking classes that need to be registered for reflection so that
    GraalVM can include them in native executables.
    
    The approach using the annotation is much more explicit and easier to
    maintain than the JSON file based approach.
    
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch.io>

--- service-base/src/main/java/org/eclipse/hono/service/cache/Caches.java
+++ service-base/src/main/java/org/eclipse/hono/service/cache/Caches.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021 Contributors to the Eclipse Foundation
+ * Copyright (c) 2021, 2022 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -25,10 +25,19 @@ import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.Expiry;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 /**
  * A helper class for creating Caffeine based cache instances.
  *
  */
+@RegisterForReflection(
+        classNames = {
+            "com.github.benmanes.caffeine.cache.SSMSA",
+            "com.github.benmanes.caffeine.cache.SSMSWR",
+            "com.github.benmanes.caffeine.cache.PSWMS",
+            "com.github.benmanes.caffeine.cache.PSWRMW"
+        })
 public final class Caches {
 
     private Caches() {
