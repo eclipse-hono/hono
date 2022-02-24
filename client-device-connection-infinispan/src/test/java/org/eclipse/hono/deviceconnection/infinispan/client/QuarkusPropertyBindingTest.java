97d1b80a570d8058d2dd4b14dfa08e78b9388ab4
Carsten Lohmann <carsten.lohmann@bosch.io>
2022-02-24 09:16:46 +0100

    [#3106] Allow nested HotRod config keys to be in camel-case.
    Also add support for configuring
    "infinispan.client.hotrod.default_executor_factory.*"
    HotRod client properties.
    
    Signed-off-by: Carsten Lohmann <carsten.lohmann@bosch.io>

--- client-device-connection-infinispan/src/test/java/org/eclipse/hono/deviceconnection/infinispan/client/QuarkusPropertyBindingTest.java
+++ client-device-connection-infinispan/src/test/java/org/eclipse/hono/deviceconnection/infinispan/client/QuarkusPropertyBindingTest.java
@@ -11,15 +11,18 @@
  * SPDX-License-Identifier: EPL-2.0
  */
 
-
 package org.eclipse.hono.deviceconnection.infinispan.client;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import org.eclipse.hono.test.ConfigMappingSupport;
-import org.infinispan.client.hotrod.impl.ConfigurationProperties;
-import org.junit.jupiter.api.Test;
+import java.util.List;
 
+import javax.security.sasl.Sasl;
+
+import org.eclipse.hono.test.ConfigMappingSupport;
+import org.infinispan.client.hotrod.configuration.ClusterConfiguration;
+import org.infinispan.client.hotrod.configuration.Configuration;
+import org.junit.jupiter.api.Test;
 
 /**
  * Tests verifying binding of configuration properties to {@link CommonCacheConfig} and
@@ -49,20 +52,39 @@ public class QuarkusPropertyBindingTest {
                 ConfigMappingSupport.getConfigMapping(
                         InfinispanRemoteConfigurationOptions.class,
                         this.getClass().getResource("/remote-cache-options.yaml")));
+        final Configuration builtRemoteCacheConfig = remoteCacheConfig.getConfigurationBuilder().build(false);
 
         assertThat(remoteCacheConfig.getServerList()).contains("data-grid:11222");
         assertThat(remoteCacheConfig.getAuthUsername()).isEqualTo("user");
         assertThat(remoteCacheConfig.getAuthPassword()).isEqualTo("secret");
         assertThat(remoteCacheConfig.getAuthRealm()).isEqualTo("ApplicationRealm");
+
+        final List<ClusterConfiguration> clusters = builtRemoteCacheConfig.clusters();
+        assertThat(clusters.size()).isEqualTo(2);
+        assertThat(clusters.get(0).getClusterName()).isEqualTo("siteA");
+        assertThat(clusters.get(0).getCluster().size()).isEqualTo(2);
+        assertThat(clusters.get(0).getCluster().get(0).host()).isEqualTo("hostA1");
+        assertThat(clusters.get(0).getCluster().get(0).port()).isEqualTo(11222);
+        assertThat(clusters.get(1).getClusterName()).isEqualTo("siteB");
+
+        assertThat(remoteCacheConfig.gtConnectionPoolMinIdle()).isEqualTo(10);
+        assertThat(remoteCacheConfig.getConnectionPoolMaxActive()).isEqualTo(10);
+        assertThat(remoteCacheConfig.getConnectionPoolMaxPendingRequests()).isEqualTo(400);
+        assertThat(remoteCacheConfig.getConnectionPoolMaxWait()).isEqualTo(500);
+
+        assertThat(remoteCacheConfig.getDefaultExecutorFactoryPoolSize()).isEqualTo(200);
+
         assertThat(remoteCacheConfig.getSaslMechanism()).contains("DIGEST-MD5");
+        final var saslProperties = builtRemoteCacheConfig.security().authentication().saslProperties();
+        assertThat(saslProperties.get(Sasl.QOP)).isEqualTo("auth");
+
         assertThat(remoteCacheConfig.getSoTimeout()).isEqualTo(5000);
         assertThat(remoteCacheConfig.getConnectTimeout()).isEqualTo(5000);
         assertThat(remoteCacheConfig.getKeyStoreFileName()).isEqualTo("/etc/hono/key-store.p12");
         assertThat(remoteCacheConfig.getKeyStoreType()).isEqualTo("PKCS12");
         assertThat(remoteCacheConfig.getKeyStorePassword()).isEqualTo("key-store-secret");
         assertThat(remoteCacheConfig.getKeyAlias()).isEqualTo("infinispan");
-        assertThat(remoteCacheConfig.getProperties().getProperty(ConfigurationProperties.KEY_STORE_CERTIFICATE_PASSWORD))
-            .isEqualTo("cert-secret");
+        assertThat(remoteCacheConfig.getKeyStoreCertificatePassword()).isEqualTo("cert-secret");
         assertThat(remoteCacheConfig.getTrustStoreFileName()).isEqualTo("/etc/hono/trust-store-file.p12");
         assertThat(remoteCacheConfig.getTrustStorePath()).isEqualTo("/etc/hono/trust-store.p12");
         assertThat(remoteCacheConfig.getTrustStoreType()).isEqualTo("PKCS12");
