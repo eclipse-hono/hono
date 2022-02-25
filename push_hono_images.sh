4c11e3fbf8f816ea4a0920b647dce23218f4278c
Kai Hudalla <kai.hudalla@bosch.io>
2022-02-25 13:01:19 +0100

    [#3116] Remove Spring Boot based Lora adapter
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch.io>

--- push_hono_images.sh
+++ push_hono_images.sh
@@ -20,7 +20,6 @@ IMAGES="hono-adapter-amqp-vertx \
         hono-adapter-coap-vertx-quarkus \
         hono-adapter-http-vertx \
         hono-adapter-http-vertx-quarkus \
-        hono-adapter-lora-vertx \
         hono-adapter-lora-vertx-quarkus \
         hono-adapter-mqtt-vertx \
         hono-adapter-mqtt-vertx-quarkus \
