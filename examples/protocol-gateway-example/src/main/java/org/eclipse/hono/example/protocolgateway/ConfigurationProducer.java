7dc54dc401433d415bd873bb3b8e784dc5a192d0
Carsten Lohmann <carsten.lohmann@bosch.io>
2022-04-20 11:55:10 +0200

    Adapt documentation related to AmqpAdapterClient.
    Signed-off-by: Carsten Lohmann <carsten.lohmann@bosch.io>

--- examples/protocol-gateway-example/src/main/java/org/eclipse/hono/example/protocolgateway/ConfigurationProducer.java
+++ examples/protocol-gateway-example/src/main/java/org/eclipse/hono/example/protocolgateway/ConfigurationProducer.java
@@ -66,7 +66,7 @@ public class ConfigurationProducer {
 
     @Produces
     @Dependent
-    AmqpAdapterClient amqpAdapterClientFactory(
+    AmqpAdapterClient amqpAdapterClient(
             final HonoConnection amqpAdapterConnection) {
 
         return AmqpAdapterClient.create(amqpAdapterConnection);
