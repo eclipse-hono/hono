b3d8d968c3adb6c01bda30f36eeb62b0716f400f
Kai Hudalla <kai.hudalla@bosch.io>
2022-04-20 18:09:16 +0200

    Register legacy checks with SmallRye HealthRegistry directly
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch.io>

--- service-base/src/main/java/org/eclipse/hono/service/SmallRyeHealthCheckServer.java
+++ service-base/src/main/java/org/eclipse/hono/service/SmallRyeHealthCheckServer.java
@@ -18,52 +18,73 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 
-import javax.inject.Singleton;
+import javax.enterprise.context.ApplicationScoped;
 
 import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+import org.eclipse.microprofile.health.Readiness;
 
+import io.smallrye.health.api.HealthRegistry;
+import io.smallrye.mutiny.vertx.UniHelper;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.ext.healthchecks.CheckResult;
 import io.vertx.ext.healthchecks.HealthCheckHandler;
-import io.vertx.ext.healthchecks.HealthChecks;
 import io.vertx.ext.healthchecks.Status;
 import io.vertx.ext.web.RoutingContext;
 
 
 /**
- * A HealthCheckServer that exposes Vert.x Health Checks as MicroProfile Health checks.
+ * A SmallRye Health based {@link HealthCheckServer} that adapts Vert.x Health Checks to MicroProfile Health checks.
+ * <p>
+ * All checks that are being {@linkplain #registerHealthCheckResources(HealthCheckProvider registered)} are added to
+ * the underlying SmallRye {@code HealthRegistry}.
  *
  * @deprecated Consider implementing health checks according to the MicroProfile Health specification instead of
  *             Vert.x Health and register them as CDI beans as described in https://quarkus.io/guides/smallrye-health
  */
-@Singleton
+@ApplicationScoped
 @Deprecated
 public class SmallRyeHealthCheckServer implements HealthCheckServer {
 
-    private final HealthChecks readinessChecks;
-    private final HealthChecks livenessChecks;
-    private final HealthChecksAdapter readinessChecksAdapter;
-    private final HealthChecksAdapter livenessChecksAdapter;
+    private final SmallRyeHealthAdapter readinessChecksAdapter;
+    private final SmallRyeHealthAdapter livenessChecksAdapter;
 
     /**
      * @param vertx The vert.x instance to use.
+     * @param readinessCheckRegistry The registry for readiness checks.
+     * @param livenessCheckRegistry The registry for liveness checks.
      */
-    public SmallRyeHealthCheckServer(final Vertx vertx) {
+    public SmallRyeHealthCheckServer(
+            final Vertx vertx,
+            @Readiness
+            final HealthRegistry readinessCheckRegistry,
+            @Liveness
+            final HealthRegistry livenessCheckRegistry) {
         Objects.requireNonNull(vertx);
-        this.readinessChecks = HealthChecks.create(vertx);
-        this.livenessChecks = HealthChecks.create(vertx);
-        this.readinessChecksAdapter = new HealthChecksAdapter(readinessChecks);
-        this.livenessChecksAdapter = new HealthChecksAdapter(livenessChecks);
+        Objects.requireNonNull(readinessCheckRegistry);
+        Objects.requireNonNull(livenessCheckRegistry);
+        this.readinessChecksAdapter = new SmallRyeHealthAdapter(readinessCheckRegistry);
+        this.livenessChecksAdapter = new SmallRyeHealthAdapter(livenessCheckRegistry);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @return A succeeded future.
+     */
     @Override
     public Future<Void> start() {
         return Future.succeededFuture();
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @return A succeeded future.
+     */
     @Override
     public Future<Void> stop() {
         return Future.succeededFuture();
@@ -72,85 +93,103 @@ public class SmallRyeHealthCheckServer implements HealthCheckServer {
     @Override
     public void registerHealthCheckResources(final HealthCheckProvider serviceInstance) {
         Objects.requireNonNull(serviceInstance);
-        synchronized (readinessChecks) {
-            serviceInstance.registerReadinessChecks(readinessChecksAdapter);
-        }
-        synchronized (livenessChecks) {
-            serviceInstance.registerLivenessChecks(livenessChecksAdapter);
-        }
+        serviceInstance.registerReadinessChecks(readinessChecksAdapter);
+        serviceInstance.registerLivenessChecks(livenessChecksAdapter);
     }
 
-    /**
-     * Invokes the registered readiness checks.
-     *
-     * @return The status reported by the checks.
-     */
-    public Future<HealthCheckResponse> invokeReadinessChecks() {
-        synchronized (readinessChecks) {
-            return readinessChecks.checkStatus()
-                    .map(result -> mapCheckResult(result, "legacy-readiness-checks"));
-        }
-    }
+    private static class SmallRyeHealthAdapter implements HealthCheckHandler {
 
-    /**
-     * Invokes the registered liveness checks.
-     *
-     * @return The status reported by the checks.
-     */
-    public Future<HealthCheckResponse> invokeLivenessChecks() {
-        synchronized (livenessChecks) {
-            return livenessChecks.checkStatus()
-                    .map(result -> mapCheckResult(result, "legacy-liveness-checks"));
-        }
-    }
+        final HealthRegistry registry;
 
-    private HealthCheckResponse mapCheckResult(final CheckResult result, final String defaultName) {
-        return HealthCheckResponse.builder()
-                .name(Optional.ofNullable(result.getId()).orElse(defaultName))
-                .status(result.getUp())
-                .build();
-    }
-
-    private static class HealthChecksAdapter implements HealthCheckHandler {
-
-        private final HealthChecks checks;
-
-        private HealthChecksAdapter(final HealthChecks healthChecks) {
-            this.checks = Objects.requireNonNull(healthChecks);
+        /**
+         * Creates a new adapter.
+         *
+         * @param registry The registry to add the health checks to.
+         */
+        private SmallRyeHealthAdapter(final HealthRegistry registry) {
+            this.registry = Objects.requireNonNull(registry);
         }
 
+        /**
+         * {@inheritDoc}
+         *
+         * @throws UnsupportedOperationException if invoked.
+         */
         @Override
         public void handle(final RoutingContext event) {
             throw new UnsupportedOperationException();
         }
 
+        private HealthCheckResponse getResponse(final String name, final Status status) {
+            final var builder = HealthCheckResponse.builder();
+            builder.name(name);
+            builder.status(status.isOk());
+            Optional.ofNullable(status.getData())
+                .ifPresent(json -> {
+                    json.stream().forEach(entry -> {
+                        if (entry.getValue() instanceof Boolean) {
+                            builder.withData(entry.getKey(), (Boolean) entry.getValue());
+                        } else if (entry.getValue() instanceof String) {
+                            builder.withData(entry.getKey(), (String) entry.getValue());
+                        } else if (entry.getValue() instanceof Number) {
+                            builder.withData(entry.getKey(), ((Number) entry.getValue()).longValue());
+                        }
+                    });
+                });
+            return builder.build();
+        }
+
+        private HealthCheckResponse getResponse(final String name, final Throwable error) {
+            final var builder = HealthCheckResponse.builder()
+                    .name(name)
+                    .withData("error", error.getMessage())
+                    .down();
+            return builder.build();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public HealthCheckHandler register(final String name, final Handler<Promise<Status>> procedure) {
-            checks.register(name, procedure);
+            registry.register(name, () -> {
+                final Promise<Status> result = Promise.promise();
+                procedure.handle(result);
+                return UniHelper.toUni(result.future()
+                        .map(status -> getResponse(name, status))
+                        .otherwise(t -> getResponse(name, t)));
+            });
             return this;
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public HealthCheckHandler register(
                 final String name,
                 final long timeout,
                 final Handler<Promise<Status>> procedure) {
-            checks.register(name, timeout, procedure);
-            return this;
+            return register(name, procedure);
         }
 
         /**
-         * Always throws {@link UnsupportedOperationException}.
+         * {@inheritDoc}
+         */
+        @Override
+        public HealthCheckHandler unregister(final String name) {
+            registry.remove(name);
+            return null;
+        }
+
+        /**
+         * {@inheritDoc}
+         *
+         * @throws UnsupportedOperationException if invoked.
          */
         @Override
         public HealthCheckHandler resultMapper(final Function<CheckResult, Future<CheckResult>> resultMapper) {
             throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public HealthCheckHandler unregister(final String name) {
-            checks.unregister(name);
-            return this;
         }
     }
 }
