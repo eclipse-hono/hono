e392e82ae7248dcd2e100027518793d04d12f5fe
Kai Hudalla <kai.hudalla@bosch.io>
2022-06-07 09:10:24 +0200

    Support setting a custom repository name when pushing images
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch.io>

--- push_hono_images.sh
+++ push_hono_images.sh
@@ -13,7 +13,9 @@
 #*******************************************************************************
 
 TAG=$1
-CR=$2
+CR=${2:-"docker.io"}
+REPO=${3:-"eclipse"}
+
 IMAGES="hono-adapter-amqp \
         hono-adapter-coap \
         hono-adapter-http \
@@ -38,30 +40,33 @@ NATIVE_IMAGES="hono-adapter-amqp-native \
 ME=`basename "$0"`
 echo "called as $ME"
 
-if [ "push_hono_native_images.sh" == "$ME" ]
+if [[ "push_hono_native_images.sh" == "$ME" ]]
 then
   IMAGES=${NATIVE_IMAGES}
 fi
 
-if [ -n "$TAG" ]
+if [[ -n "$TAG" ]]
 then
   for image in $IMAGES
   do
     ECLIPSE_IMAGE_NAME="eclipse/$image"
-    IMAGE_NAME=$ECLIPSE_IMAGE_NAME
-    if [ -n "$CR" ]
+    if [[ "docker.io" != "${CR}" || "eclipse" != "${REPO}" ]]
     then
-      IMAGE_NAME="$CR/$IMAGE_NAME"
+      IMAGE_NAME="${CR}/${REPO}/${image}"
       docker tag $ECLIPSE_IMAGE_NAME:$TAG $IMAGE_NAME:$TAG
+    else
+      IMAGE_NAME=$ECLIPSE_IMAGE_NAME
     fi
     echo "pushing image $IMAGE_NAME:$TAG ..."
     docker push $IMAGE_NAME:$TAG
   done
 else
   echo "This script can be used to push Hono's images from"
-  echo "the local Docker registry to Docker Hub."
+  echo "the local Docker registry to a (remote) container registry."
   echo ""
-  echo "usage: push_hono_images.sh TAG [CR]"
-  echo "where TAG is the TAG to push to Docker Hub"
-  echo "and the (optional) CR is the name of the container registry to push to"
+  echo "usage: push_hono_images.sh TAG [CR [REPOSITORY]]"
+  echo "TAG is the (already existing) TAG to push to the registry."
+  echo "CR is the name of the container registry to push to."
+  echo "REPOSITORY is the name of the repository within the registry to push to."
+  echo "If only TAG is specified, then the images are pushed to 'docker.io' using repository name 'eclipse'."
 fi
