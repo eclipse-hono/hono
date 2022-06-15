7d0002bb0d6ac7731681021f4be287440e72b454
Kai Hudalla <kai.hudalla@bosch.io>
2022-06-15 07:54:12 +0200

    Explicitly add opens directives to MAVEN_OPTS
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch.io>

--- jenkins/Hono-Deploy-Maven-Central-Pipeline-Declarative.groovy
+++ jenkins/Hono-Deploy-Maven-Central-Pipeline-Declarative.groovy
@@ -133,7 +133,7 @@ spec:
             sh 'gpg --list-secret-keys'
           }
           sh '''
-            export MAVEN_OPTS="--illegal-access=permit"
+            export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
             mvn deploy \
               -DskipTests=true -DnoDocker -DcreateGPGSignature=true -DcreateJavadoc=true -DenableEclipseJarSigner=true \
               -am -pl "\
