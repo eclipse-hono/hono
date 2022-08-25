c1cf9b0143cea942c661282651e6982e880fe714
Kai Hudalla <kai.hudalla@bosch.io>
2022-08-25 07:43:15 +0200

    Fix comment
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch.io>

--- tests/src/test/resources/mongodb/init_db/5_create_http_users.js
+++ tests/src/test/resources/mongodb/init_db/5_create_http_users.js
@@ -1,5 +1,5 @@
 db.registryusers.createIndex( { login: 1 }, { unique: true } );
-// alllow user "test-client" with password "secret" to access the registry's HTTP API endpoint
+// alllow the user defined by the maven properties in the pom.xml to access the registry's HTTP API endpoint
 db.registryusers.insertOne( {
   login: "${hono.deviceregistry.http.authConfig.username}",
   spice: "${hono.deviceregistry.http.authConfig.salt}",
