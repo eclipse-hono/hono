ff9b771b08bdef897311c273594ab0079924dd51
Carsten Lohmann <carsten.lohmann@bosch.io>
2022-09-15 16:36:55 +0200

    Simplify SamplerProducer.
    With Quarkus 2.12.2, the manual exclusion of
    non-application URI request traces if a custom
    Sampler is used isn't needed anymore.
    
    Signed-off-by: Carsten Lohmann <carsten.lohmann@bosch.io>

--- service-base/src/main/java/org/eclipse/hono/service/tracing/SamplerProducer.java
+++ service-base/src/main/java/org/eclipse/hono/service/tracing/SamplerProducer.java
@@ -12,7 +12,6 @@
  */
 package org.eclipse.hono.service.tracing;
 
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -23,13 +22,11 @@ import javax.inject.Singleton;
 
 import org.eclipse.hono.util.AuthenticationConstants;
 import org.eclipse.hono.util.Constants;
-import org.eclipse.microprofile.config.ConfigProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.opentelemetry.sdk.trace.samplers.Sampler;
 import io.quarkus.opentelemetry.runtime.tracing.TracerRuntimeConfig;
-import io.smallrye.config.SmallRyeConfig;
 
 /**
  * A producer for an OpenTelemetry Sampler.
@@ -74,11 +71,8 @@ public class SamplerProducer {
         sampler = Sampler.parentBased(sampler);
         sampler = new SamplingPrioritySampler(sampler);
 
-        final SmallRyeConfig config = ConfigProvider.getConfig().unwrap(SmallRyeConfig.class);
-        final List<String> prefixesOfSpansToDrop = new ArrayList<>(getNonApplicationUriPaths(config));
-        LOG.info("spans for non-application-URI HTTP requests will be dropped; relevant span name prefixes: {}", prefixesOfSpansToDrop);
         // drop spans for event bus message prefixes
-        prefixesOfSpansToDrop.addAll(EVENT_BUS_ADDRESS_PREFIXES_TO_IGNORE);
+        final List<String> prefixesOfSpansToDrop = new ArrayList<>(EVENT_BUS_ADDRESS_PREFIXES_TO_IGNORE);
         // drop "/bad-request" spans for invalid HTTP requests
         prefixesOfSpansToDrop.add(INVALID_HTTP_REQUEST_SPAN_NAME);
         return new DropBySpanNamePrefixSampler(sampler, prefixesOfSpansToDrop);
@@ -125,63 +119,5 @@ public class SamplerProducer {
                 return DEFAULT_MAX_TRACES_PER_SECOND;
             }
         }).orElse(DEFAULT_MAX_TRACES_PER_SECOND);
-    }
-
-    /**
-     * Gets a list of the URI paths of the non-application endpoints provided by Quarkus, i.e. the paths
-     * of the health-check and metrics endpoints.
-     *
-     * @param config The config object.
-     * @return The list of URI paths.
-     */
-    static List<String> getNonApplicationUriPaths(final SmallRyeConfig config) {
-        final List<String> paths = new ArrayList<>();
-
-        // the logic here has been adopted from io.quarkus.vertx.http.deployment.NonApplicationRootPathBuildItem and its usage
-        Optional.ofNullable(config.getRawValue("quarkus.http.root-path"))
-                .map(path -> UriNormalizationUtil.toURI(path, true))
-                .ifPresent(httpRootPath -> {
-                    final URI nonAppRootPath = UriNormalizationUtil.normalizeWithBase(
-                            httpRootPath,
-                            config.getRawValue("quarkus.http.non-application-root-path"),
-                            true);
-
-                    addHealthPaths(paths, config, nonAppRootPath);
-                    Optional.ofNullable(config.getRawValue("quarkus.micrometer.export.prometheus.path"))
-                            .map(path -> resolveNonApplicationPath(nonAppRootPath, path))
-                            .ifPresent(paths::add);
-                });
-        return paths;
-    }
-
-    private static void addHealthPaths(final List<String> pathsToAddTo, final SmallRyeConfig config, final URI nonAppRootPath) {
-        Optional.ofNullable(config.getRawValue("quarkus.smallrye-health.root-path"))
-                .map(path -> resolveNonApplicationPath(nonAppRootPath, path))
-                .ifPresent(healthRootPath -> {
-                    Optional.ofNullable(config.getRawValue("quarkus.smallrye-health.liveness-path"))
-                            .map(subPath -> resolveNestedNonApplicationPath(nonAppRootPath, healthRootPath, subPath))
-                            .ifPresent(pathsToAddTo::add);
-                    Optional.ofNullable(config.getRawValue("quarkus.smallrye-health.readiness-path"))
-                            .map(subPath -> resolveNestedNonApplicationPath(nonAppRootPath, healthRootPath, subPath))
-                            .ifPresent(pathsToAddTo::add);
-                    Optional.ofNullable(config.getRawValue("quarkus.smallrye-health.startup-path"))
-                            .map(subPath -> resolveNestedNonApplicationPath(nonAppRootPath, healthRootPath, subPath))
-                            .ifPresent(pathsToAddTo::add);
-                });
-    }
-
-    private static String resolveNonApplicationPath(final URI nonAppRootPath, final String path) {
-        if (path == null || path.trim().isEmpty()) {
-            throw new IllegalArgumentException("Specified path can not be empty");
-        }
-        return UriNormalizationUtil.normalizeWithBase(nonAppRootPath, path, false).getPath();
-    }
-
-    private static String resolveNestedNonApplicationPath(final URI nonAppRootPath, final String path, final String subRoute) {
-        if (path == null || path.trim().isEmpty()) {
-            throw new IllegalArgumentException("Specified path can not be empty");
-        }
-        final URI base = UriNormalizationUtil.normalizeWithBase(nonAppRootPath, path, true);
-        return UriNormalizationUtil.normalizeWithBase(base, subRoute, false).getPath();
     }
 }
