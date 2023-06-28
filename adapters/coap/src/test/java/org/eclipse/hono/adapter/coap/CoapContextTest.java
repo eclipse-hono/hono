bb007338aca82e787683a2600c254443eb9f141e
Stefan Freyr Stefansson <stefan.freyr@controlant.com>
2023-06-28 18:00:25 +0000

    Removing html paragraph tags from copyright notice

--- adapters/coap/src/test/java/org/eclipse/hono/adapter/coap/CoapContextTest.java
+++ adapters/coap/src/test/java/org/eclipse/hono/adapter/coap/CoapContextTest.java
@@ -1,13 +1,13 @@
 /**
  * Copyright (c) 2020, 2023 Contributors to the Eclipse Foundation
- * <p>
+ *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
- * <p>
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0
- * <p>
+ *
  * SPDX-License-Identifier: EPL-2.0
  */
 
