825cd393d0971907b33888de960671df359cf2b8
Kai Hudalla <sophokles.kh@gmail.com>
2024-04-17 08:47:34 +0200

    Properly disable tests for non-implemented features
    The JDBC based Tenant Service does not support Trust Anchor Groups. The
    corresponding unit tests have therefore been marked as @Disabled to
    prevent (slow) execution of the empty test method bodies.

--- services/device-registry-jdbc/src/test/java/org/eclipse/hono/deviceregistry/jdbc/impl/JdbcBasedTenantServiceTest.java
+++ services/device-registry-jdbc/src/test/java/org/eclipse/hono/deviceregistry/jdbc/impl/JdbcBasedTenantServiceTest.java
@@ -14,31 +14,39 @@
 package org.eclipse.hono.deviceregistry.jdbc.impl;
 
 import org.eclipse.hono.service.tenant.AbstractTenantServiceTest;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 import io.vertx.junit5.VertxTestContext;
 
 class JdbcBasedTenantServiceTest extends AbstractJdbcRegistryTest implements AbstractTenantServiceTest {
 
+    @Disabled("This feature is not implemented")
+    @Test
     @Override
     public void testAddTenantWithTrustAnchorGroupAndDuplicateTrustAnchorFails(
             final VertxTestContext ctx) {
-        //This feature is not implemented
     }
 
+    @Disabled("This feature is not implemented")
+    @Test
     @Override
     public void testAddTenantWithTrustAnchorGroupAndDuplicateTrustAnchorSucceeds(
             final VertxTestContext ctx) {
         //This feature is not implemented
     }
 
+    @Disabled("This feature is not implemented")
+    @Test
     @Override
     public void testUpdateTenantWithTrustAnchorGroupAndDuplicateTrustAnchorFails(final VertxTestContext ctx) {
         //This feature is not implemented
     }
 
+    @Disabled("This feature is not implemented")
+    @Test
     @Override
     public void testUpdateTenantWithTrustAnchorGroupAndDuplicateTrustAnchorSucceeds(
             final VertxTestContext ctx) {
-        //This feature is not implemented
     }
 }
